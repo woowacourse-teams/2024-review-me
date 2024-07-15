1dae1cc5fbbc13c74d89e59de75de7ab92469d92
donghoony <aru0504@naver.com>
2024-07-15 17:18:53 +0900

    feat: 초기 환경 설정

---
+++ backend/src/main/java/reviewme/ReviewMeApplication.java
@@ -0,0 +1,13 @@
+package reviewme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewMeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/reviewme/ReviewMeApplicationTests.java
@@ -0,0 +1,13 @@
+package reviewme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
