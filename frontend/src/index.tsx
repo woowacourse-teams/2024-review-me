c66136da8587a6f29578f4d67c0f29d436639315
badahertz52 <badahertz52@gmail.com>
2024-07-15 22:00:25 +0900

    design : reset 스타일, global style에서 폰트 적용
    Co-Authored-By: ImxYJL <111052302+ImxYJL@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,20 @@
 import App from '@/App';
+import { css, Global } from '@emotion/react';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reset from './styles/reset';
+
+import globalStyles from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Global
+      styles={css`
+        ${reset()} ${globalStyles}
+      `}
+    />
+    {/* <App /> */}
+  </React.StrictMode>,
 );
---
+++ frontend/src/styles/globalStyles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  body {
+    font-family: 'NanumGothic', 'Noto Sans', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default globalStyles;
