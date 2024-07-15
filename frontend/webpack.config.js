9fc5c9702d0bfba60801e438664d0f97691fc512
chysis <chysiss@naver.com>
2024-07-15 16:21:18 +0900

    ci: webpack, react, typescript 설정

---
+++ frontend/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpeg|jpg)$/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new StylelintPlugin({
+        files: 'src/**/*.{ts, tsx}',
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+        hash: true,
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    devtool: isProduction ? 'hidden-source-map' : 'eval',
+    devServer: {
+      historyApiFallback: true,
+      port: 3001,
+      hot: true,
+    },
+  };
+};
