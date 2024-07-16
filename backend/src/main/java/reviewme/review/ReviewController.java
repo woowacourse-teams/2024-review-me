5c11f8c3a8d02518427c300ab477b7700f51a74a
Kimprodp <145949635+Kimprodp@users.noreply.github.com>
2024-07-16 12:44:30 +0900

    fix: 누락된 리뷰 작성 매핑 URL 추가 (#17)

--- backend/src/main/java/reviewme/review/ReviewController.java
+++ backend/src/main/java/reviewme/review/ReviewController.java
@@ -2,6 +2,7 @@ package reviewme.review;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,11 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @PostMapping("/reviews")
     public ResponseEntity<Void> createReview(@RequestBody CreateReviewRequest request) {
-        Long review = reviewService.createReview(request);
+        Long id = reviewService.createReview(request);
         return ResponseEntity
-                .created(URI.create("/reviews/" + review))
+                .created(URI.create("/reviews/" + id))
                 .build();
     }
 }
