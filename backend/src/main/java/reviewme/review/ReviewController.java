b468b59131fc54f3c423c78acce54e3f24a42132
donghoony <aru0504@naver.com>
2024-07-16 13:08:45 +0900

    style: 개행 수정

--- backend/src/main/java/reviewme/review/ReviewController.java
+++ backend/src/main/java/reviewme/review/ReviewController.java
@@ -1,13 +1,12 @@
 package reviewme.review;
 
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,16 +16,12 @@ public class ReviewController {
 
     public ResponseEntity<Void> createReview(@RequestBody CreateReviewRequest request) {
         Long review = reviewService.createReview(request);
-        return ResponseEntity
-                .created(URI.create("/reviews/" + review))
-                .build();
+        return ResponseEntity.created(URI.create("/reviews/" + review)).build();
     }
 
     @GetMapping("/reviews/{id}")
     public ResponseEntity<ReviewResponse> findReview(@PathVariable long id) {
         ReviewResponse response = reviewService.findReview(id);
-
-        return ResponseEntity
-                .ok(response);
+        return ResponseEntity.ok(response);
     }
 }
