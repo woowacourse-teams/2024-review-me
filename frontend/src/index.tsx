330d6170837ade73b4542853cc78921097e47bbc
badahertz52 <badahertz52@gmail.com>
2024-07-16 13:40:19 +0900

    feat: react-router-dom, react-router 설치 및 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reset from './styles/reset';
 import globalStyles from './styles/globalStyles';
+import ReviewWriting from './pages/ReviewWriting';
+import DetailedReviewPage from './pages/DetailedReviewPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'user',
+        element: <div>user</div>,
+      },
+
+      {
+        path: 'user/review-writing',
+        element: <ReviewWriting />,
+      },
+      {
+        path: 'user/detailed-review',
+        element: <DetailedReviewPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -14,5 +39,6 @@ root.render(
         ${reset()} ${globalStyles}
       `}
     />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
