267266b9275e2c4321b55bc18b0189180dd73ec3
donghoony <aru0504@naver.com>
2024-07-16 13:13:16 +0900

    fix: PostMapping 수정

--- backend/src/main/java/reviewme/review/ReviewController.java
+++ backend/src/main/java/reviewme/review/ReviewController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,10 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @PostMapping("/reviews")
     public ResponseEntity<Void> createReview(@RequestBody CreateReviewRequest request) {
-        Long review = reviewService.createReview(request);
-        return ResponseEntity.created(URI.create("/reviews/" + review)).build();
+        long id = reviewService.createReview(request);
+        return ResponseEntity.created(URI.create("/reviews/" + id)).build();
     }
 
     @GetMapping("/reviews/{id}")
