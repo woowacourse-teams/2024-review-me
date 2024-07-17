7927468cd5cbac44b7b9f77050441a1dfa56f2ba
hyeonjilee <skylar12200@gmail.com>
2024-07-17 16:17:53 +0900

    docs: 컨트롤러에 swagger 어노테이션 추가

--- backend/src/main/java/reviewme/keyword/contoller/KeywordController.java
+++ backend/src/main/java/reviewme/keyword/contoller/KeywordController.java
@@ -1,5 +1,7 @@
 package reviewme.keyword.contoller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import reviewme.keyword.dto.response.KeywordsResponse;
 import reviewme.keyword.service.KeywordService;
 
+@Tag(name = "키워드 관리")
 @RestController
 @RequiredArgsConstructor
 public class KeywordController {
 
     private final KeywordService keywordService;
 
+    @Operation(summary = "모든 키워드 조회", description = "모든 키워드를 조회한다.")
     @GetMapping("/keywords")
     public ResponseEntity<KeywordsResponse> findAllKeywords() {
         KeywordsResponse response = keywordService.findAllKeywords();
--- backend/src/main/java/reviewme/member/controller/ReviewerGroupController.java
+++ backend/src/main/java/reviewme/member/controller/ReviewerGroupController.java
@@ -1,5 +1,7 @@
 package reviewme.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import reviewme.member.dto.response.ReviewerGroupResponse;
 import reviewme.member.service.ReviewerGroupService;
 
+@Tag(name = "리뷰어 그룹 관리")
 @RestController
 @RequiredArgsConstructor
 public class ReviewerGroupController {
 
     private final ReviewerGroupService reviewerGroupService;
 
+    @Operation(summary = "리뷰어 그룹 조회", description = "리뷰어 그룹을 조회한다.")
     @GetMapping("/reviewer-groups/{id}")
     public ResponseEntity<ReviewerGroupResponse> findReviewerGroup(@PathVariable long id) {
         ReviewerGroupResponse response = reviewerGroupService.findReviewerGroup(id);
