1e824d8aae770bedc395b7c5ff4c1106c6432849
nayonsoso <panda0329@naver.com>
2024-07-18 16:38:24 +0900

    feat: FieldError 핸들러 추가

--- backend/src/main/java/reviewme/global/GlobalExceptionHandler.java
+++ backend/src/main/java/reviewme/global/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package reviewme.global;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reviewme.global.exception.FieldErrorResponse;
 import reviewme.global.exception.NotFoundException;
 
 @RestControllerAdvice
@@ -12,5 +16,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<FieldErrorResponse> fieldErrors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> new FieldErrorResponse(
+                        fieldError.getField(),
+                        fieldError.getRejectedValue(),
+                        fieldError.getDefaultMessage()))
+                .toList();
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        Map<String, Object> properties = Map.of("fieldErrors", fieldErrors);
+        problemDetail.setProperties(properties);
+        return problemDetail;
     }
 }
