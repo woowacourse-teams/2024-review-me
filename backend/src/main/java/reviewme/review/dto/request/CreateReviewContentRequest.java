0705980840a605bb8388312a196a58e0ff6fd8b8
nayonsoso <panda0329@naver.com>
2024-07-18 16:40:18 +0900

    refactor: 문자열 공백 검증을 위해 NotBlank 사용

--- backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
@@ -1,6 +1,7 @@
 package reviewme.review.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "리뷰 내용 등록 요청")
@@ -9,11 +10,11 @@ public record CreateReviewContentRequest(
         @Schema(description = "리뷰 항목 순서")
         Long order,
 
-        @NotNull(message = "질문을 입력해주세요.")
+        @NotBlank(message = "질문을 입력해주세요.")
         @Schema(description = "리뷰 문항")
         String question,
 
-        @NotNull(message = "답변을 입력해주세요.")
+        @NotBlank(message = "답변을 입력해주세요.")
         @Schema(description = "리뷰 문항에 대한 답변")
         String answer
 ) {
