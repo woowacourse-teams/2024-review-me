7c4f7dbf75de760c22abea4e8b2ec202df8b9b0f
hyeonjilee <skylar12200@gmail.com>
2024-07-18 19:21:36 +0900

    refactor: Review 엔티티의 reviewer와의 연관관계 ManyToOne으로 변경

--- backend/src/main/java/reviewme/review/domain/Review.java
+++ backend/src/main/java/reviewme/review/domain/Review.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,7 +23,7 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "reviewer_id", nullable = false)
     private Member reviewer;
 
