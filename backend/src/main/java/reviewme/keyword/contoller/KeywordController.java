4157bc4b81df6098c30db3b2740b15c53095284b
KIMGYUTAE <kiimwf0819@gmail.com>
2024-07-18 16:41:05 +0900

    refactor: 컨트롤러 내 swagger 어노테이션 분리

--- backend/src/main/java/reviewme/keyword/contoller/KeywordController.java
+++ backend/src/main/java/reviewme/keyword/contoller/KeywordController.java
@@ -1,7 +1,5 @@
 package reviewme.keyword.contoller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import reviewme.keyword.dto.response.KeywordsResponse;
 import reviewme.keyword.service.KeywordService;
 
-@Tag(name = "키워드 관리")
 @RestController
 @RequiredArgsConstructor
-public class KeywordController {
+public class KeywordController implements KeywordApi {
 
     private final KeywordService keywordService;
 
-    @Operation(summary = "모든 키워드 조회", description = "모든 키워드를 조회한다.")
     @GetMapping("/keywords")
     public ResponseEntity<KeywordsResponse> findAllKeywords() {
         KeywordsResponse response = keywordService.findAllKeywords();
--- backend/src/main/java/reviewme/review/controller/ReviewController.java
+++ backend/src/main/java/reviewme/review/controller/ReviewController.java
@@ -1,8 +1,5 @@
 package reviewme.review.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +12,18 @@ import reviewme.review.dto.request.CreateReviewRequest;
 import reviewme.review.dto.response.ReviewResponse;
 import reviewme.review.service.ReviewService;
 
-@Tag(name = "리뷰 관리")
 @RestController
 @RequiredArgsConstructor
-public class ReviewController {
+public class ReviewController implements ReviewApi {
 
     private final ReviewService reviewService;
 
-    @Operation(summary = "리뷰 등록", description = "리뷰 작성 정보를 받아 리뷰를 등록한다.")
-    @ApiResponse(responseCode = "201")
     @PostMapping("/reviews")
     public ResponseEntity<Void> createReview(@RequestBody CreateReviewRequest request) {
         long id = reviewService.createReview(request);
         return ResponseEntity.created(URI.create("/reviews/" + id)).build();
     }
 
-    @Operation(summary = "리뷰 조회", description = "단일 리뷰를 조회한다.")
     @GetMapping("/reviews/{id}")
     public ResponseEntity<ReviewResponse> findReview(@PathVariable long id) {
         ReviewResponse response = reviewService.findReview(id);
