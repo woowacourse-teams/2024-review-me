680c6a68dba291fb4497b5875beee0644898b0d7
KIMGYUTAE <kiimwf0819@gmail.com>
2024-07-18 11:20:02 +0900

    refactor: SwaggerConfig info 설정 파일 변수 참조하도록 변경

--- backend/src/main/java/reviewme/config/SwaggerConfig.java
+++ backend/src/main/java/reviewme/config/SwaggerConfig.java
@@ -2,18 +2,28 @@ package reviewme.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${api.info.title}")
+    private String title;
+
+    @Value("${api.info.description}")
+    private String description;
+
+    @Value("${api.info.version}")
+    private String version;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("리뷰미 API")
-                        .description("이 문서는 리뷰미 API 구현 방법을 소개합니다.")
-                        .version("1.0.0"));
+                        .title(title)
+                        .description(description)
+                        .version(version));
     }
 }
