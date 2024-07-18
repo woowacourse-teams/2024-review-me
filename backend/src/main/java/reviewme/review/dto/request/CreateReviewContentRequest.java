6c3b4973140386faaf753f1f1025bc5158f0b902
nayonsoso <panda0329@naver.com>
2024-07-18 16:34:39 +0900

    feat: dto에 jakarta validation 어노테이션 추가

--- backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
@@ -1,7 +1,15 @@
 package reviewme.review.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateReviewContentRequest(
+        @NotNull(message = "리뷰 항목 순서를 입력해주세요.")
         Long order,
+
+        @NotNull(message = "질문을 입력해주세요.")
         String question,
-        String answer) {
+
+        @NotNull(message = "답변을 입력해주세요.")
+        String answer
+) {
 }
--- backend/src/main/java/reviewme/review/dto/request/CreateReviewRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewRequest.java
@@ -1,10 +1,24 @@
 package reviewme.review.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record CreateReviewRequest(
+        @NotNull(message = "리뷰어 아이디를 입력해주세요.")
         Long reviewerId,
+
+        @NotNull(message = "리뷰어 그룹 아이디를 입력해주세요.")
         Long reviewerGroupId,
+
+        @Valid
+        @NotNull(message = "리뷰 내용을 입력해주세요.")
+        @Size(min = 1, message = "리뷰 내용을 하나 이상 입력해주세요.")
         List<CreateReviewContentRequest> contents,
-        List<Long> selectedKeywordIds) {
+
+        @NotNull(message = "키워드를 입력해주세요.")
+        @Size(min = 1, max = 5, message = "키워드를 최소 1개, 최대 5개 입력해주세요.")
+        List<Long> selectedKeywordIds
+) {
 }
