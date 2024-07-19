c4c481e70684125d4c2c14d5e2f5a12186498f71
hyeonjilee <skylar12200@gmail.com>
2024-07-19 10:47:34 +0900

    refactor: 누락된 @Column 추가

--- backend/src/main/java/reviewme/member/domain/GithubReviewerGroup.java
+++ backend/src/main/java/reviewme/member/domain/GithubReviewerGroup.java
@@ -1,5 +1,6 @@
 package reviewme.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class GithubReviewerGroup {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "github_id", nullable = false)
     private String githubId;
 
     @ManyToOne
