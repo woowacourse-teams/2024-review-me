799d07a0c8b1beee81ed85cf9f56a8c5e68f29f5
nayonsoso <panda0329@naver.com>
2024-07-19 09:09:20 +0900

    refactor: 사이즈 검증문 제거

--- backend/src/main/java/reviewme/review/dto/request/CreateReviewRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewRequest.java
@@ -2,7 +2,6 @@ package reviewme.review.dto.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record CreateReviewRequest(
@@ -14,11 +13,9 @@ public record CreateReviewRequest(
 
         @Valid
         @NotNull(message = "리뷰 내용을 입력해주세요.")
-        @Size(min = 1, message = "리뷰 내용을 하나 이상 입력해주세요.")
         List<CreateReviewContentRequest> contents,
 
         @NotNull(message = "키워드를 입력해주세요.")
-        @Size(min = 1, max = 5, message = "키워드를 최소 1개, 최대 5개 입력해주세요.")
         List<Long> selectedKeywordIds
 ) {
 }
