7a18dc4007f9c54b255298a556e25485badda11c
nayonsoso <panda0329@naver.com>
2024-07-19 11:43:58 +0900

    fix: conflict 해결

--- backend/src/main/java/reviewme/keyword/dto/response/KeywordsResponse.java
+++ backend/src/main/java/reviewme/keyword/dto/response/KeywordsResponse.java
@@ -1,6 +1,12 @@
 package reviewme.keyword.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-public record KeywordsResponse(List<KeywordResponse> keywords) {
+@Schema(description = "키워드 목록 응답")
+public record KeywordsResponse(
+
+        @Schema(description = "키워드 목록")
+        List<KeywordResponse> keywords
+) {
 }
--- backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
@@ -1,16 +1,21 @@
 package reviewme.review.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "리뷰 내용 등록 요청")
 public record CreateReviewContentRequest(
+        @Schema(description = "리뷰 항목 순서")
         @NotNull(message = "리뷰 항목 순서를 입력해주세요.")
         Long order,
 
+        @Schema(description = "리뷰 문항")
         @NotBlank(message = "질문을 입력해주세요.")
         String question,
 
+        @Schema(description = "리뷰 문항에 대한 답변")
         @NotBlank(message = "답변을 입력해주세요.")
-        String answer
-) {
+        String answer) {
 }
