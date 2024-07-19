b5384c18222a0c48af83d2a25cb7e5129f5f7740
badahertz52 <badahertz52@gmail.com>
2024-07-19 16:16:50 +0900

    refactor : App에 페이지 레이아웃 적용
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,15 @@
-import { css } from '@emotion/react';
-import React from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router';
 
-import { Header } from './components';
-import Main from './components/Main';
-import Sidebar from './components/Sidebar';
+import { Main, PageLayout, Sidebar, Topbar } from './components';
 
 const App = () => {
   return (
-    <div
-      css={css`
-        display: flex;
-      `}
-    >
-      <Sidebar />
-      <div>
-        <Header />
-        <Main>
-          <Outlet />
-        </Main>
-      </div>
-    </div>
+    <PageLayout>
+      <Main>
+        <Outlet />
+      </Main>
+    </PageLayout>
   );
 };
 
