5a798ef11613620d06985f65f3c1cabc13836c7a
badahertz52 <badahertz52@gmail.com>
2024-07-19 16:03:41 +0900

    design : theme 추가
    - 추가된 theme : breakpoints, sidebarWidth
    
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- frontend/src/types/emotion.ts
+++ frontend/src/types/emotion.ts
@@ -1,16 +1,20 @@
 import '@emotion/react';
-import theme, { colors, fontSize, fontWeight, zIndex } from '../styles/theme';
+import { colors, fontSize, fontWeight, zIndex, breakpoints, sidebarWidth } from '../styles/theme';
 
 export type colorType = typeof colors;
 export type zIndexType = typeof zIndex;
 export type fontSizeType = typeof fontSize;
 export type fontWeightType = typeof fontWeight;
+export type breakpoints = typeof breakpoints;
+export type sidebarWidth = typeof sidebarWidth;
 
 type ThemeType = {
   fontSize: fontSizeType;
   fontWeight: fontWeightType;
   colors: colorType;
   zIndex: zIndexType;
+  breakpoints: breakpoints;
+  sidebarWidth: sidebarWidth;
 };
 
 declare module '@emotion/react' {
