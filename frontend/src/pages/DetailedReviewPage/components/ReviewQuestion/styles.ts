a6f57cef7f65a0192180b80e77db45492609a110
badahertz52 <badahertz52@gmail.com>
2024-07-21 10:52:30 +0900

    [FE] fix : stylelint 적용 오류 수정  (#66)
    * fix: stylelint를 통한 css 속성 정렬 기능 오류 수정
    
    - stylelint 버전16과 충돌되는 플러그인 삭제 : stylelint-config-prettier, stylelint-prettier
    - css 정렬에 필요하지 않은 플러그인 삭제 : stylelint-config-standard, stylelint-config-styled-componented, stylelint-webpack-plugin
    - 추가로 설치한 플러그인: postcss-syntax, @stylelint/postcss-css-in-js
    - stylelint 적용 script 추가
    - .stylelintrc.json 수정 : css 관련 rule 설정
    
    * refactor: stylelint 적용에 따른 css 속성 정렬

--- frontend/src/pages/DetailedReviewPage/components/ReviewQuestion/styles.ts
+++ frontend/src/pages/DetailedReviewPage/components/ReviewQuestion/styles.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Question = styled.p`
+  margin-bottom: 0.5rem;
   font-size: 1rem;
   font-weight: bold;
-  margin-bottom: 0.5rem;
 `;
--- frontend/src/pages/ReviewWriting/components/KeywordButton/styles.ts
+++ frontend/src/pages/ReviewWriting/components/KeywordButton/styles.ts
@@ -2,18 +2,18 @@ import styled from '@emotion/styled';
 
 export const KeywordButton = styled.button<{ isSelected: boolean }>`
   display: flex;
-  justify-content: flex-start;
   align-items: center;
+  justify-content: flex-start;
 
   width: 30rem;
   height: 5rem;
+  padding: 2rem;
 
-  background-color: ${({ theme, isSelected }) => (isSelected ? '#E6E3F9' : theme.colors.white)};
   color: black;
 
+  background-color: ${({ theme, isSelected }) => (isSelected ? '#E6E3F9' : theme.colors.white)};
   border: 0.2rem solid ${({ theme, isSelected }) => (isSelected ? theme.colors.primary : theme.colors.lightGray)};
   border-radius: 2rem;
-  padding: 2rem;
 
   &:hover {
     background-color: ${({ theme, isSelected }) => (isSelected ? theme.colors.primaryHover : theme.colors.lightGray)};
