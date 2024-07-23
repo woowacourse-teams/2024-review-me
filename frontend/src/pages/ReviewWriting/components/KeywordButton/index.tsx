7ead5a91354ebd5346e70c6920d88aa63c924956
chysis <chysiss@naver.com>
2024-07-24 02:23:50 +0900

    fix: 키워드 버튼을 눌렀을 때 폼이 제출되는 문제 해결

--- frontend/src/pages/ReviewWriting/components/KeywordButton/index.tsx
+++ frontend/src/pages/ReviewWriting/components/KeywordButton/index.tsx
@@ -4,10 +4,21 @@ import * as S from './styles';
 
 interface KeywordButtonProps {
   isSelected: boolean;
+  onClick: () => void;
 }
 
-const KeywordButton = ({ isSelected, children }: PropsWithChildren<KeywordButtonProps>) => {
-  return <S.KeywordButton isSelected={isSelected}>{children}</S.KeywordButton>;
+const KeywordButton = ({ isSelected, children, onClick }: PropsWithChildren<KeywordButtonProps>) => {
+  return (
+    <S.KeywordButton
+      isSelected={isSelected}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {children}
+    </S.KeywordButton>
+  );
 };
 
 export default KeywordButton;
