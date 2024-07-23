c638a6beef45cb55aa50619ea4276231f8e1eeaa
donghoony <aru0504@naver.com>
2024-07-23 17:40:32 +0900

    refactor: 답변 구조 변경, 길이 검증
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: Kimprodp <145949635+kimprodp@users.noreply.github.com>
    Co-authored-by: hyeonjilee <skylar12200@gmail.com>

--- backend/src/main/java/reviewme/review/domain/ReviewContent.java
+++ backend/src/main/java/reviewme/review/domain/ReviewContent.java
@@ -6,11 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import reviewme.review.domain.exception.InvalidAnswerLengthException;
 
 @Entity
 @Table(name = "review_content")
@@ -18,23 +19,29 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ReviewContent {
 
+    private static final int MIN_ANSWER_LENGTH = 20;
+    private static final int MAX_ANSWER_LENGTH = 1_000;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "review_id")
-    private Review review;
-
-    @Column(name = "question", nullable = false)
-    private String question;
+    @OneToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
 
     @Column(name = "answer", nullable = false)
     private String answer;
 
-    public ReviewContent(Review review, String question, String answer) {
-        this.review = review;
+    public ReviewContent(Question question, String answer) {
+        validateAnswerLength(answer);
         this.question = question;
         this.answer = answer;
+    }
+
+    private void validateAnswerLength(String answer) {
+        if (answer.length() < MIN_ANSWER_LENGTH || answer.length() > MAX_ANSWER_LENGTH) {
+            throw new InvalidAnswerLengthException(MIN_ANSWER_LENGTH, MAX_ANSWER_LENGTH);
+        }
     }
 }
