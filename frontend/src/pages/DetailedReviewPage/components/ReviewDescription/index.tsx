eb186cc69494f93ffbace87df9493039826e7b47
badahertz52 <badahertz52@gmail.com>
2024-07-23 16:56:46 +0900

    refactor: formatDate를 utils/date 파일로 이동

--- frontend/src/pages/DetailedReviewPage/components/ReviewDescription/index.tsx
+++ frontend/src/pages/DetailedReviewPage/components/ReviewDescription/index.tsx
@@ -1,4 +1,5 @@
 import LockButton from '../LockButton';
+
 import * as S from './styles';
 
 interface ReviewDescriptionItemProps {
@@ -21,14 +22,7 @@ interface ReviewDescriptionProps {
   isLock: boolean;
 }
 
-const formatDate = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  //const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  return `${year}/${month}/${day}`;
-};
 
 const ReviewDescription = ({ projectName, createdAt, isLock }: ReviewDescriptionProps) => {
   return (
