6fc61045effdefddfcb367b80ef0a092c39e5461
chysis <chysiss@naver.com>
2024-07-24 02:26:10 +0900

    feat: 작성한 리뷰 내용을 부모 컴포넌트의 상태와 동기화하도록 설정

--- frontend/src/pages/ReviewWriting/components/ReviewItem/index.tsx
+++ frontend/src/pages/ReviewWriting/components/ReviewItem/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import * as S from './styles';
 import { REVIEW } from '@/constants/review';
+
+import * as S from './styles';
 
 interface ReviewItemProps {
   question: string;
+  answerValue: string;
+  onWrite: (value: string) => void;
 }
 
-const ReviewItem = ({ question }: ReviewItemProps) => {
+const ReviewItem = ({ question, answerValue, onWrite }: ReviewItemProps) => {
   return (
     <S.ReviewItem>
       <S.ReviewQuestion>{question}</S.ReviewQuestion>
-      <S.ReviewTextarea placeholder={REVIEW.answerMaxLengthMessage} />
+      <S.ReviewTextarea
+        placeholder={REVIEW.answerMaxLengthMessage}
+        value={answerValue}
+        onChange={(e) => onWrite(e.target.value)}
+      />
     </S.ReviewItem>
   );
 };
