fcf85a8a94f518a7d64b9972247dacc10d633469
hyeonjilee <skylar12200@gmail.com>
2024-07-23 10:37:04 +0900

    test: given 데이터 오류 수정

--- backend/src/test/java/reviewme/member/repository/GithubReviewerGroupRepositoryTest.java
+++ backend/src/test/java/reviewme/member/repository/GithubReviewerGroupRepositoryTest.java
@@ -3,6 +3,7 @@ package reviewme.member.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static reviewme.fixture.ReviewerGroupFixture.리뷰_그룹;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,32 +24,42 @@ class GithubReviewerGroupRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    void 깃허브_아이디와_리뷰어_그룹이_모두_일치하는_깃허브_리뷰어_그룹이_있는_경우를_확인한다() {
+    void 깃허브_아이디와_리뷰어_그룹이_모두_일치하는_깃허브_리뷰어_그룹이_있는_경우를_확인한다() { // given
         // given
-        String githubId = "ted";
-        Member ted = new Member("테드", githubId);
-        memberRepository.save(ted);
-        ReviewerGroup reviewerGroup = 리뷰_그룹.create(ted);
-        reviewerGroupRepository.save(reviewerGroup);
-        githubReviewerGroupRepository.save(new GithubReviewerGroup(githubId, reviewerGroup));
+        String revieweeGithubId = "kirby";
+        Member reviewee = new Member("커비", revieweeGithubId);
+        String reviewerGithubId = "ted";
+        Member reviewer = new Member("테드", reviewerGithubId);
+        memberRepository.saveAll(List.of(reviewee, reviewer));
 
-        // when & then
-        boolean actual = githubReviewerGroupRepository.existsByGithubIdAndReviewerGroup(githubId, reviewerGroup);
+        ReviewerGroup reviewerGroup = 리뷰_그룹.create(reviewee);
+        reviewerGroupRepository.save(reviewerGroup);
+        githubReviewerGroupRepository.save(new GithubReviewerGroup(reviewerGithubId, reviewerGroup));
+
+        // when
+        boolean actual = githubReviewerGroupRepository.existsByGithubIdAndReviewerGroup(reviewerGithubId, reviewerGroup);
+
+        // then
         assertThat(actual).isTrue();
     }
 
     @Test
     void 깃허브_아이디와_리뷰어_그룹이_모두_일치하는_깃허브_리뷰어_그룹이_없는_경우를_확인한다() {
         // given
-        String githubId = "ted";
-        Member ted = new Member("테드", githubId);
-        memberRepository.save(ted);
-        ReviewerGroup reviewerGroup = 리뷰_그룹.create(ted);
-        reviewerGroupRepository.save(reviewerGroup);
-        githubReviewerGroupRepository.save(new GithubReviewerGroup(githubId, reviewerGroup));
+        String revieweeGithubId = "kirby";
+        Member reviewee = new Member("커비", revieweeGithubId);
+        String reviewerGithubId = "ted";
+        Member reviewer = new Member("테드", reviewerGithubId);
+        memberRepository.saveAll(List.of(reviewee, reviewer));
 
-        // when & then
+        ReviewerGroup reviewerGroup = 리뷰_그룹.create(reviewee);
+        reviewerGroupRepository.save(reviewerGroup);
+        githubReviewerGroupRepository.save(new GithubReviewerGroup(reviewerGithubId, reviewerGroup));
+
+        // when
         boolean actual = githubReviewerGroupRepository.existsByGithubIdAndReviewerGroup("aru", reviewerGroup);
+
+        // then
         assertThat(actual).isFalse();
     }
 }
