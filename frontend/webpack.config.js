0cb6fcbe0d71cd7e6f92f3e9ec4a567c9a34f157
ImxYJL <allensain14@gmail.com>
2024-07-24 10:45:05 +0900

    ci: 절대경로 수정 2

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       new Dotenv({
         systemvars: true,
-        path: '/home/runner/work/2024-review-me/2024-review-me/frontend/.env.',
+        path: '/home/runner/work/2024-review-me/frontend/.env.',
         //path: './.env',
       }),
     ],
