163726ebb47ef7f6512125e2b50972e15398d377
soosoo22 <lllllh00@naver.com>
2024-07-24 10:33:12 +0900

    feat: ReviewPreviewListPage 구현 및 모의 데이터 추가
    - `ReviewPreviewListPage` 컴포넌트 구현
      - `SearchSection`을 포함하여 검색 기능을 구현
      - `ReviewPreviewCard`를 사용하여 리뷰 목록을 표시
    - 모의 데이터(`mockReviewPreviews`) 추가
    
    Co-Authored-By: badahertz52 <badahertz52@gmail.com>
    Co-Authored-By: ImxYJL <111052302+ImxYJL@users.noreply.github.com>
    Co-Authored-By: Fe <64690761+chysis@users.noreply.github.com>

---
+++ frontend/src/pages/ReviewPreviewListPage/styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  width: 80%;
+`;
+
+export const ReviewSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7rem;
+`;
