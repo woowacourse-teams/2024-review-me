4a9920708787d207680232fb8a64d41afbfb7f61
chysis <chysiss@naver.com>
2024-07-24 17:56:18 +0900

    refactor: 공통 버튼의 타입별 스타일을 함수로 분리

--- frontend/src/components/common/Button/styles.ts
+++ frontend/src/components/common/Button/styles.ts
@@ -1,7 +1,50 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { ButtonType } from '@/types/styles';
+
+const primaryStyle = (theme: Theme) => css`
+  color: ${theme.colors.white};
+  background-color: ${theme.colors.primary};
+
+  &:hover {
+    background-color: ${theme.colors.primaryHover};
+    border: 0.1rem solid ${theme.colors.primaryHover};
+  }
+`;
+
+const secondaryStyle = (theme: Theme) => css`
+  color: ${theme.colors.primary};
+  background-color: ${theme.colors.white};
+
+  &:hover {
+    background-color: ${theme.colors.lightPurple};
+  }
+`;
+
+const disabledStyle = (theme: Theme) => css`
+  pointer-events: none;
+  color: ${theme.colors.disabledText};
+  background-color: ${theme.colors.disabled};
+  border-color: ${theme.colors.disabled};
+
+  &:hover {
+    background-color: ${theme.colors.disabled};
+  }
+`;
+
+const getButtonStyle = (buttonType: ButtonType, theme: Theme) => {
+  switch (buttonType) {
+    case 'primary':
+      return primaryStyle(theme);
+    case 'secondary':
+      return secondaryStyle(theme);
+    case 'disabled':
+      return disabledStyle(theme);
+    default:
+      return css``;
+  }
+};
 
 export const Button = styled.button<{ buttonType: ButtonType }>`
   display: flex;
@@ -15,39 +58,5 @@ export const Button = styled.button<{ buttonType: ButtonType }>`
   border: 0.1rem solid ${({ theme }) => theme.colors.primary};
   border-radius: 0.8rem;
 
-  ${({ theme, buttonType }) => {
-    switch (buttonType) {
-      case 'primary':
-        return `
-          color: ${theme.colors.white};
-          background-color: ${theme.colors.primary};
-
-          &:hover {
-            background-color: ${theme.colors.primaryHover};
-          }
-        `;
-      case 'secondary':
-        return `
-          color: ${theme.colors.primary};
-          background-color: ${theme.colors.white};
-
-          &:hover {
-            background-color: ${theme.colors.lightPurple};
-          }
-        `;
-      case 'disabled':
-        return `
-          pointer-events: none;
-          color: ${theme.colors.disabledText};
-          background-color: ${theme.colors.disabled};
-          border-color: ${theme.colors.disabled};
-
-          &:hover {
-            background-color: ${theme.colors.disabled};
-          }
-        `;
-      default:
-        return css``;
-    }
-  }}
+  ${({ buttonType, theme }) => getButtonStyle(buttonType, theme)};
 `;
