1c2ce1f9ebbceb928b313efee31569b8be7c0c60
KIMGYUTAE <kiimwf0819@gmail.com>
2024-07-24 23:08:21 +0900

    feat: QuestionRepository 추가

---
+++ backend/src/main/java/reviewme/review/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package reviewme.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import reviewme.review.domain.Question;
+import reviewme.review.exception.QuestionNotFoundException;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    default Question getQuestionById(Long id) {
+        return findById(id).orElseThrow(QuestionNotFoundException::new);
+    }
+}
