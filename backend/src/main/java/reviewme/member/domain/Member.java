70de3cfd73916671fac9989fd53ebd2044b97527
donghoony <aru0504@naver.com>
2024-07-25 11:38:41 +0900

    refactor: 회원 도메인에서의 비교를 GithubId로 진행하도록 수정

--- backend/src/main/java/reviewme/member/domain/Member.java
+++ backend/src/main/java/reviewme/member/domain/Member.java
@@ -7,14 +7,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "member")
-@EqualsAndHashCode(of = "id")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Member {
@@ -29,13 +28,30 @@ public class Member {
     @Embedded
     private GithubId githubId;
 
-    public Member(Long id, String name, GithubId githubId) {
-        this.id = id;
+    public Member(String name, long githubId) {
         this.name = name;
-        this.githubId = githubId;
+        this.githubId = new GithubId(githubId);
     }
 
-    public Member(String name, long githubId) {
-        this(null, name, new GithubId(githubId));
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Member member)) {
+            return false;
+        }
+        if (id == null) {
+            return Objects.equals(githubId, member.githubId);
+        }
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return Objects.hash(githubId);
+        }
+        return Objects.hash(id);
     }
 }
