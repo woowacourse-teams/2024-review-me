3a229bbc0c2769e3d755f1248391c44e52304223
nayonsoso <panda0329@naver.com>
2024-07-25 16:14:27 +0900

    docs: 스웨거 데코레이션 적용

--- backend/src/main/java/reviewme/review/controller/ReviewApi.java
+++ backend/src/main/java/reviewme/review/controller/ReviewApi.java
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import reviewme.review.dto.request.CreateReviewRequest;
+import reviewme.review.dto.response.ReceivedReviewsResponse;
 import reviewme.review.dto.response.ReviewDetailResponse;
 
 @Tag(name = "리뷰 관리")
@@ -23,5 +25,13 @@ public interface ReviewApi {
             summary = "리뷰 조회",
             description = "단일 리뷰를 조회한다."
     )
-    ResponseEntity<ReviewDetailResponse> findReview(@PathVariable long id);
+    ResponseEntity<ReviewDetailResponse> findReview(@PathVariable long id, @RequestParam long memberId);
+
+    @Operation(
+            summary = "내가 받은 리뷰 조회",
+            description = "내가 받은 리뷰를 조회한다. (로그인을 구현하지 않은 지금 시점에서는 memberId로 로그인했다고 가정한다.)"
+    )
+    ResponseEntity<ReceivedReviewsResponse> findMyReceivedReview(@RequestParam long memberId,
+                                                                 @RequestParam Long lastReviewId,
+                                                                 @RequestParam(defaultValue = "10") int size);
 }
