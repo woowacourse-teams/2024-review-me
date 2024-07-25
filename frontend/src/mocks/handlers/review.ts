9cf2d03df6b809deda6774268da831ab42379958
chysis <chysiss@naver.com>
2024-07-25 15:29:54 +0900

    feat: MSW 핸들러 및 mock data 추가

--- frontend/src/mocks/handlers/review.ts
+++ frontend/src/mocks/handlers/review.ts
@@ -3,12 +3,18 @@ import { http, HttpResponse } from 'msw';
 import endPoint from '@/apis/endpoints';
 
 import { DETAILED_REVIEW_MOCK_DATA } from '../mockData/detailedReviewMockData';
+import { REVIEW_WRITING_DATA } from '../mockData/reviewWritingData';
 
 const getDetailedReview = () =>
   http.get(endPoint.gettingDetailedReview(0), async ({ request }) => {
     return HttpResponse.json(DETAILED_REVIEW_MOCK_DATA);
   });
 
-const reviewHandler = [getDetailedReview()];
+const getDataToWriteReview = () =>
+  http.get(endPoint.gettingDataToWriteReview(10), async ({ request }) => {
+    return HttpResponse.json(REVIEW_WRITING_DATA);
+  });
+
+const reviewHandler = [getDetailedReview(), getDataToWriteReview()];
 
 export default reviewHandler;
