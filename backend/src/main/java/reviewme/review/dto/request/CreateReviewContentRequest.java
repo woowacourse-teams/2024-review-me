eb999670db9b0a19a3299ad1e93307fe1aec10c2
donghoony <aru0504@naver.com>
2024-07-25 18:26:33 +0900

    fix: Long notblank -> notnull

--- backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
+++ backend/src/main/java/reviewme/review/dto/request/CreateReviewContentRequest.java
@@ -2,12 +2,13 @@ package reviewme.review.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "리뷰 내용 등록 요청")
 public record CreateReviewContentRequest(
 
         @Schema(description = "리뷰 문항")
-        @NotBlank(message = "질문을 입력해주세요.")
+        @NotNull(message = "질문을 입력해주세요.")
         Long questionId,
 
         @Schema(description = "리뷰 문항에 대한 답변")
