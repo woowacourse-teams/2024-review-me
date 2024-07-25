0745549c201b2927fdde0d5cb08580957b5d6080
Fe <64690761+chysis@users.noreply.github.com>
2024-07-25 17:42:12 +0900

    fix: 잘못 정의된 endpoint 수정 (#118)

--- frontend/src/apis/endpoints.ts
+++ frontend/src/apis/endpoints.ts
@@ -1,8 +1,8 @@
 const endPoint = {
   postingReview: `${process.env.API_BASE_URL}/reviews`,
   gettingDetailedReview: (reviewId: number, memberId: number) =>
-    `${process.env.API_BASE_URL}reviews/${reviewId}?memberId=${memberId}`,
-  gettingDataToWriteReview: (reviewerGroupId: number) => `${process.env.API_BASE_URL}reviews/write/${reviewerGroupId}`,
+    `${process.env.API_BASE_URL}/reviews/${reviewId}?memberId=${memberId}`,
+  gettingDataToWriteReview: (reviewerGroupId: number) => `${process.env.API_BASE_URL}/reviews/write/${reviewerGroupId}`,
   gettingReviewList: (revieweeId: number, lastReviewId: number, memberId: number) =>
     `${process.env.API_BASE_URL}/reviews?revieweeId=${revieweeId}&lastReviewId=${lastReviewId}&memberId=${memberId}`,
 };
