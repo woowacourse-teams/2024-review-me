065f7524e836b02de2cf7a31912037d03e503bc7
donghoony <aru0504@naver.com>
2024-07-25 17:25:45 +0900

    refactor: API Docs 반영

--- backend/src/main/java/reviewme/review/controller/ReviewApi.java
+++ backend/src/main/java/reviewme/review/controller/ReviewApi.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import reviewme.review.dto.request.CreateReviewRequest;
 import reviewme.review.dto.response.ReceivedReviewsResponse;
+import reviewme.review.dto.response.ReviewCreationResponse;
 import reviewme.review.dto.response.ReviewDetailResponse;
 
 @Tag(name = "리뷰 관리")
@@ -32,7 +33,12 @@ public interface ReviewApi {
             description = "내가 받은 리뷰를 조회한다. (로그인을 구현하지 않은 지금 시점에서는 memberId로 로그인했다고 가정한다.)"
     )
     ResponseEntity<ReceivedReviewsResponse> findMyReceivedReview(@RequestParam long memberId,
-                                                                 @RequestParam Long lastReviewId,
+                                                                 @RequestParam(required = false) Long lastReviewId,
                                                                  @RequestParam(defaultValue = "10") int size);
 
+    @Operation(
+            summary = "리뷰 작성 정보 조회",
+            description = "리뷰 생성 시 필요한 정보를 조회한다."
+    )
+    ResponseEntity<ReviewCreationResponse> findReviewCreationSetup(@RequestParam long reviewerGroupId);
 }
