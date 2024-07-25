f9daee130c8613a0cd1864f60378d00e4acbb161
badahertz52 <badahertz52@gmail.com>
2024-07-25 17:54:34 +0900

    fix: DetailedReviewPage 복구 및 데이터 타입 변경에 따른 수정

--- frontend/src/pages/DetailedReviewPage/index.tsx
+++ frontend/src/pages/DetailedReviewPage/index.tsx
@@ -1,29 +1,30 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
+
+import { ReviewComment } from '@/components';
 import { DetailReviewData } from '@/types';
 
 import { getDetailedReviewApi } from '../../apis/review';
 
+import KeywordSection from './components/KeywordSection';
 import ReviewDescription from './components/ReviewDescription';
-import ReviewViewSection from './components/ReviewViewSection';
+import ReviewSection from './components/ReviewSection/index';
+import * as S from './styles';
 
-const ANSWER =
-  '림순의 바람은 그윽한 산들바람처럼 잔잔하게 흘러갔습니다. \n 눈부신 햇살이 그의 어깨를 감싸며, 푸른 하늘 아래 펼쳐진 들판을 바라보았습니다.\n 그의 마음은 자연의 아름다움 속에서 평온을 찾았고, 그 순간마다 삶의 소중함을 느꼈습니다.\n 그는 늘 그러한 순간들을 기억하며, 미래의 나날들을 기대했습니다. \n 바람은 여전히 그를 감싸며, 그의 마음 속 깊은 곳에 있는 꿈과 희망을 불러일으켰습니다.\n 림순은 미소 지으며 앞으로 나아갔습니다.림순의 바람은 그윽한 산들바람처럼 잔잔하게 흘러갔습니다. \n 눈부신 햇살이 그의 어깨를 감싸며, 푸른 하늘 아래 펼쳐진 들판을 바라보았습니다.\n 그의 마음은 자연의 아름다움 속에서 평온을 찾았고, 그 순간마다 삶의 소중함을 느꼈습니다.\n 그는 늘 그러한 순간들을 기억하며, 미래의 나날들을 기대했습니다. ';
-
-
-const COMMENT = 'VITE 쓰고 싶다.';
-
-const DetailedReviewPage = ({}) => {
+const DetailedReviewPage = () => {
   const { id } = useParams<{ id: string }>();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const memberId = queryParams.get('memberId');
 
-  const [detailReview, setDetailReview] = useState<DetailReviewData>(MOCK_DATA);
+  const [detailedReview, setDetailReview] = useState<DetailReviewData>();
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
 
   useEffect(() => {
     const fetchReview = async () => {
       try {
-        const result = await getDetailedReviewApi({ reviewId: Number(id), memberId: 4 });
+        const result = await getDetailedReviewApi({ reviewId: Number(id), memberId: Number(memberId) });
 
         setDetailReview(result);
         setErrorMessage('');
@@ -46,15 +47,15 @@ const DetailedReviewPage = ({}) => {
     <S.DetailedReviewPage>
       <ReviewDescription
         projectName={detailedReview.reviewerGroup.name}
-        date={new Date(detailedReview.reviewerGroup.deadline)}
+        date={new Date(detailedReview.createdAt)}
         isPublic={true}
         handleClickToggleButton={() => console.log('click toggle ')}
       />
-      <ReviewComment comment={COMMENT} />
-      {detailedReview.contents.map((item, index) => (
+      <ReviewComment comment={detailedReview.reviewerGroup.description} />
+      {detailedReview.reviews.map((item, index) => (
         <ReviewSection question={item.question} answer={item.answer} key={index} index={index} />
       ))}
-      <KeywordSection keywords={detailedReview.keywords} index={detailedReview.contents.length} />
+      <KeywordSection keywords={detailedReview.keywords} index={detailedReview.reviews.length} />
     </S.DetailedReviewPage>
   );
 };
