9e31285d9c3e2b9b4b0c84308f4acfa6ea9adc19
badahertz52 <badahertz52@gmail.com>
2024-07-27 19:54:01 +0900

    feat: Outlet에 QueryErrorResetBoundary,ErrorBoundary, Suspense 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import { Outlet } from 'react-router';
 
 import { Main, PageLayout, Sidebar, Topbar, SideModal } from './components';
 import { useSidebar } from './hooks';
+
+// 에러가 발생했을 때 보여줄 Fallback 컴포넌트
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>오류:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>재시도</button>
+    </div>
+  );
+}
 
 const App = () => {
   const { isSidebarHidden, isSidebarModalOpen, closeSidebar, openSidebar } = useSidebar();
@@ -15,7 +29,15 @@ const App = () => {
       )}
       <Topbar openSidebar={openSidebar} />
       <Main>
-        <Outlet />
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+              <Suspense fallback={<div>로딩 중....</div>}>
+                <Outlet />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </Main>
     </PageLayout>
   );
