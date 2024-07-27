54d927cbf118d466cced35964e49f2659805ff96
badahertz52 <badahertz52@gmail.com>
2024-07-27 23:58:20 +0900

    refactor: 리뷰 상세 페이지 컴포넌트 속에서만 사용하는 상수들 상수화

--- frontend/src/pages/DetailedReviewPage/components/ReviewDescription/index.tsx
+++ frontend/src/pages/DetailedReviewPage/components/ReviewDescription/index.tsx
@@ -7,6 +7,7 @@ import LockToggle from '../LockToggle';
 import * as S from './styles';
 
 const PROJECT_IMAGE_SIZE = '6rem';
+const DATE_TITLE = '리뷰 작성일';
 
 interface ReviewDescriptionProps extends Omit<ProjectImgProps, '$size'>, Omit<ReviewDateProps, 'dateTitle'> {
   isPublic: boolean;
@@ -26,7 +27,7 @@ const ReviewDescription = ({
         <ProjectImg thumbnailUrl={thumbnailUrl} projectName={projectName} $size={PROJECT_IMAGE_SIZE} />
         <S.ProjectNameAndDateContainer>
           <S.ProjectName>{projectName}</S.ProjectName>
-          <ReviewDate date={date} dateTitle="리뷰 작성일" />
+          <ReviewDate date={date} dateTitle={DATE_TITLE} />
         </S.ProjectNameAndDateContainer>
       </S.DescriptionSide>
       <S.DescriptionSide>
