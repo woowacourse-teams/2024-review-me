f2e96a73441f668def3448ee4ff61084ed53300a
badahertz52 <badahertz52@gmail.com>
2024-07-27 18:39:03 +0900

    feat : msw에 리뷰 상세페이지 404 오류 추가

--- frontend/src/mocks/handlers/review.ts
+++ frontend/src/mocks/handlers/review.ts
@@ -2,7 +2,11 @@ import { http, HttpResponse } from 'msw';
 
 import endPoint from '@/apis/endpoints';
 
-import { DETAILED_REVIEW_MOCK_DATA, DETAILED_PAGE_MOCK_API_SETTING_VALUES } from '../mockData/detailedReviewMockData';
+import {
+  DETAILED_REVIEW_MOCK_DATA,
+  DETAILED_PAGE_MOCK_API_SETTING_VALUES,
+  DETAILED_PAGE_ERROR_API_VALUES,
+} from '../mockData/detailedReviewMockData';
 import { REVIEW_PREVIEW_LIST } from '../mockData/reviewPreviewList';
 import { REVIEW_WRITING_DATA } from '../mockData/reviewWritingData';
 
@@ -17,6 +21,14 @@ const getDetailedReview = () =>
     },
   );
 
+const getWrongDetailReview = () =>
+  http.get(
+    endPoint.gettingDetailedReview(DETAILED_PAGE_ERROR_API_VALUES.reviewId, DETAILED_PAGE_ERROR_API_VALUES.memberId),
+    async () => {
+      return HttpResponse.json({ error: '잘못된 상세리뷰 요청' }, { status: 404 });
+    },
+  );
+
 const getDataToWriteReview = () =>
   http.get(endPoint.gettingDataToWriteReview(10), async ({ request }) => {
     return HttpResponse.json(REVIEW_WRITING_DATA);
@@ -27,6 +39,6 @@ const getReviewPreviewList = () =>
     return HttpResponse.json(REVIEW_PREVIEW_LIST);
   });
 
-const reviewHandler = [getDetailedReview(), getReviewPreviewList(), getDataToWriteReview()];
+const reviewHandler = [getDetailedReview(), getWrongDetailReview(), getReviewPreviewList(), getDataToWriteReview()];
 
 export default reviewHandler;
