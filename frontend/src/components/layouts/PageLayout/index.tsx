9674ad255a1f2a12db733eb049951ffe9c225e07
chysis <chysiss@naver.com>
2024-07-30 17:59:19 +0900

    chore: 각 페이지마다 top button이 보여지도록 설정

--- frontend/src/components/layouts/PageLayout/index.tsx
+++ frontend/src/components/layouts/PageLayout/index.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from 'react';
 
+import { TopButton } from '@/components/common';
+
 import * as S from './styles';
 
 const PageLayout = ({ children }: PropsWithChildren) => {
   return (
     <S.Layout>
       <S.Wrapper>{children}</S.Wrapper>
+      <TopButton />
     </S.Layout>
   );
 };
