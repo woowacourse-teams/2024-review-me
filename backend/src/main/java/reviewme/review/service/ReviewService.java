7600ecfe11243b9465002c7045b62b5bdf79e9ea
hyeonjilee <skylar12200@gmail.com>
2024-07-30 20:20:04 +0900

    refactor: 리뷰 저장 기능 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: hyeonjilee <skylar12200@gmail.com>

--- backend/src/main/java/reviewme/review/service/ReviewService.java
+++ backend/src/main/java/reviewme/review/service/ReviewService.java
@@ -5,31 +5,17 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reviewme.keyword.domain.Keyword;
-import reviewme.keyword.dto.response.KeywordResponse;
-import reviewme.keyword.repository.KeywordRepository;
-import reviewme.keyword.service.KeywordService;
-import reviewme.member.dto.response.ReviewCreationReviewerGroupResponse;
-import reviewme.member.repository.MemberRepository;
-import reviewme.member.repository.ReviewerGroupRepository;
-import reviewme.member.service.ReviewerGroupService;
-import reviewme.question.domain.Question;
+import reviewme.keyword.domain.exception.DuplicateKeywordException;
+import reviewme.keyword.domain.exception.KeywordLimitExceedException;
 import reviewme.review.domain.Review;
 import reviewme.review.domain.ReviewContent;
+import reviewme.review.domain.ReviewKeyword;
 import reviewme.review.dto.request.CreateReviewRequest;
-import reviewme.review.dto.response.QuestionResponse;
-import reviewme.review.dto.response.ReceivedReviewKeywordsResponse;
-import reviewme.review.dto.response.ReceivedReviewResponse;
-import reviewme.review.dto.response.ReceivedReviewReviewerGroupResponse;
-import reviewme.review.dto.response.ReceivedReviewsResponse;
-import reviewme.review.dto.response.ReviewCreationResponse;
-import reviewme.review.dto.response.ReviewDetailResponse;
-import reviewme.review.dto.response.ReviewDetailReviewContentResponse;
-import reviewme.review.dto.response.ReviewDetailReviewerGroupResponse;
-import reviewme.review.exception.ReviewUnAuthorizedException;
-import reviewme.review.repository.QuestionRepository;
 import reviewme.review.repository.ReviewContentRepository;
+import reviewme.review.repository.ReviewKeywordRepository;
 import reviewme.review.repository.ReviewRepository;
+import reviewme.reviewgroup.domain.ReviewGroup;
+import reviewme.reviewgroup.repository.ReviewGroupRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -42,8 +28,7 @@ public class ReviewService {
     private final MemberRepository memberRepository;
     private final ReviewerGroupRepository reviewerGroupRepository;
     private final ReviewContentRepository reviewContentRepository;
-    private final QuestionRepository questionRepository;
-    private final KeywordRepository keywordRepository;
+    private final ReviewGroupRepository reviewGroupRepository;
 
     @Transactional
     public Long createReview(CreateReviewRequest request) {
@@ -68,102 +53,42 @@ public class ReviewService {
                     reviewContentRepository.save(reviewContent);
                 });
 
+        Review savedReview = saveReview(request);
         return savedReview.getId();
     }
 
-    @Transactional(readOnly = true)
-    public ReviewDetailResponse findReview(long reviewId, long memberId) {
-        Review review = reviewRepository.getReviewById(reviewId);
-        Member member = memberRepository.getMemberById(memberId);
-        if (!review.isForReviewee(member)) {
-            throw new ReviewUnAuthorizedException();
+    private Review saveReview(CreateReviewRequest request) {
+        ReviewGroup reviewGroup = reviewGroupRepository.getReviewGroupByReviewRequestCode(request.reviewRequestCode());
+        List<ReviewContent> reviewContents = request.reviewContents()
+                .stream()
+                .map(r -> new ReviewContent(r.questionId(), r.answer()))
+                .toList();
+        Review review = new Review(reviewGroup.getId(), reviewContents, LocalDateTime.now());
+
+        Review savedReview = reviewRepository.save(review);
+        reviewContentRepository.saveAll(reviewContents);
+        return savedReview;
+    }
+
+    private void saveReviewKeywords(List<Long> selectedKeywordIds, long savedReviewId) {
+        validateKeywords(selectedKeywordIds);
+        List<ReviewKeyword> reviewKeywords = selectedKeywordIds.stream()
+                .map(keyword -> new ReviewKeyword(savedReviewId, keyword))
+                .toList();
+        reviewKeywordRepository.saveAll(reviewKeywords);
+    }
+
+    private void validateKeywords(List<Long> selectedKeywordIds) {
+        int keywordsSize = selectedKeywordIds.size();
+        long distinctCount = selectedKeywordIds.stream()
+                .distinct()
+                .count();
+        if (distinctCount != keywordsSize) {
+            throw new DuplicateKeywordException();
         }
 
-        ReviewerGroup reviewerGroup = review.getReviewerGroup();
-        Keywords keywords = review.getKeywords();
-        List<ReviewContent> reviewContents = reviewContentRepository.findAllByReviewId(reviewId);
-
-        ReviewDetailReviewerGroupResponse reviewerGroupResponse = new ReviewDetailReviewerGroupResponse(
-                reviewerGroup.getId(),
-                reviewerGroup.getGroupName(),
-                reviewerGroup.getDescription(),
-                reviewerGroup.getThumbnailUrl()
-        );
-        List<ReviewDetailReviewContentResponse> reviewContentResponses = reviewContents.stream()
-                .map(content -> new ReviewDetailReviewContentResponse(
-                        content.getQuestion(),
-                        content.getAnswer()
-                ))
-                .toList();
-        List<String> keywordContents = keywords.getKeywordIds()
-                .stream()
-                .map(keywordRepository::getKeywordById)
-                .map(Keyword::getContent)
-                .toList();
-
-        return new ReviewDetailResponse(
-                reviewId,
-                review.getCreatedAt().toLocalDate(),
-                review.isPublic(),
-                reviewerGroupResponse,
-                reviewContentResponses,
-                keywordContents
-        );
-    }
-
-    @Transactional(readOnly = true)
-    public ReviewCreationResponse findReviewCreationSetup(long reviewerGroupId) {
-        ReviewCreationReviewerGroupResponse reviewerGroup = reviewerGroupService.findReviewCreationReviewerGroup(
-                reviewerGroupId);
-        List<QuestionResponse> questions = questionService.findAllQuestions();
-        List<KeywordResponse> keywords = keywordService.findAllKeywords();
-        return new ReviewCreationResponse(reviewerGroup, questions, keywords);
-    }
-
-    @Transactional(readOnly = true)
-    public ReceivedReviewsResponse findMyReceivedReview(long memberId, Long lastReviewId, int size) {
-        List<Review> reviews = reviewRepository.findLimitedReviewsWrittenForReviewee(memberId, lastReviewId, size);
-
-        if (reviews.isEmpty()) {
-            return new ReceivedReviewsResponse(0, 0, List.of());
+        if (keywordsSize < MIN_KEYWORD_COUNT || keywordsSize > MAX_KEYWORD_COUNT) {
+            throw new KeywordLimitExceedException(MIN_KEYWORD_COUNT, MAX_KEYWORD_COUNT);
         }
-
-        return new ReceivedReviewsResponse(
-                reviews.size(),
-                reviews.get(reviews.size() - 1).getId(),
-                reviews.stream()
-                        .map(this::createReceivedReviewResponse)
-                        .toList());
-    }
-
-    private ReceivedReviewResponse createReceivedReviewResponse(Review review) {
-        return new ReceivedReviewResponse(
-                review.getId(),
-                review.isPublic(),
-                review.getCreatedAt().toLocalDate(),
-                createReviewContentPreview(review),
-                new ReceivedReviewReviewerGroupResponse(
-                        review.getReviewerGroup().getId(),
-                        review.getReviewerGroup().getGroupName(),
-                        review.getReviewerGroup().getThumbnailUrl()
-                ),
-                createKeywordResponse(review));
-    }
-
-    private String createReviewContentPreview(Review review) {
-        return reviewContentRepository.findAllByReviewId(review.getId())
-                .get(0)
-                .getAnswerPreview();
-    }
-
-    private List<ReceivedReviewKeywordsResponse> createKeywordResponse(Review review) {
-        return review.getKeywords().getKeywordIds()
-                .stream()
-                .map(keywordRepository::getKeywordById)
-                .map(keyword -> new ReceivedReviewKeywordsResponse(
-                        keyword.getId(),
-                        keyword.getContent()
-                ))
-                .toList();
     }
 }
