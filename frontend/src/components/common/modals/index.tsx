d19c304c51c294c2af6f7a5343e2a820ba072951
chysis <chysiss@naver.com>
2024-08-01 01:57:18 +0900

    chore: AlertModal과 ErrorAlertModal export

--- frontend/src/components/common/modals/index.tsx
+++ frontend/src/components/common/modals/index.tsx
@@ -1,2 +1,4 @@
 export { default as SideModal } from './SideModal';
 export { default as ConfirmModal } from './ConfirmModal';
+export { default as AlertModal } from './AlertModal';
+export { default as ErrorAlertModal } from './ErrorAlertModal';
