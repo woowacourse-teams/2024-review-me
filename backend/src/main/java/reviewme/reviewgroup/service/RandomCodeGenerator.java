57f8dd124b19aa1bead1549007f25f9a60705dc6
nayonsoso <panda0329@naver.com>
2024-07-31 16:25:13 +0900

    feat: 랜덤한 문자열 생성기 구현
    Co-authored-by: donghoony <aru0504@naver.com>

---
+++ backend/src/main/java/reviewme/reviewgroup/service/RandomCodeGenerator.java
@@ -0,0 +1,20 @@
+package reviewme.reviewgroup.service;
+
+import java.util.Random;
+
+public class RandomCodeGenerator {
+
+    private static final Random random = new Random();
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String CHARACTER = UPPERCASE + LOWERCASE + NUMBERS;
+
+    public String generate(int length) {
+        StringBuilder sb = new StringBuilder();
+        random.ints(length, 0, CHARACTER.length())
+                .mapToObj(CHARACTER::charAt)
+                .forEach(sb::append);
+        return sb.toString();
+    }
+}
