301bdd423abd9ca329e056ff7c6fe561410e344a
nayonsoso <panda0329@naver.com>
2024-07-31 14:44:22 +0900

    chore: 테스트 코드 컨벤션 통일

--- backend/src/test/java/reviewme/review/service/ReviewCreationKeywordValidatorTest.java
+++ backend/src/test/java/reviewme/review/service/ReviewCreationKeywordValidatorTest.java
@@ -2,11 +2,14 @@ package reviewme.review.service;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static reviewme.fixture.KeywordFixture.꼼꼼하게_기록해요;
+import static reviewme.fixture.KeywordFixture.의견을_잘_조율해요;
+import static reviewme.fixture.KeywordFixture.추진력이_좋아요;
+import static reviewme.fixture.KeywordFixture.회의를_이끌어요;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import reviewme.fixture.KeywordFixture;
 import reviewme.keyword.domain.Keyword;
 import reviewme.keyword.domain.exception.DuplicateKeywordException;
 import reviewme.keyword.domain.exception.KeywordLimitExceedException;
@@ -26,8 +29,8 @@ class ReviewCreationKeywordValidatorTest {
     @Test
     void 존재하는_키워드의_아이디인지_검사한다() {
         // given
-        Keyword keyword1 = keywordRepository.save(KeywordFixture.회의를_이끌어요.create());
-        Keyword keyword2 = keywordRepository.save(KeywordFixture.추진력이_좋아요.create());
+        Keyword keyword1 = keywordRepository.save(회의를_이끌어요.create());
+        Keyword keyword2 = keywordRepository.save(추진력이_좋아요.create());
         long nonExistKeywordId = Long.MAX_VALUE;
 
         // when, then
@@ -43,14 +46,14 @@ class ReviewCreationKeywordValidatorTest {
     @Test
     void 중복되는_아이디의_키워드인지_검사한다() {
         // given
-        Keyword keyword1 = keywordRepository.save(KeywordFixture.회의를_이끌어요.create());
+        Keyword keyword = keywordRepository.save(회의를_이끌어요.create());
 
         // when, then
         assertAll(
-                () -> assertThatCode(() -> reviewCreationKeywordValidator.validate(List.of(keyword1.getId())))
+                () -> assertThatCode(() -> reviewCreationKeywordValidator.validate(List.of(keyword.getId())))
                         .doesNotThrowAnyException(),
                 () -> assertThatCode(
-                        () -> reviewCreationKeywordValidator.validate(List.of(keyword1.getId(), keyword1.getId())))
+                        () -> reviewCreationKeywordValidator.validate(List.of(keyword.getId(), keyword.getId())))
                         .isInstanceOf(DuplicateKeywordException.class)
         );
     }
@@ -58,10 +61,10 @@ class ReviewCreationKeywordValidatorTest {
     @Test
     void 유효한_개수의_키워드인지_검사한다() {
         // given
-        Keyword keyword1 = keywordRepository.save(KeywordFixture.회의를_이끌어요.create());
-        Keyword keyword2 = keywordRepository.save(KeywordFixture.추진력이_좋아요.create());
-        Keyword keyword3 = keywordRepository.save(KeywordFixture.꼼꼼하게_기록해요.create());
-        Keyword keyword4 = keywordRepository.save(KeywordFixture.의견을_잘_조율해요.create());
+        Keyword keyword1 = keywordRepository.save(회의를_이끌어요.create());
+        Keyword keyword2 = keywordRepository.save(추진력이_좋아요.create());
+        Keyword keyword3 = keywordRepository.save(꼼꼼하게_기록해요.create());
+        Keyword keyword4 = keywordRepository.save(의견을_잘_조율해요.create());
         Keyword keyword5 = keywordRepository.save(new Keyword("간식을 잘 나눠줘요."));
         Keyword keyword6 = keywordRepository.save(new Keyword("감정을 잘 공감해줘요."));
         List<Long> keywordIds = List.of(
@@ -69,7 +72,7 @@ class ReviewCreationKeywordValidatorTest {
                 keyword6.getId()
         );
 
-        // when & then
+        // when, then
         assertThatCode(() -> reviewCreationKeywordValidator.validate(keywordIds))
                 .isInstanceOf(KeywordLimitExceedException.class);
     }
