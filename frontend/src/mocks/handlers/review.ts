48ffb9ec5cec5948cbcb79a2ca63c99a93be4677
soosoo22 <lllllh00@naver.com>
2024-07-31 22:25:27 +0900

    feat: 리뷰 미리보기 리스트 핸들러에 페이지네이션 로직 추가
    - 기존의 단순 반환 핸들러에서 페이지네이션 기능을 포함한 핸들러로 수정

--- frontend/src/mocks/handlers/review.ts
+++ frontend/src/mocks/handlers/review.ts
@@ -6,6 +6,13 @@ import { DETAILED_REVIEW_MOCK_DATA } from '../mockData/detailedReviewMockData';
 import { REVIEW_PREVIEW_LIST } from '../mockData/reviewPreviewList';
 import { REVIEW_WRITING_DATA } from '../mockData/reviewWritingData';
 
+export const PAGE = {
+  firstPageNumber: 1,
+  firstPageStartIndex: 0,
+  defaultPageSize: 5,
+  additionalPageSize: 2,
+};
+
 const getDetailedReview = () =>
   http.get(endPoint.gettingDetailedReview(123456, 123456), async ({ request }) => {
     return HttpResponse.json(DETAILED_REVIEW_MOCK_DATA);
@@ -16,10 +23,31 @@ const getDataToWriteReview = () =>
     return HttpResponse.json(REVIEW_WRITING_DATA);
   });
 
-const getReviewPreviewList = () =>
-  http.get(endPoint.gettingReviewList(1, 3, 1), async ({ request }) => {
-    return HttpResponse.json(REVIEW_PREVIEW_LIST);
+const getReviewPreviewList = () => {
+  return http.get(endPoint.gettingReviewList(1, 6, 2), async ({ request }) => {
+    const url = new URL(request.url);
+
+    const lastReviewId = Number(url.searchParams.get('lastReviewId'));
+
+    const isFirstPage = lastReviewId === 0;
+    const limit = isFirstPage ? PAGE.defaultPageSize : PAGE.additionalPageSize;
+    const startIndex = isFirstPage
+      ? PAGE.firstPageStartIndex
+      : REVIEW_PREVIEW_LIST.reviews.findIndex((review) => review.id === lastReviewId) + 1;
+
+    const endIndex = startIndex + limit;
+
+    const paginatedReviews = REVIEW_PREVIEW_LIST.reviews.slice(startIndex, endIndex);
+
+    const isLastPage = endIndex >= REVIEW_PREVIEW_LIST.reviews.length;
+
+    return HttpResponse.json({
+      size: paginatedReviews.length,
+      lastReviewId: isLastPage ? null : lastReviewId + limit,
+      reviews: paginatedReviews,
+    });
   });
+};
 
 const reviewHandler = [getDetailedReview(), getReviewPreviewList(), getDataToWriteReview()];
 
