eaaaf4c55bf6100e5820cbbad544fe5135661a60
soosoo22 <lllllh00@naver.com>
2024-07-31 14:53:34 +0900

    refactor: buttons 배열을 통해 버튼 생성

--- frontend/src/pages/ErrorPage/components/ErrorSection/index.tsx
+++ frontend/src/pages/ErrorPage/components/ErrorSection/index.tsx
@@ -1,17 +1,35 @@
 import AlertTriangle from '@/assets/alertTriangle.svg';
-import Home from '@/assets/home.svg';
-import ReLoad from '@/assets/reload.svg';
+import HomeIcon from '@/assets/home.svg';
+import ReloadIcon from '@/assets/reload.svg';
 import { Button } from '@/components';
+import { ButtonType } from '@/types/styles';
 
 import * as S from './styles';
 
 interface ErrorSectionProps {
   errorMessage: string;
-  handleReLoad: () => void;
+  handleReload: () => void;
   handleGoHome: () => void;
 }
 
-const ErrorSection = ({ errorMessage, handleReLoad, handleGoHome }: ErrorSectionProps) => {
+const ErrorSection = ({ errorMessage, handleReload, handleGoHome }: ErrorSectionProps) => {
+  const buttons = [
+    {
+      buttonType: 'primary' as ButtonType,
+      text: '새로고침하기',
+      image: ReloadIcon,
+      imageDescription: '새로고침 이미지',
+      onClick: handleReload,
+    },
+    {
+      buttonType: 'secondary' as ButtonType,
+      text: '홈으로 이동하기',
+      image: HomeIcon,
+      imageDescription: '홈 이미지',
+      onClick: handleGoHome,
+    },
+  ];
+
   return (
     <S.Layout>
       <S.ErrorLogoWrapper>
@@ -19,12 +37,17 @@ const ErrorSection = ({ errorMessage, handleReLoad, handleGoHome }: ErrorSection
       </S.ErrorLogoWrapper>
       <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
       <S.Container>
-        <S.ButtonContainer>
-          <Button buttonType="primary" text="새로고침하기" icon={ReLoad} onClick={handleReLoad} />
-        </S.ButtonContainer>
-        <S.ButtonContainer>
-          <Button buttonType="secondary" text="홈으로 이동하기" icon={Home} onClick={handleGoHome} />
-        </S.ButtonContainer>
+        {buttons.map((button, index) => (
+          <S.ButtonContainer key={index}>
+            <Button
+              buttonType={button.buttonType}
+              text={button.text}
+              image={button.image}
+              imageDescription={button.imageDescription}
+              onClick={button.onClick}
+            />
+          </S.ButtonContainer>
+        ))}
       </S.Container>
     </S.Layout>
   );
