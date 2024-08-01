31c9f16ac2c0df174dd21433c68a9c693f1abd09
chysis <chysiss@naver.com>
2024-08-01 14:40:36 +0900

    refactor: AlertModal의 모달 닫는 정책을 props로 선택 가능하도록 수정

--- frontend/src/components/common/modals/AlertModal/index.tsx
+++ frontend/src/components/common/modals/AlertModal/index.tsx
@@ -9,16 +9,29 @@ import * as S from './styles';
 interface CloseButton {
   type: ButtonStyleType;
   handleClick: () => void;
+  content: React.ReactNode;
 }
 
 interface AlertModalProps {
   closeButton: CloseButton;
+  isCloseOnBackground?: boolean;
+  isCloseOnEsc?: boolean;
+  handleClose?: () => void;
 }
 
-const AlertModal = ({ closeButton, children }: EssentialPropsWithChildren<AlertModalProps>) => {
+const AlertModal = ({
+  closeButton,
+  isCloseOnBackground = true,
+  isCloseOnEsc = true,
+  handleClose = closeButton.handleClick,
+  children,
+}: EssentialPropsWithChildren<AlertModalProps>) => {
   return (
     <ModalPortal>
-      <ModalBackground closeModal={() => {}}>
+      <ModalBackground
+        closeModalOnBackground={isCloseOnBackground ? handleClose : () => {}}
+        closeModalOnEsc={isCloseOnEsc ? handleClose : () => {}}
+      >
         <S.AlertModalContainer>
           <S.Contents>{children}</S.Contents>
           <Button
@@ -26,7 +39,7 @@ const AlertModal = ({ closeButton, children }: EssentialPropsWithChildren<AlertM
             onClick={closeButton.handleClick}
             style={{ width: '100%', minWidth: '30rem', height: '4rem' }}
           >
-            닫기
+            {closeButton.content}
           </Button>
         </S.AlertModalContainer>
       </ModalBackground>
