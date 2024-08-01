11eb23e74543f1e1ae717a00110f886f277ffc0b
soosoo22 <lllllh00@naver.com>
2024-08-01 19:02:54 +0900

    refactor: 변경된 api에 따라 props 수정

--- frontend/src/components/ReviewPreviewCard/index.tsx
+++ frontend/src/components/ReviewPreviewCard/index.tsx
@@ -1,27 +1,29 @@
-import GithubLogo from '@/assets/githubLogo.svg';
-import Lock from '@/assets/lock.svg';
-import UnLock from '@/assets/unLock.svg';
+import GithubLogoIcon from '@/assets/githubLogo.svg';
 import { ReviewPreview } from '@/types';
 
 import * as S from './styles';
 
-const ReviewPreviewCard = ({ id, reviewerGroup, createdAt, contentPreview, keywords, isPublic }: ReviewPreview) => {
+interface ReviewPreviewCardProps extends ReviewPreview {
+  projectName: string;
+}
+
+const ReviewPreviewCard = ({ id, projectName, createdAt, contentPreview, keywords }: ReviewPreviewCardProps) => {
   return (
     <S.Layout data-id={id}>
       <S.Header>
         <S.HeaderContent>
           <div>
-            <img src={GithubLogo} alt="깃허브 로고" />
+            <img src={GithubLogoIcon} alt="깃허브 로고" />
           </div>
           <div>
-            <S.Title>{reviewerGroup.name}</S.Title>
+            <S.Title>{projectName}</S.Title>
             <S.SubTitle>{createdAt}</S.SubTitle>
           </div>
         </S.HeaderContent>
-        <S.Visibility>
+        {/* 추후에 사용될 수 있어서 일단 주석 처리 <S.Visibility>
           <img src={isPublic ? UnLock : Lock} alt="자물쇠 아이콘" />
           <span>{isPublic ? '공개' : '비공개'}</span>
-        </S.Visibility>
+        </S.Visibility> */}
       </S.Header>
       <S.Main>
         <span>{contentPreview}</span>
