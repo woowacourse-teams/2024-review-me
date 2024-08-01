aee643a8ade855940f2b0319c1b722ca4220b4a0
badahertz52 <badahertz52@gmail.com>
2024-08-01 17:40:23 +0900

    feat: DetailedReviewPage 에서 groupAccessCode 상태 적용

--- frontend/src/pages/DetailedReviewPage/index.tsx
+++ frontend/src/pages/DetailedReviewPage/index.tsx
@@ -1,10 +1,13 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useLocation, useParams } from 'react-router';
+import { useRecoilValue } from 'recoil';
 
 import { DETAILED_REVIEW_API_PARAMS } from '@/apis/endpoints';
 import { getDetailedReviewApi } from '@/apis/review';
 import { REVIEW_QUERY_KEYS } from '@/constants';
 import { ReviewDescription, ReviewSection, KeywordSection } from '@/pages/DetailedReviewPage/components';
+import { groupAccessCodeAtom } from '@/recoil';
 import { DetailReviewData } from '@/types';
 
 import * as S from './styles';
@@ -14,18 +17,22 @@ const DetailedReviewPage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const memberId = queryParams.get(DETAILED_REVIEW_API_PARAMS.queryString.memberId);
+  const groupAccessCode = useRecoilValue(groupAccessCodeAtom);
 
   const fetchDetailedReview = async (reviewId: number, memberId: number) => {
-    const result = await getDetailedReviewApi({ reviewId, memberId });
+    if (!groupAccessCode) return;
+
+    const result = await getDetailedReviewApi({ reviewId, memberId, groupAccessCode });
     return result;
   };
 
-  const { data: detailedReview } = useSuspenseQuery<DetailReviewData>({
+  const { data: detailedReview } = useSuspenseQuery<DetailReviewData | undefined>({
     queryKey: [REVIEW_QUERY_KEYS.detailedReview, id, memberId],
     queryFn: () => fetchDetailedReview(Number(id), Number(memberId)),
   });
 
   if (!detailedReview) throw new Error(' 상세보기 리뷰 데이터를 가져올 수 없어요.');
+  if (!groupAccessCode) return;
   // TODO: 리뷰 공개/비공개 토글 버튼 기능
   return (
     <S.DetailedReviewPage>
