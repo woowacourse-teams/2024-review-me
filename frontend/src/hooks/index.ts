bc4cf3926b19de38b8f67d8ae781fa0e93ba18a1
badahertz52 <badahertz52@gmail.com>
2024-08-01 14:40:51 +0900

    feat: useGroupAccessCode 훅 추가
    - useGroupAccessCode : groupAccessCode의 atom 상태를 관리하는 훅

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useSidebar } from './useSidebar';
 export { default as useModalClose } from './useModalClose';
+export { default as useGroupAccessCode } from './useGroupAccessCode';
