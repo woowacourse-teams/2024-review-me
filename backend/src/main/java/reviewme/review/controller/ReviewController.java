efce1614e4ac1972758cfad6527782ab7d22095e
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-01 14:53:13 +0900

    feat: 컨트롤러 리뷰 상세 조회 api 응답 기능 구현

--- backend/src/main/java/reviewme/review/controller/ReviewController.java
+++ backend/src/main/java/reviewme/review/controller/ReviewController.java
@@ -1,14 +1,17 @@
 package reviewme.review.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reviewme.review.dto.response.ReviewBygroupAccessCodeResponse;
 import reviewme.review.dto.request.CreateReviewRequest;
 import reviewme.review.dto.response.ReviewSetupResponse;
 import reviewme.review.service.ReviewService;
@@ -28,6 +31,13 @@ public class ReviewController implements ReviewApi{
     @GetMapping("/reviews/write")
     public ResponseEntity<ReviewSetupResponse> findReviewCreationSetup(@RequestParam String reviewRequestCode) {
         ReviewSetupResponse response = reviewService.findReviewCreationSetup(reviewRequestCode);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/reviews/{id}")
+    public ResponseEntity<ReviewBygroupAccessCodeResponse> findReview(@PathVariable long id, HttpServletRequest request) {
+        String groupAccessCode = request.getHeader("GroupAccessCode");
+        ReviewBygroupAccessCodeResponse response = reviewService.findReviewByGroupAccessCode(groupAccessCode, id);
         return ResponseEntity.ok(response);
     }
 }
