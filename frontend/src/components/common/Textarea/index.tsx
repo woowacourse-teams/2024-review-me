2edf0b4750453bf86feb6ded241ce52e61a3f1d5
chysis <chysiss@naver.com>
2024-08-01 17:32:34 +0900

    feat: Textarea 공통 컴포넌트 뼈대 구현

---
+++ frontend/src/components/common/Textarea/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+interface TextareaProps {}
+
+const Textarea = () => {
+  return <div>Textarea</div>;
+};
+
+export default Textarea;
