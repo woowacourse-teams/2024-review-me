8c4f9dc5050a772bd89f1cbb9b3690dfd42d47ae
badahertz52 <badahertz52@gmail.com>
2024-08-01 17:04:07 +0900

    [FE] recoil을 통한 groupAccessCode 전역 상태 관리 (#180)
    * ci: recoil 설치
    
    * feat: src/index.ts에 RecoilRoot 적용
    
    * feat: groupAccessCode 에 대한 atom 상태 추가
    
    * feat: useGroupAccessCode 훅 추가
    
    - useGroupAccessCode : groupAccessCode의 atom 상태를 관리하는 훅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import App from '@/App';
 
@@ -62,7 +63,9 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <Global styles={globalStyles} />
-        <RouterProvider router={router} />
+        <RecoilRoot>
+          <RouterProvider router={router} />
+        </RecoilRoot>
       </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
---
+++ frontend/src/recoil/keys/index.ts
@@ -0,0 +1,3 @@
+export const ATOM_KEY = {
+  groupAccessCodeAtom: 'groupAccessCodeAtom',
+};
