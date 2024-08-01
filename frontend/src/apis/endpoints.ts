b19a4d19bb58972bd07805589e447610ef9aeb10
soosoo22 <lllllh00@naver.com>
2024-08-01 18:42:20 +0900

    refactor: 리뷰 목록을 불러오는 api 변경 이슈로 인해 엔드포인트, fetch 함수 수정

--- frontend/src/apis/endpoints.ts
+++ frontend/src/apis/endpoints.ts
@@ -4,8 +4,7 @@ const endPoint = {
     `${process.env.API_BASE_URL}/reviews/${reviewId}?memberId=${memberId}`,
   gettingDataToWriteReview: (reviewerGroupId: number) =>
     `${process.env.API_BASE_URL}/reviews/write?reviewerGroupId=${reviewerGroupId}`,
-  gettingReviewList: (revieweeId: number, lastReviewId: number, memberId: number) =>
-    `${process.env.API_BASE_URL}/reviews?revieweeId=${revieweeId}&lastReviewId=${lastReviewId}&memberId=${memberId}`,
+  gettingReviewList: `${process.env.API_BASE_URL}/reviews`,
 };
 
 export default endPoint;
