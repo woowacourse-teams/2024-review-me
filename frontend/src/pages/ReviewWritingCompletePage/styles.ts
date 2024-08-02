d073e4332c9354e50e37627a69d23de6c5ae50e0
Fe <64690761+chysis@users.noreply.github.com>
2024-08-02 12:21:33 +0900

    feat: 리뷰 작성 완료 페이지에서 홈 버튼 추가 (#201)

--- frontend/src/pages/ReviewWritingCompletePage/styles.ts
+++ frontend/src/pages/ReviewWritingCompletePage/styles.ts
@@ -1,13 +1,26 @@
 import styled from '@emotion/styled';
 
-export const Title = styled.div`
+export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 5rem;
   align-items: center;
   justify-content: center;
 
   width: 100%;
   height: 100%;
+`;
 
+export const Title = styled.p`
   font-size: 3rem;
   font-weight: bold;
+`;
+
+export const HomeIcon = styled.img`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const HomeText = styled.p`
+  margin-left: 0.5rem;
 `;
