9c171d6a4aa83c8903682f929684f1c6de3f5750
badahertz52 <badahertz52@gmail.com>
2024-08-03 16:14:24 +0900

    ci: jest에서 절대 경로 사용할 수 있도록  jest.config.js 추가

---
+++ frontend/jest.config.js
@@ -0,0 +1,6 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
