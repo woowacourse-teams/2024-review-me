4553c2c38e0e3829bb00d57c8f7238023e8e9fa4
badahertz52 <badahertz52@gmail.com>
2024-08-03 18:07:29 +0900

    fix: jest에서 msw ver2를 목서버로 사용 시 생기는 오류 수정
    1. msw/node 를 읽지 못함
    - jest.config.js의 testEnvironment 빈문자열
    
    2. ReferentError: TextEnCoder is not defined
    - 해결 : jest.polyfills.js 추가 및 undici 설치
    
    3. ReferenceError: ReadableStream is not defined
    - 해결 : undici 다운 그레이드
    undici": "^6.19.5", -> "^5.0.0"

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,5 +4,10 @@ const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  setupFiles: ['./jest.polyfills.js'],
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
 };
