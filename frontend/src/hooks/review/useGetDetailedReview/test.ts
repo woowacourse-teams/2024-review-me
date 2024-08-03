83f2bfbd4571cef401b1df6a1411cad52be325d2
badahertz52 <badahertz52@gmail.com>
2024-08-03 20:06:05 +0900

    test:리뷰 상세 페이지 api 요청 성공에 대한 테스트 추가

---
+++ frontend/src/hooks/review/useGetDetailedReview/test.ts
@@ -0,0 +1,26 @@
+import { renderHook, waitFor } from '@testing-library/react';
+
+import { DETAILED_PAGE_MOCK_API_SETTING_VALUES } from '@/mocks/mockData/detailedReviewMockData';
+import queryClientWrapper from '@/queryTestSetup/queryClientWrapper';
+
+import useGetDetailedReview from '.';
+// 아래의 테스트는 로그인이 유효하다는 가정하에서 진행
+
+describe('리뷰 상세페이지 데이터 요청 테스트', () => {
+  const GROUND_ACCESS_CODE = '1234';
+
+  it('유효힌 id,memberId 사용해야 라뷰 상세 페이지 데이터를 불러온다.', async () => {
+    const { reviewId, memberId } = DETAILED_PAGE_MOCK_API_SETTING_VALUES;
+
+    const { result } = renderHook(
+      () => useGetDetailedReview({ reviewId, memberId, groupAccessCode: GROUND_ACCESS_CODE }),
+      { wrapper: queryClientWrapper },
+    );
+
+    await waitFor(() => {
+      expect(result.current.status).toBe('success');
+    });
+
+    expect(result.current.data).toBeDefined();
+  });
+});
