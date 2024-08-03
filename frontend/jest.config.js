a3f17059fb22491c3fd40cc706685e0a95944a71
badahertz52 <badahertz52@gmail.com>
2024-08-03 16:16:36 +0900

    ci: jest의 testEnvioronment를 jsdom으로 설정

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,8 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
+
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  testEnvironment: 'jsdom',
 };
