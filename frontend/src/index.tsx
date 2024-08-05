77116ad64f26428fdd7f55c5899c9573c4abe2e2
soosoo22 <lllllh00@naver.com>
2024-08-05 20:10:25 +0900

    chore: msw 모킹 코드 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -60,37 +60,15 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-async function enableMocking() {
-  if (process.env.MSW) {
-    const { worker } = await import('./mocks/browser');
-    return worker.start();
-  }
-}
-
-enableMocking().then(() => {
-  root.render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <Global styles={globalStyles} />
-          <RecoilRoot>
-            <RouterProvider router={router} />
-          </RecoilRoot>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
-});
-
-// root.render(
-//   <React.StrictMode>
-//     <QueryClientProvider client={queryClient}>
-//       <ThemeProvider theme={theme}>
-//         <Global styles={globalStyles} />
-//         <RecoilRoot>
-//           <RouterProvider router={router} />
-//         </RecoilRoot>
-//       </ThemeProvider>
-//     </QueryClientProvider>
-//   </React.StrictMode>,
-// );
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyles} />
+        <RecoilRoot>
+          <RouterProvider router={router} />
+        </RecoilRoot>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
