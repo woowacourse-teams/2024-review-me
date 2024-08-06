2a65bd8d6b441bd6aa6a5ab216c16dac6e5c8510
chysis <chysiss@naver.com>
2024-08-07 01:34:25 +0900

    feat: 공통 textarea 컴포넌트 작성

--- frontend/src/components/common/Textarea/index.tsx
+++ frontend/src/components/common/Textarea/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { TextareaHTMLAttributes } from 'react';
 
 import * as S from './styles';
 
-interface TextareaProps {}
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  value: string;
+  textLength: string;
+  isError?: boolean;
+  errorMessage?: string;
+}
 
-const Textarea = () => {
-  return <div>Textarea</div>;
+const Textarea = ({ value, textLength, isError = false, errorMessage = '', style, ...rest }: TextareaProps) => {
+  return (
+    <S.TextareaContainer>
+      <S.Textarea value={value} $isError={isError} $style={style} {...rest} />
+      <div>
+        <S.ReviewTextareaError>{errorMessage}</S.ReviewTextareaError>
+        <S.ReviewTextLength>{textLength}</S.ReviewTextLength>
+      </div>
+    </S.TextareaContainer>
+  );
 };
 
 export default Textarea;
--- frontend/src/components/common/Textarea/styles.ts
+++ frontend/src/components/common/Textarea/styles.ts
@@ -1,3 +1,42 @@
 import styled from '@emotion/styled';
 
-export const Textarea = styled.textarea``;
+export const TextareaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface TextareaProps {
+  $isError: boolean;
+  $style?: React.CSSProperties;
+}
+
+export const Textarea = styled.textarea<TextareaProps>`
+  resize: none;
+
+  overflow-y: auto;
+
+  width: 100%;
+  height: 15rem;
+  padding: 1.6rem;
+
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+
+  border: 0.1rem solid ${({ $isError, theme }) => ($isError ? theme.colors.red : theme.colors.black)};
+  border-radius: 0.8rem;
+
+  &::placeholder {
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+
+  ${({ $style }) => $style && { ...$style }};
+`;
+
+export const ReviewTextareaError = styled.p`
+  color: ${({ theme }) => theme.colors.red};
+`;
+
+export const ReviewTextLength = styled.p`
+  display: flex;
+  justify-content: flex-end;
+  margin: 0;
+`;
