d5e374255683013fd1165d9e3d407b8ba8b5a50b
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2024-08-07 23:32:39 +0900

    [FE] feat: 공통 컴포넌트인 체크박스와 체크박스 아이템 제작 (#240)
    * feat: 공통 체크박스 컴포넌트 작성
    
    * feat: 체크박스에 레이블을 달 수 있는 CheckboxItem 컴포넌트 작성
    
    * fix: style을 잘못 전달하던 문제 해결

---
+++ frontend/src/components/common/CheckboxItem/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const CheckboxItem = styled.div``;
+
+export const CheckboxLabel = styled.label`
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
+`;
