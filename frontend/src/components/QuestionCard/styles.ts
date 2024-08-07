58ce56d7ebdf4df36380fa8a3b064ad2a9d1914c
soosoo22 <lllllh00@naver.com>
2024-08-07 23:42:29 +0900

    style: CSS 속성 순서 정렬

--- frontend/src/components/QuestionCard/styles.ts
+++ frontend/src/components/QuestionCard/styles.ts
@@ -4,5 +4,5 @@ import { QuestionCardStyleType } from '@/types';
 
 export const QuestionCard = styled.span<{ questionType: QuestionCardStyleType }>`
   font-size: ${({ theme }) => theme.fontSize.basic};
-  color: ${({ questionType, theme }) => (questionType === 'guideline' ? theme.colors.lightGray : theme.colors.black)};
+  color: ${({ questionType, theme }) => (questionType === 'guideline' ? theme.colors.placeholder : theme.colors.black)};
 `;
