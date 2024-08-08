b0f7dbe6097033c05b41ae777bbf22ecaf2d1270
chysis <chysiss@naver.com>
2024-08-08 15:10:42 +0900

    refactor: 리뷰 작성 후 게시 요청 로직에 useMutation 적용

---
+++ frontend/src/hooks/review/useMutateReview/index.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { postReviewApi } from '@/apis/review';
+import { REVIEW_QUERY_KEYS } from '@/constants';
+import { ReviewData } from '@/types';
+
+interface PostReviewArgs {
+  reviewData: ReviewData;
+}
+
+const useMutateReview = () => {
+  const queryClient = useQueryClient();
+
+  const reviewMutation = useMutation({
+    mutationFn: ({ reviewData }: PostReviewArgs) => postReviewApi({ reviewData }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [REVIEW_QUERY_KEYS.postReview] });
+    },
+    onError: () => {
+      console.error('리뷰 제출에 실패했어요.');
+    },
+  });
+
+  const postReview = ({ reviewData }: PostReviewArgs) => {
+    reviewMutation.mutate({ reviewData });
+  };
+
+  return { postReview };
+};
+
+export default useMutateReview;
