1e748f775cc800094847b07844b20ed77ffafd4d
badahertz52 <badahertz52@gmail.com>
2024-08-08 17:28:51 +0900

    design: QuestionCard 폰트 사이즈 변경

--- frontend/src/pages/ReviewWritingFormPage/components/QuestionCard/styles.ts
+++ frontend/src/pages/ReviewWritingFormPage/components/QuestionCard/styles.ts
@@ -4,7 +4,7 @@ import { QuestionCardStyleType } from '@/types';
 
 export const QuestionCard = styled.div<{ questionType: QuestionCardStyleType }>`
   margin-bottom: 2rem;
-  font-size: 1.8rem;
+  font-size: ${({ questionType, theme }) => (questionType === 'guideline' ? theme.fontSize.basic : '1.8rem')};
   font-weight: ${({ questionType, theme }) =>
     questionType === 'guideline' ? theme.fontWeight.normal : theme.fontWeight.semibold};
   color: ${({ questionType, theme }) => (questionType === 'guideline' ? theme.colors.placeholder : theme.colors.black)};
