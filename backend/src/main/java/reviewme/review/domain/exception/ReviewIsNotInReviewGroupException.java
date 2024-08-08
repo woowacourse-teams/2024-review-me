cecefa0a0113492eff527920968c33005ef839f1
nayonsoso <panda0329@naver.com>
2024-08-08 15:01:14 +0900

    feat: 리뷰 그룹 코드 검증 검증 로깅 추가

--- backend/src/main/java/reviewme/review/domain/exception/ReviewIsNotInReviewGroupException.java
+++ backend/src/main/java/reviewme/review/domain/exception/ReviewIsNotInReviewGroupException.java
@@ -1,10 +1,13 @@
 package reviewme.review.domain.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import reviewme.global.exception.BadRequestException;
 
+@Slf4j
 public class ReviewIsNotInReviewGroupException extends BadRequestException {
 
-    public ReviewIsNotInReviewGroupException() {
+    public ReviewIsNotInReviewGroupException(long reviewId, long reviewGroupId) {
         super("리뷰 그룹에 해당하는 리뷰가 아니에요.");
+        log.info("Review is not in review group - reviewId: {},reviewGroupId: {}", reviewId, reviewGroupId);
     }
 }
