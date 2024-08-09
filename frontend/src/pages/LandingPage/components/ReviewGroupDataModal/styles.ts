473fb886531bcdc887eb4e8d9a7e9a52e99f1344
ImxYJL <allensain14@gmail.com>
2024-08-09 17:05:06 +0900

    feat: 내용 확인 체크박스에 체크해야 모달을 닫을 수 있도록 수정 및 모달 스타일 조정

--- frontend/src/pages/LandingPage/components/ReviewGroupDataModal/styles.ts
+++ frontend/src/pages/LandingPage/components/ReviewGroupDataModal/styles.ts
@@ -6,27 +6,25 @@ export const ReviewGroupDataModal = styled.div`
 
   width: 52rem;
   height: 23rem;
-
-  gap: 4rem;
 `;
 
 export const ReviewGroupDataTitle = styled.h3`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: 2rem;
+  margin-bottom: 4rem;
 `;
 
 export const ReviewGroupDataContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.7rem;
-  min-width: 0;
+  gap: 2.4rem;
 `;
 
 export const ReviewGroupDataItem = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 1.8rem;
-
+  align-items: center;
   font-size: 1.5rem;
 `;
 
@@ -40,20 +38,33 @@ export const Data = styled.span`
   color: ${({ theme }) => theme.colors.gray};
 `;
 
-export const CheckContainer = styled.div`
+export const WarningContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
+  margin-top: 3.7rem;
+`;
+
+export const CheckContainer = styled.div`
   font-size: 1.5rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
 `;
 
 export const Checkbox = styled.input`
   height: 1rem;
   width: 1rem;
-  margin-right: 0.5rem;
+  margin-right: 0.3rem;
 
   cursor: pointer;
 `;
 
-export const CheckMessage = styled.p``;
+export const CheckMessage = styled.p`
+  font-size: 1.3rem;
+`;
 
 export const Warning = styled.p`
   color: ${({ theme }) => theme.colors.red};
