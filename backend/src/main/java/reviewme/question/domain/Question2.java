15c11e8af268d11e5f833ded6a95a8aa5c36c165
donghoony <aru0504@naver.com>
2024-08-10 15:05:14 +0900

    feat: 초기 엔티티 구성

---
+++ backend/src/main/java/reviewme/question/domain/Question2.java
@@ -0,0 +1,40 @@
+package reviewme.question.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "question2")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Question2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "required", nullable = false)
+    private boolean required;
+
+    @Column(name = "question_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+
+    @Column(name = "content", nullable = false, length = 1_000)
+    private String content;
+
+    @Column(name = "guideline", nullable = true, length = 1_000)
+    private String guideline;
+
+    @Column(name = "order", nullable = false)
+    private int order;
+}
---
+++ backend/src/main/java/reviewme/template/domain/Section.java
@@ -0,0 +1,45 @@
+package reviewme.template.domain;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "section")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "visible_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private VisibleType visibleType;
+
+    @ElementCollection
+    @CollectionTable(name = "question_ids", joinColumns = @JoinColumn(name = "section_id"))
+    private List<Long> questionIds;
+
+    @Column(name = "on_selected_option_id", nullable = true)
+    private Long onSelectedOptionId;
+
+    @Column(name = "header", nullable = false, length = 1_000)
+    private String header;
+
+    @Column(name = "order", nullable = false)
+    private int order;
+}
