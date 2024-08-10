9c504134023cef0f4b47c5a32cbba4798a82c5a3
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-11 02:41:31 +0900

    refactor: 로깅 레벨 warn으로 변경

--- backend/src/main/java/reviewme/question/domain/exception/OptionGroupNotFoundByQuestionException.java
+++ backend/src/main/java/reviewme/question/domain/exception/OptionGroupNotFoundByQuestionException.java
@@ -8,6 +8,6 @@ public class OptionGroupNotFoundByQuestionException extends NotFoundException {
 
     public OptionGroupNotFoundByQuestionException(long questionId) {
         super("서버 내부에서 문제가 발생했어요. 서버에 문의해주세요.");
-        log.info("OptionGroup not found by question - questionId: {}", questionId);
+        log.warn("OptionGroup not found by question - questionId: {}", questionId);
     }
 }
