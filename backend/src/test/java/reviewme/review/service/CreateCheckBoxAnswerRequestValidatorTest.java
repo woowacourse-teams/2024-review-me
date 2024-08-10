5d30d5c27bc1680d5ca30861f8d7938be894f996
nayonsoso <panda0329@naver.com>
2024-08-11 04:24:41 +0900

    test: 선택 문항 검증 테스트 추가

---
+++ backend/src/test/java/reviewme/review/service/CreateCheckBoxAnswerRequestValidatorTest.java
@@ -0,0 +1,166 @@
+package reviewme.review.service;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reviewme.question.domain.OptionGroup;
+import reviewme.question.domain.OptionItem;
+import reviewme.question.domain.Question2;
+import reviewme.question.domain.QuestionType;
+import reviewme.question.domain.exception.QuestionNotFoundException;
+import reviewme.question.repository.Question2Repository;
+import reviewme.review.dto.request.create.CreateReviewAnswerRequest;
+import reviewme.review.service.exception.CheckBoxAnswerIncludedNotProvidedOptionItemException;
+import reviewme.review.service.exception.CheckBoxAnswerIncludedTextException;
+import reviewme.review.service.exception.SelectedCheckBoxAnswerCountOutOfRange;
+import reviewme.review.service.exception.RequiredQuestionMustBeAnsweredException;
+import reviewme.support.ServiceTest;
+import reviewme.template.domain.exception.OptionGroupNotFoundException;
+import reviewme.template.repository.OptionGroupRepository;
+import reviewme.template.repository.OptionItemRepository;
+
+@ServiceTest
+class CreateCheckBoxAnswerRequestValidatorTest {
+
+    @Autowired
+    private CreateCheckBoxAnswerRequestValidator createCheckBoxAnswerRequestValidator;
+
+    @Autowired
+    private Question2Repository question2Repository;
+
+    @Autowired
+    private OptionGroupRepository optionGroupRepository;
+
+    @Autowired
+    private OptionItemRepository optionItemRepository;
+
+    private Question2 savedQuestion;
+
+    @BeforeEach
+    void setUp() {
+        savedQuestion = question2Repository.save(new Question2(true, QuestionType.CHECKBOX, "질문", null, 1));
+    }
+
+    @Test
+    void 저장되지_않은_질문에_대한_응답이면_예외가_발생한다() {
+        // given
+        long notSavedQuestionId = 100L;
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                notSavedQuestionId,
+                List.of(1L),
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(QuestionNotFoundException.class);
+    }
+
+    @Test
+    void 선택형_질문에_텍스트_응답을_하면_예외가_발생한다() {
+        // given
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                List.of(1L),
+                "서술형 응답"
+        );
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(CheckBoxAnswerIncludedTextException.class);
+    }
+
+    @Test
+    void 저장되지_않은_옵션그룹에_대해_응답하면_예외가_발생한다() {
+        // given
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                List.of(1L),
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(OptionGroupNotFoundException.class);
+    }
+
+    @Test
+    void 필수_선택형_질문에_응답을_하지_않으면_예외가_발생한다() {
+        // given
+        optionGroupRepository.save(
+                new OptionGroup(savedQuestion.getId(), 1, 3)
+        );
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                null,
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(RequiredQuestionMustBeAnsweredException.class);
+    }
+
+    @Test
+    void 옵션그룹에서_제공하지_않은_옵션아이템을_응답하면_예외가_발생한다() {
+        // given
+        OptionGroup savedOptionGroup = optionGroupRepository.save(
+                new OptionGroup(savedQuestion.getId(), 1, 3)
+        );
+        OptionItem savedOptionItem = optionItemRepository.save(
+                new OptionItem("옵션", savedOptionGroup.getId(), 1)
+        );
+
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                List.of(savedOptionItem.getId() + 1L),
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(CheckBoxAnswerIncludedNotProvidedOptionItemException.class);
+    }
+
+    @Test
+    void 옵션그룹에서_정한_최소_선택_수_보다_적게_선택하면_예외가_발생한다() {
+        // given
+        OptionGroup savedOptionGroup = optionGroupRepository.save(
+                new OptionGroup(savedQuestion.getId(), 2, 3)
+        );
+        OptionItem savedOptionItem1 = optionItemRepository.save(
+                new OptionItem("옵션1", savedOptionGroup.getId(), 1)
+        );
+
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                List.of(savedOptionItem1.getId()),
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(SelectedCheckBoxAnswerCountOutOfRange.class);
+    }
+
+    @Test
+    void 옵션그룹에서_정한_최대_선택_수_보다_많이_선택하면_예외가_발생한다() {
+        // given
+        OptionGroup savedOptionGroup = optionGroupRepository.save(
+                new OptionGroup(savedQuestion.getId(), 1, 1)
+        );
+        OptionItem savedOptionItem1 = optionItemRepository.save(
+                new OptionItem("옵션1", savedOptionGroup.getId(), 1)
+        );
+        OptionItem savedOptionItem2 = optionItemRepository.save(
+                new OptionItem("옵션2", savedOptionGroup.getId(), 2)
+        );
+
+        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
+                savedQuestion.getId(),
+                List.of(savedOptionItem1.getId(), savedOptionItem2.getId()),
+                null);
+
+        // when, then
+        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+                .isInstanceOf(SelectedCheckBoxAnswerCountOutOfRange.class);
+    }
+}
