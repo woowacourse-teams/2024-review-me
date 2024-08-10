ef8a5b97bacd6bf672698092303e22d89790bb74
badahertz52 <badahertz52@gmail.com>
2024-08-10 22:12:07 +0900

    chore: 3차 데모데이 시연 중 추가한 cd 관련 코드 사항 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,6 @@ const App = () => {
         </SideModal>
       )} */}
       <Topbar openSidebar={openSidebar} />
-      <div>주문하신 CD 반영입니다~</div>
       <Main>
         <Outlet />
       </Main>
