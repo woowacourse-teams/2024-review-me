5abbf9213c0506237636474467d6c9873fb107ee
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-10 23:24:38 +0900

    refactor: 에러 응답 메세지 변경

--- backend/src/main/java/reviewme/review/domain/exception/InvalidAnswerLengthException.java
+++ backend/src/main/java/reviewme/review/domain/exception/InvalidAnswerLengthException.java
@@ -7,7 +7,7 @@ import reviewme.global.exception.BadRequestException;
 public class InvalidAnswerLengthException extends BadRequestException {
 
     public InvalidAnswerLengthException(int answerLength, int minLength, int maxLength) {
-        super("답변의 길이는 %d자 이상 %d자 이하여야 합니다.".formatted(minLength, maxLength));
+        super("답변의 길이는 %d자 이상 %d자 이하여야 해요.".formatted(minLength, maxLength));
         log.info("AnswerLength is out of bound - answerLength:{}, minLength: {}, maxLength: {}",
                 answerLength, minLength, maxLength);
     }
