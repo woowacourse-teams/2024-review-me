194239e23fdd1c2b086b525924dd50a0468c5227
nayonsoso <panda0329@naver.com>
2024-08-11 03:15:09 +0900

    feat: 선택형 대답 검증기 생성

---
+++ backend/src/main/java/reviewme/review/service/CreateCheckBoxAnswerRequestValidator.java
@@ -0,0 +1,62 @@
+package reviewme.review.service;
+
+import java.util.HashSet;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reviewme.question.domain.OptionGroup;
+import reviewme.question.domain.OptionItem;
+import reviewme.question.domain.Question2;
+import reviewme.question.repository.Question2Repository;
+import reviewme.review.dto.request.create.CreateReviewAnswerRequest;
+import reviewme.review.service.exception.CheckBoxAnswerIncludedNotProvidedOptionItemException;
+import reviewme.review.service.exception.CheckBoxAnswerIncludedTextException;
+import reviewme.template.domain.exception.OptionGroupNotFoundException;
+import reviewme.template.repository.OptionGroupRepository;
+import reviewme.template.repository.OptionItemRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CreateCheckBoxAnswerRequestValidator {
+
+    private final Question2Repository question2Repository;
+    private final OptionGroupRepository optionGroupRepository;
+    private final OptionItemRepository optionItemRepository;
+
+    public void validate(CreateReviewAnswerRequest request) {
+        validateNotContainingText(request);
+        Question2 question = validateQuestionExists(request);
+        OptionGroup optionGroup = validateOptionGroupExists(question);
+        validateOnlyIncludingProvidedOptionItem(request, optionGroup);
+    }
+
+    private void validateNotContainingText(CreateReviewAnswerRequest request) {
+        if (!request.text().isBlank()) {
+            throw new CheckBoxAnswerIncludedTextException();
+        }
+    }
+
+    private Question2 validateQuestionExists(CreateReviewAnswerRequest request) {
+        long questionId = request.questionId();
+        return question2Repository.getQuestionById(questionId);
+    }
+
+    private OptionGroup validateOptionGroupExists(Question2 question) {
+        return optionGroupRepository.findByQuestionId(question.getId())
+                .orElseThrow(() -> new OptionGroupNotFoundException(question.getId()));
+    }
+
+    private void validateOnlyIncludingProvidedOptionItem(CreateReviewAnswerRequest request, OptionGroup optionGroup) {
+        List<Long> providedOptionItemIds = optionItemRepository.findAllByOptionGroupId(optionGroup.getId())
+                .stream()
+                .map(OptionItem::getId)
+                .toList();
+        List<Long> submittedOptionItemIds = request.selectedOptionIds();
+
+        if (!new HashSet<>(providedOptionItemIds).containsAll(submittedOptionItemIds)) {
+            throw new CheckBoxAnswerIncludedNotProvidedOptionItemException(providedOptionItemIds,
+                    submittedOptionItemIds
+            );
+        }
+    }
+}
