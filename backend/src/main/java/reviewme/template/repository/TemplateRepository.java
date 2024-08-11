bc2868197796ce214cf9c5c7c9eaee3c0c88c42c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-11 14:49:27 +0900

    refactor: 사용하지 않는 메서드 삭제

--- backend/src/main/java/reviewme/template/repository/TemplateRepository.java
+++ backend/src/main/java/reviewme/template/repository/TemplateRepository.java
@@ -1,6 +1,5 @@
 package reviewme.template.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import reviewme.template.domain.Template;
@@ -8,5 +7,4 @@ import reviewme.template.domain.Template;
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Long> {
 
-    Optional<Template> findTopByOrderByIdDesc();
 }
