73d01bc2eaf3b077cdf353f9949c26b99f93aba0
badahertz52 <badahertz52@gmail.com>
2024-08-11 15:54:29 +0900

    refactor: hooks/review 에 index.ts 추가해서 import 코드 리팩토링

---
+++ frontend/src/hooks/review/index.ts
@@ -0,0 +1,4 @@
+export * from './writingCardForm';
+export { default as useGetDataToWrite } from './useGetDataToWrite';
+export { default as useGetDetailedReview } from './useGetDetailedReview';
+export { default as useMutateReview } from './useMutateReview';
