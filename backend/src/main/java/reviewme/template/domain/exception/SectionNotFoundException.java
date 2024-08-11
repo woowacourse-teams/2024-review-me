fef29f6026dbf930f4d837ad76ba7c398314bbc1
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-11 14:48:53 +0900

    refactor: SectionNotFoundException의 로그 레벨 수정 (info -> warn)

--- backend/src/main/java/reviewme/template/domain/exception/SectionNotFoundException.java
+++ backend/src/main/java/reviewme/template/domain/exception/SectionNotFoundException.java
@@ -8,6 +8,6 @@ public class SectionNotFoundException extends NotFoundException {
 
     public SectionNotFoundException(long id) {
         super("서버 내부에서 문제가 발생했어요. 서버에 문의해주세요.");
-        log.info("SectionNotFoundException is occurred - id: {}", id);
+        log.warn("SectionNotFoundException is occurred - id: {}", id);
     }
 }
