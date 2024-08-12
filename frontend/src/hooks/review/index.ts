3a902a1757c4e5698cf9b33872f17d82c25a61e8
badahertz52 <badahertz52@gmail.com>
2024-08-12 13:16:32 +0900

    refactor: hooks/review/iondex.ts에서 useGetReviewList export

--- frontend/src/hooks/review/index.ts
+++ frontend/src/hooks/review/index.ts
@@ -2,3 +2,4 @@ export * from './writingCardForm';
 export { default as useGetDataToWrite } from './useGetDataToWrite';
 export { default as useGetDetailedReview } from './useGetDetailedReview';
 export { default as useMutateReview } from './useMutateReview';
+export { default as useGetReviewList } from './useGetReviewList';
