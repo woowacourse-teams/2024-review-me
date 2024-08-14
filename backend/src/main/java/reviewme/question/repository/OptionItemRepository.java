dd8f8d91b15b36331ab56d95a620d1afbe4c4c58
donghoony <aru0504@naver.com>
2024-08-14 22:30:31 +0900

    fix: 쿼리 문법 수정

--- backend/src/main/java/reviewme/question/repository/OptionItemRepository.java
+++ backend/src/main/java/reviewme/question/repository/OptionItemRepository.java
@@ -20,23 +20,23 @@ public interface OptionItemRepository extends JpaRepository<OptionItem, Long> {
 
     @Query(value = """
             SELECT o.id FROM option_item o
-            LEFT JOIN checkbox_answer ca
             LEFT JOIN checkbox_answer_selected_option c
+            ON c.selected_option_id = o.id
+            LEFT JOIN checkbox_answer ca
             ON c.checkbox_answer_id = ca.id
             WHERE ca.review_id = :reviewId
-            AND c.selected_option_id = o.id
             """, nativeQuery = true)
     Set<Long> findSelectedOptionItemIdsByReviewId(long reviewId);
 
     @Query(value = """
             SELECT o.* FROM option_item o
-            LEFT JOIN checkbox_answer ca
             LEFT JOIN checkbox_answer_selected_option c
+            ON c.selected_option_id = o.id
+            LEFT JOIN checkbox_answer ca
             ON c.checkbox_answer_id = ca.id
             WHERE ca.review_id = :reviewId
             AND ca.question_id = :questionId
-            AND c.selected_option_id = o.id
-            ORDER BY o.position ASC
+            ORDER BY o.position
             """, nativeQuery = true)
     List<OptionItem> findSelectedOptionItemsByReviewIdAndQuestionId(long reviewId, long questionId);
 
