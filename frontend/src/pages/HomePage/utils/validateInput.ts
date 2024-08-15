5a064a6aaee119de5fce79e58c17f841859f1a2c
ImxYJL <allensain14@gmail.com>
2024-08-15 16:14:50 +0900

    refactor: 문자열 길이 검증 함수에서 최소 길이도 검증할 수 있도록 변경

--- frontend/src/pages/HomePage/utils/validateInput.ts
+++ frontend/src/pages/HomePage/utils/validateInput.ts
@@ -7,18 +7,30 @@ export const isAlphanumeric = (input: string) => {
   return alphanumericRegex.test(input);
 };
 
-export const isWithinMaxLength = (input: string, length: number) => {
-  return input.length <= length;
+export const isWithinLengthRange = (input: string, end: number, start: number = 0) => {
+  const length = input.length;
+  return length >= start && length <= end;
 };
 
 const MAX_VALID_REVIEW_GROUP_DATA_INPUT = 50;
 
 export const isValidReviewGroupDataInput = (input: string) => {
-  return isNotEmptyInput(input) && isWithinMaxLength(input, MAX_VALID_REVIEW_GROUP_DATA_INPUT);
+  return isNotEmptyInput(input) && isWithinLengthRange(input, MAX_VALID_REVIEW_GROUP_DATA_INPUT);
 };
 
 const MAX_GROUP_ACCESS_CODE_INPUT = 62;
 
 export const isValidAccessCodeInput = (input: string) => {
-  return isNotEmptyInput(input) && isAlphanumeric(input) && isWithinMaxLength(input, MAX_GROUP_ACCESS_CODE_INPUT);
+  return isNotEmptyInput(input) && isAlphanumeric(input) && isWithinLengthRange(input, MAX_GROUP_ACCESS_CODE_INPUT);
+};
+
+export const MIN_PASSWORD_INPUT = 4;
+export const MAX_PASSWORD_INPUT = 20;
+
+export const isValidPasswordInput = (input: string) => {
+  return (
+    isNotEmptyInput(input) &&
+    isAlphanumeric(input) &&
+    isWithinLengthRange(input, MAX_GROUP_ACCESS_CODE_INPUT, MIN_PASSWORD_INPUT)
+  );
 };
