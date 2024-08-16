b9d9508f5ec1cb2ae6161c9233364c6591637830
soosoo22 <lllllh00@naver.com>
2024-08-16 16:36:00 +0900

    refactor: 각 페이지별 경로 상수 처리

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  HOME: '/',
+  REVIEW_LIST: '/user/review-list',
+  REVIEW_WRITING: '/user/review-writing',
+  REVIEW_WRITING_COMPLETE: '/user/review-writing-complete',
+  DETAILED_REVIEW: '/user/detailed-review',
+};
--- frontend/src/hooks/useBreadcrumbPaths.ts
+++ frontend/src/hooks/useBreadcrumbPaths.ts
@@ -1,33 +1,31 @@
 import { useLocation } from 'react-router-dom';
 
 import { Path } from '@/components/common/Breadcrumb';
+import { ROUTES } from '@/constants/routes';
 
 const useBreadcrumbPaths = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   const breadcrumbPathList: Path[] = [
-    { pageName: '연결 페이지', path: '/' }, // TODO: 연결 페이지 경로 결정되면 수정 필요
+    { pageName: '연결 페이지', path: ROUTES.HOME }, // TODO: 연결 페이지 경로 결정되면 수정 필요
   ];
 
-  if (location.pathname === '/user/review-list') {
-    breadcrumbPathList.push({ pageName: '목록 페이지', path: location.pathname });
+  if (pathname === ROUTES.REVIEW_LIST) {
+    breadcrumbPathList.push({ pageName: '목록 페이지', path: pathname });
   }
 
-  if (location.pathname.includes('/user/review-writing/')) {
-    breadcrumbPathList.push({ pageName: '작성 페이지', path: location.pathname });
+  if (pathname.includes(`${ROUTES.REVIEW_WRITING}/`)) {
+    breadcrumbPathList.push({ pageName: '작성 페이지', path: pathname });
   }
 
-  if (location.pathname === '/user/review-writing-complete') {
+  if (pathname === ROUTES.REVIEW_WRITING_COMPLETE) {
+    breadcrumbPathList.push({ pageName: '작성 페이지', path: -1 }, { pageName: '작성 완료 페이지', path: pathname });
+  }
+
+  if (pathname.includes(ROUTES.DETAILED_REVIEW)) {
     breadcrumbPathList.push(
-      { pageName: '작성 페이지', path: -1 },
-      { pageName: '작성 완료 페이지', path: location.pathname },
-    );
-  }
-
-  if (location.pathname.includes('/user/detailed-review/')) {
-    breadcrumbPathList.push(
-      { pageName: '목록 페이지', path: '/user/review-list' },
-      { pageName: '상세 페이지', path: location.pathname },
+      { pageName: '목록 페이지', path: ROUTES.REVIEW_LIST },
+      { pageName: '상세 페이지', path: pathname },
     );
   }
 
