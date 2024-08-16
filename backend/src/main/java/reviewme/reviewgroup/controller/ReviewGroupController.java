eeece954fcb78edec4ad0b833205c40320043bad
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-16 23:47:54 +0900

    feat: ReviewGroupController 리뷰 그룹 조회 기능 추가

--- backend/src/main/java/reviewme/reviewgroup/controller/ReviewGroupController.java
+++ backend/src/main/java/reviewme/reviewgroup/controller/ReviewGroupController.java
@@ -3,24 +3,35 @@ package reviewme.reviewgroup.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reviewme.reviewgroup.service.ReviewGroupService;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationRequest;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationResponse;
-import reviewme.reviewgroup.service.ReviewGroupService;
+import reviewme.reviewgroup.service.dto.ReviewGroupResponse;
+import reviewme.reviewgroup.service.ReviewGroupFindService;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewGroupController implements ReviewGroupApi {
 
     private final ReviewGroupService reviewGroupService;
+    private final ReviewGroupFindService findService;
 
     @PostMapping("/v2/groups")
     public ResponseEntity<ReviewGroupCreationResponse> createReviewGroup(
             @Valid @RequestBody ReviewGroupCreationRequest request
     ) {
         ReviewGroupCreationResponse response = reviewGroupService.createReviewGroup(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v2/groups")
+    public ResponseEntity<ReviewGroupResponse> findReviewGroup(@RequestParam String reviewRequestCode) {
+        ReviewGroupResponse response = findService.findReviewGroup(reviewRequestCode);
         return ResponseEntity.ok(response);
     }
 }
