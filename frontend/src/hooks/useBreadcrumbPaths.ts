755d10f2d18e25294604616f2cd6cf6d9fd126db
soosoo22 <lllllh00@naver.com>
2024-08-16 16:09:40 +0900

    feat: 작성 완료 페이지 경로 추가

--- frontend/src/hooks/useBreadcrumbPaths.ts
+++ frontend/src/hooks/useBreadcrumbPaths.ts
@@ -1,9 +1,11 @@
 import { useLocation } from 'react-router-dom';
 
+import { Path } from '@/components/common/Breadcrumb';
+
 const useBreadcrumbPaths = () => {
   const location = useLocation();
 
-  const breadcrumbPaths = [
+  const breadcrumbPaths: Path[] = [
     { pageName: '연결 페이지', path: '/' }, // TODO: 연결 페이지 경로 결정되면 수정 필요
   ];
 
@@ -11,8 +13,15 @@ const useBreadcrumbPaths = () => {
     breadcrumbPaths.push({ pageName: '목록 페이지', path: location.pathname });
   }
 
-  if (location.pathname === '/user/review-writing') {
+  if (location.pathname.includes('/user/review-writing/')) {
     breadcrumbPaths.push({ pageName: '작성 페이지', path: location.pathname });
+  }
+
+  if (location.pathname === '/user/review-writing-complete') {
+    breadcrumbPaths.push(
+      { pageName: '작성 페이지', path: -1 },
+      { pageName: '작성 완료 페이지', path: location.pathname },
+    );
   }
 
   if (location.pathname.includes('/user/detailed-review/')) {
