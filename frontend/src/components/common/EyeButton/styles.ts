b720ca058993c3ee611e11ed425205a2fdfaba3c
ImxYJL <allensain14@gmail.com>
2024-08-16 15:51:04 +0900

    feat: 입력한 비밀번호를 확인할 수 있는 EyeButton 컴포넌트 작성

---
+++ frontend/src/components/common/EyeButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const EyeButton = styled.button`
+  display: inline-block;
+
+  position: absolute;
+  top: 76%;
+  right: 1rem;
+  transform: translateY(-50%);
+
+  width: 2rem;
+  height: 2rem;
+
+  cursor: pointer;
+`;
