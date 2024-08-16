775b8dcdeb227edcaf5a4c3c5d7b6ab4b0b82a4b
ImxYJL <allensain14@gmail.com>
2024-08-16 17:55:21 +0900

    chore: 사용하지 않는 파일을 index에서 제거

--- frontend/src/pages/HomePage/components/index.ts
+++ frontend/src/pages/HomePage/components/index.ts
@@ -1,6 +1,5 @@
 export { default as FormBody } from './FormBody';
 export { default as FormLayout } from './FormLayout';
-export { default as ReviewAccessForm } from './ReviewAccessForm';
 export { default as ReviewURLModal } from './ReviewURLModal';
 export { default as URLGeneratorForm } from './URLGeneratorForm';
 export { default as CopyTextButton } from './CopyTextButton';
