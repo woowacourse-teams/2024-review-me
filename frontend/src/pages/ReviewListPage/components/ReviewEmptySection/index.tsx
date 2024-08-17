1265ffde529ca3034e6ebfc7989800d19082db07
soosoo22 <lllllh00@naver.com>
2024-08-17 16:46:58 +0900

    refactor: 기존 빈 박스 이미지를 없애고 문구만 보여주는 것으로 수정

--- frontend/src/pages/ReviewListPage/components/ReviewEmptySection/index.tsx
+++ frontend/src/pages/ReviewListPage/components/ReviewEmptySection/index.tsx
@@ -1,12 +1,9 @@
-import EmptyBox from '../../../../assets/emptyBox.svg';
-
 import * as S from './styles';
 
 const ReviewEmptySection = () => {
   return (
     <S.Container>
-      <S.NullText>널</S.NullText>
-      <S.EmptyBox src={EmptyBox} alt="빈 박스 이미지" />
+      <S.NullText>널~</S.NullText>
       <S.EmptyReviewsText>아직 받은 리뷰가 없어요!</S.EmptyReviewsText>
     </S.Container>
   );
--- frontend/src/pages/ReviewListPage/components/ReviewEmptySection/styles.ts
+++ frontend/src/pages/ReviewListPage/components/ReviewEmptySection/styles.ts
@@ -1,35 +1,23 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  position: relative;
-
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-
-  width: 100%;
-  padding-top: 25rem;
 `;
 
 export const NullText = styled.span`
-  position: absolute;
-  top: -5rem;
-  left: 22rem;
   transform: rotate(-20deg);
 
-  font-size: 30rem;
+  font-size: 25rem;
   font-weight: ${({ theme }) => theme.fontWeight.bolder};
   color: #3f393b;
-  text-shadow: 1rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
-`;
-
-export const EmptyBox = styled.img`
-  width: 20rem;
+  text-shadow: 2rem 2rem 0.7rem rgba(0, 0, 0, 0.2);
 `;
 
 export const EmptyReviewsText = styled.span`
-  position: absolute;
-  bottom: -4rem;
-  left: 23rem;
+  margin-top: 3rem;
   font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  color: ${({ theme }) => theme.colors.disabledText};
 `;
