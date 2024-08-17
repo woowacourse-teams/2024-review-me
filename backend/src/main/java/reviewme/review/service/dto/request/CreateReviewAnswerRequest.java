7aa0d378f517edfa4df7a427689c8488debf82c4
nayonsoso <panda0329@naver.com>
2024-08-18 03:33:11 +0900

    refactor: Nullable 어노테이션 추가

--- backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
+++ backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
@@ -1,6 +1,7 @@
 package reviewme.review.service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import java.util.List;
 
 @Schema(name = "리뷰 답변 내용")
@@ -10,9 +11,11 @@ public record CreateReviewAnswerRequest(
         long questionId,
 
         @Schema(description = "선택된 옵션 ID 목록", nullable = true)
+        @Nullable
         List<Long> selectedOptionIds,
 
         @Schema(description = "답변 내용", nullable = true)
+        @Nullable
         String text
 ) {
 }
