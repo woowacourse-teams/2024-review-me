e611ea64a6f41b1f30b2eeb096520b779af5d807
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-18 01:32:22 +0900

    refactor: 리뷰 상세 조회 시, 발생하는 예외의 로그 레벨을 error로 변경

--- backend/src/main/java/reviewme/review/domain/exception/MissingCheckboxAnswerForQuestionException.java
+++ backend/src/main/java/reviewme/review/domain/exception/MissingCheckboxAnswerForQuestionException.java
@@ -7,7 +7,7 @@ import reviewme.global.exception.NotFoundException;
 public class MissingCheckboxAnswerForQuestionException extends NotFoundException {
 
     public MissingCheckboxAnswerForQuestionException(long questionId) {
-        super("질문에 해당하는 선택형 답변을 찾지 못했어요.");
-        log.warn("Checkbox Answer not found for questionId: {}", questionId);
+        super("서버 내부에서 문제가 발생했어요. 서버에 문의해주세요.");
+        log.error("Checkbox Answer not found for questionId: {}", questionId);
     }
 }
