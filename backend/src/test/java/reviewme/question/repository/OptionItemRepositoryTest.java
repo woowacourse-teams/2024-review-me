7d9d3ffc6d64ca09209553eef83318661d1aa871
nayonsoso <panda0329@naver.com>
2024-08-18 02:32:36 +0900

    test: fixture 적용
    - 죽복되는 생성자를 대체
    - 변수명 변경으로 의미 전달

--- backend/src/test/java/reviewme/question/repository/OptionItemRepositoryTest.java
+++ backend/src/test/java/reviewme/question/repository/OptionItemRepositoryTest.java
@@ -1,6 +1,10 @@
 package reviewme.question.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static reviewme.fixture.OptionItemFixture.꼬리_질문_선택지;
+import static reviewme.fixture.OptionItemFixture.카테고리_선택지;
+import static reviewme.fixture.QuestionFixture.꼬리_질문_선택형;
+import static reviewme.fixture.QuestionFixture.카테고리_질문_선택형;
 
 import java.util.List;
 import java.util.Set;
@@ -8,9 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import reviewme.question.domain.OptionItem;
-import reviewme.question.domain.OptionType;
 import reviewme.question.domain.Question;
-import reviewme.question.domain.QuestionType;
 import reviewme.review.domain.CheckboxAnswer;
 import reviewme.review.domain.Review;
 import reviewme.review.repository.ReviewRepository;
@@ -30,15 +32,14 @@ class OptionItemRepositoryTest {
     @Test
     void 리뷰_아이디로_선택한_옵션_아이템_아이디를_불러온다() {
         // given
-        long optionId1 = optionItemRepository.save(new OptionItem("1", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId2 = optionItemRepository.save(new OptionItem("2", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId3 = optionItemRepository.save(new OptionItem("3", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId4 = optionItemRepository.save(new OptionItem("4", 0, 1, OptionType.KEYWORD)).getId();
-        optionItemRepository.save(new OptionItem("5", 0, 1, OptionType.KEYWORD));
+        long categoryOptionItemId1 = optionItemRepository.save(카테고리_선택지.create()).getId();
+        long categoryOptionItemId2 = optionItemRepository.save(카테고리_선택지.create()).getId();
+        long followingOptionItemId1 = optionItemRepository.save(꼬리_질문_선택지.create()).getId();
+        optionItemRepository.save(꼬리_질문_선택지.create());
 
         List<CheckboxAnswer> checkboxAnswers = List.of(
-                new CheckboxAnswer(1, List.of(optionId1, optionId2)),
-                new CheckboxAnswer(2, List.of(optionId3, optionId4))
+                new CheckboxAnswer(1, List.of(categoryOptionItemId1, categoryOptionItemId2)),
+                new CheckboxAnswer(2, List.of(followingOptionItemId1))
         );
         Review review = reviewRepository.save(new Review(0, 0, List.of(), checkboxAnswers));
 
@@ -46,34 +47,36 @@ class OptionItemRepositoryTest {
         Set<Long> actual = optionItemRepository.findSelectedOptionItemIdsByReviewId(review.getId());
 
         // then
-        assertThat(actual).containsExactlyInAnyOrder(optionId1, optionId2, optionId3, optionId4);
+        assertThat(actual)
+                .containsExactlyInAnyOrder(categoryOptionItemId1, categoryOptionItemId2, followingOptionItemId1);
     }
 
     @Test
-    void 리뷰_아이디와_질문_아이디로_선택한_옵션_아이템을_순서대로_불러온다() {
+    void 리뷰_아이디와_질문_아이디로_선택한_옵션_아이템을_position_순서대로_불러온다() {
         // given
-        long optionId1 = optionItemRepository.save(new OptionItem("1", 0, 3, OptionType.KEYWORD)).getId();
-        long optionId2 = optionItemRepository.save(new OptionItem("2", 0, 2, OptionType.KEYWORD)).getId();
-        long optionId3 = optionItemRepository.save(new OptionItem("3", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId4 = optionItemRepository.save(new OptionItem("4", 0, 1, OptionType.KEYWORD)).getId();
-        optionItemRepository.save(new OptionItem("5", 0, 1, OptionType.KEYWORD));
+        long categoryOptionItemId1 = optionItemRepository.save(카테고리_선택지.createWithPosition(1)).getId();
+        long categoryOptionItemId2 = optionItemRepository.save(카테고리_선택지.createWithPosition(2)).getId();
+        long followingOptionItemId1 = optionItemRepository.save(꼬리_질문_선택지.createWithPosition(1)).getId();
+        optionItemRepository.save(꼬리_질문_선택지.createWithPosition(2));
+        long followingOptionItemId3 = optionItemRepository.save(꼬리_질문_선택지.createWithPosition(3)).getId();
 
+        Question categoryQuestion = questionRepository.save(카테고리_질문_선택형.create());
+        Question followingQuestion = questionRepository.save(꼬리_질문_선택형.create());
         List<CheckboxAnswer> checkboxAnswers = List.of(
-                new CheckboxAnswer(1, List.of(optionId1, optionId3)),
-                new CheckboxAnswer(2, List.of(optionId4))
+                new CheckboxAnswer(categoryQuestion.getId(), List.of(categoryOptionItemId1, categoryOptionItemId2)),
+                new CheckboxAnswer(followingQuestion.getId(), List.of(followingOptionItemId1, followingOptionItemId3))
         );
-        Question question1 = questionRepository.save(new Question(true, QuestionType.CHECKBOX, "질문", null, 1));
-        questionRepository.save(new Question(true, QuestionType.CHECKBOX, "질문", null, 1));
 
         Review review = reviewRepository.save(new Review(0, 0, List.of(), checkboxAnswers));
 
         // when
         List<OptionItem> actual = optionItemRepository.findSelectedOptionItemsByReviewIdAndQuestionId(
-                review.getId(), question1.getId()
+                review.getId(), followingQuestion.getId()
         );
 
         // then
-        assertThat(actual).extracting(OptionItem::getId).containsExactly(optionId3, optionId1);
+        assertThat(actual)
+                .extracting(OptionItem::getId)
+                .containsExactly(followingOptionItemId1, followingOptionItemId3);
     }
-
 }
