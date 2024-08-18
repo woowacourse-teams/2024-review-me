6678643c18a8c077de1f7e8cf9f0f90139e1ed03
badahertz52 <badahertz52@gmail.com>
2024-08-18 19:08:34 +0900

    chore: 오타 수정

--- frontend/src/hooks/reviewGroup/useGetReviewGroupData/test.ts
+++ frontend/src/hooks/reviewGroup/useGetReviewGroupData/test.ts
@@ -4,10 +4,9 @@ import { VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE } from '@/mocks/mockData';
 import QueryClientWrapper from '@/queryTestSetup/QueryClientWrapper';
 
 import useGetReviewGroupData from '.';
-// 아래의 테스트는 로그인이 유효하다는 가정하에서 진행
 
 describe('리뷰 연결 페이지 리뷰 그룹 데이터 요청 테스트', () => {
-  it('유효힌 reviewRequestCode를 사용해야 라뷰 상세 페이지 데이터를 불러온다.', async () => {
+  it('유효한 reviewRequestCode여야 라뷰 상세 페이지 데이터를 불러온다.', async () => {
     const { result } = renderHook(
       () => useGetReviewGroupData({ reviewRequestCode: VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE }),
       {
