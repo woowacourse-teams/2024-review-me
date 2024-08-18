a83cdf63c0bb431b145ce5c87c4587854a3ee5c7
nayonsoso <panda0329@naver.com>
2024-08-18 16:57:08 +0900

    refactor: NotNull 추가

--- backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
+++ backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
@@ -2,13 +2,15 @@ package reviewme.review.service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @Schema(name = "리뷰 답변 내용")
 public record CreateReviewAnswerRequest(
 
         @Schema(description = "질문 ID")
-        long questionId,
+        @NotNull
+        Long questionId,
 
         @Schema(description = "선택된 옵션 ID 목록", nullable = true)
         @Nullable
