11b0da01358d3aec6a78add44811ee5aba98b835
hyeonjilee <skylar12200@gmail.com>
2024-08-18 16:33:50 +0900

    refactor: 작업 내용 충돌 방지를 위해 제거

--- backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
+++ backend/src/main/java/reviewme/review/service/dto/request/CreateReviewAnswerRequest.java
@@ -1,14 +1,12 @@
 package reviewme.review.service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @Schema(name = "리뷰 답변 내용")
 public record CreateReviewAnswerRequest(
 
         @Schema(description = "질문 ID")
-        @NotNull
         long questionId,
 
         @Schema(description = "선택된 옵션 ID 목록", nullable = true)
--- backend/src/main/java/reviewme/review/service/dto/request/CreateReviewRequest.java
+++ backend/src/main/java/reviewme/review/service/dto/request/CreateReviewRequest.java
@@ -1,19 +1,15 @@
 package reviewme.review.service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @Schema(name = "리뷰 생성 요청")
 public record CreateReviewRequest(
 
         @Schema(description = "리뷰 요청 코드")
-        @NotBlank
         String reviewRequestCode,
 
         @Schema(description = "답변 목록")
-        @NotNull
         List<CreateReviewAnswerRequest> answers
 ) {
 }
