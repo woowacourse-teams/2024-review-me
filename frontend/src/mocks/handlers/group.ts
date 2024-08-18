5fbfcc98d15d6a3b2df52331bf119069bd555ed5
ImxYJL <allensain14@gmail.com>
2024-08-18 17:26:47 +0900

    refactor: 새로운 리뷰 URL 생성 API를 위한 모킹 핸들러 및 데이터 추가

--- frontend/src/mocks/handlers/group.ts
+++ frontend/src/mocks/handlers/group.ts
@@ -2,42 +2,22 @@ import { http, HttpResponse } from 'msw';
 
 import endPoint from '@/apis/endpoints';
 
-import { CREATED_GROUP_DATA, INVALID_GROUP_ACCESS_CODE } from '../mockData/group';
+import { CREATED_REVIEW_REQUEST_CODE } from '../mockData/group';
 
-// NOTE: URL 생성 정상 응답
-const postDataForUrl = () => {
-  return http.post(endPoint.postingDataForURL, async () => {
-    return HttpResponse.json(CREATED_GROUP_DATA, { status: 200 });
+// NOTE: reviewRequestCode 생성 정상 응답
+const postDataForReviewRequestCode = () => {
+  return http.post(endPoint.postingDataForReviewRequestCode, async () => {
+    return HttpResponse.json(CREATED_REVIEW_REQUEST_CODE, { status: 200 });
   });
 };
 
-// NOTE: URL 생성 에러 응답
-// const postDataForUrl = () => {
-//   return http.post(endPoint.postingDataForURL, async () => {
+// NOTE: reviewRequestCode 생성 에러 응답
+// const postDataForReviewRequestCode = () => {
+//   return http.post(endPoint.postingDataForReviewRequestCode, async () => {
 //     return HttpResponse.json({ error: '서버 에러 테스트' }, { status: 500 });
 //   });
 // };
 
-// NOTE: 확인 코드 정상 응답
-const getIsValidGroupAccessCode = () => {
-  return http.get(endPoint.gettingReviewList, async () => {
-    return HttpResponse.json({ status: 200 });
-  });
-};
-
-// NOTE: 확인 코드 에러 응답
-// const getIsValidGroupAccessCode = () => {
-//   return http.get(endPoint.gettingReviewList, async () => {
-//     return HttpResponse.json(INVALID_GROUP_ACCESS_CODE, { status: 400 });
-//   });
-// };
-
-// const getIsValidGroupAccessCode = () => {
-//   return http.get(endPoint.gettingReviewList, async () => {
-//     return HttpResponse.json({ error: '서버 에러 테스트' }, { status: 500 });
-//   });
-// };
-
-const groupHandler = [postDataForUrl(), getIsValidGroupAccessCode()];
+const groupHandler = [postDataForReviewRequestCode()];
 
 export default groupHandler;
--- frontend/src/mocks/mockData/group.ts
+++ frontend/src/mocks/mockData/group.ts
@@ -1,12 +1,3 @@
-export const CREATED_GROUP_DATA = {
+export const CREATED_REVIEW_REQUEST_CODE = {
   reviewRequestCode: 'mocked-reviewRequestCode',
-  groupAccessCode: 'mocked-groupAccessCode',
-};
-
-export const INVALID_GROUP_ACCESS_CODE = {
-  type: 'about:blank',
-  title: 'Bad Request',
-  status: 400,
-  detail: '올바르지 않은 확인 코드입니다.',
-  instance: '/reviews',
 };
