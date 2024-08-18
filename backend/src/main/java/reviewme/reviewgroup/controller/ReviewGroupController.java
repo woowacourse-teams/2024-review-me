f3ca1d7ec0f172e21daf89fea5c4f4be69957ecb
nayonsoso <panda0329@naver.com>
2024-08-18 15:20:32 +0900

    feat: 컨트롤러 코드 확인 메서드 생성

--- backend/src/main/java/reviewme/reviewgroup/controller/ReviewGroupController.java
+++ backend/src/main/java/reviewme/reviewgroup/controller/ReviewGroupController.java
@@ -3,12 +3,16 @@ package reviewme.reviewgroup.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reviewme.global.HeaderProperty;
+import reviewme.reviewgroup.service.ReviewGroupService;
+import reviewme.reviewgroup.service.dto.CheckGroupAccessCodeResponse;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationRequest;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationResponse;
-import reviewme.reviewgroup.service.ReviewGroupService;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,16 @@ public class ReviewGroupController implements ReviewGroupApi {
             @Valid @RequestBody ReviewGroupCreationRequest request
     ) {
         ReviewGroupCreationResponse response = reviewGroupService.createReviewGroup(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v2/groups/code")
+    public ResponseEntity<CheckGroupAccessCodeResponse> checkGroupAccessCode(
+            @RequestParam("reviewRequestCode") String reviewRequestCode,
+            @HeaderProperty("GroupAccessCode") String groupAccessCode
+    ) {
+        CheckGroupAccessCodeResponse response
+                = reviewGroupService.checkGroupAccessCode(reviewRequestCode, groupAccessCode);
         return ResponseEntity.ok(response);
     }
 }
