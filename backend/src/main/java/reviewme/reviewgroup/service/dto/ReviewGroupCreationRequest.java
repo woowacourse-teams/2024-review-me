33dd6aaefa5e35af5a57e3547ae6e50de929b4cc
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-19 17:57:29 +0900

    refactor: 그룹 생성 요청 dto에 groupAccessCode 추가

--- backend/src/main/java/reviewme/reviewgroup/service/dto/ReviewGroupCreationRequest.java
+++ backend/src/main/java/reviewme/reviewgroup/service/dto/ReviewGroupCreationRequest.java
@@ -3,10 +3,14 @@ package reviewme.reviewgroup.service.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ReviewGroupCreationRequest(
-        @NotBlank(message = "리뷰이 이름은 공백일 수 없어요.")
+
+        @NotBlank(message = "리뷰이 이름을 입력해주세요.")
         String revieweeName,
 
-        @NotBlank(message = "프로젝트 이름은 공백일 수 없어요.")
-        String projectName
+        @NotBlank(message = "프로젝트 이름을 입력해주세요.")
+        String projectName,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        String groupAccessCode
 ) {
 }
--- backend/src/main/java/reviewme/reviewgroup/service/dto/ReviewGroupCreationResponse.java
+++ backend/src/main/java/reviewme/reviewgroup/service/dto/ReviewGroupCreationResponse.java
@@ -1,7 +1,6 @@
 package reviewme.reviewgroup.service.dto;
 
 public record ReviewGroupCreationResponse(
-        String reviewRequestCode,
-        String groupAccessCode
+        String reviewRequestCode
 ) {
 }
