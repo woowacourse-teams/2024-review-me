eb63b62a35f89a5d955a2c14446c4cf026251c46
ImxYJL <allensain14@gmail.com>
2024-08-19 19:25:20 +0900

    refactor: early return으로 리팩토링

--- frontend/src/hooks/usePasswordValidation.ts
+++ frontend/src/hooks/usePasswordValidation.ts
@@ -5,35 +5,32 @@ import {
   isAlphanumeric,
   MAX_PASSWORD_INPUT,
   MIN_PASSWORD_INPUT,
-} from '../../src/pages/HomePage/utils/validateInput';
+} from '@/pages/HomePage/utils/validateInput';
 
 const INVALID_CHAR_ERROR_MESSAGE = `영문(대/소문자) 및 숫자만 입력할 수 있습니다`;
 const PASSWORD_LENGTH_ERROR_MESSAGE = `${MIN_PASSWORD_INPUT}자부터 ${MAX_PASSWORD_INPUT}자까지 입력할 수 있습니다`;
 
 export const usePasswordValidation = (password: string) => {
   const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-  const [hasBlurred, setHasBlurred] = useState(false); 
+  const [hasBlurred, setHasBlurred] = useState(false);
 
   const validatePassword = () => {
     if (!isWithinLengthRange(password, MAX_PASSWORD_INPUT, MIN_PASSWORD_INPUT)) {
-      setPasswordErrorMessage(PASSWORD_LENGTH_ERROR_MESSAGE);
-      return;
+      return setPasswordErrorMessage(PASSWORD_LENGTH_ERROR_MESSAGE);
     }
     if (!isAlphanumeric(password)) {
-      setPasswordErrorMessage(INVALID_CHAR_ERROR_MESSAGE);
-      return;
+      return setPasswordErrorMessage(INVALID_CHAR_ERROR_MESSAGE);
     }
-    setPasswordErrorMessage(''); 
+    return setPasswordErrorMessage('');
   };
 
   const handlePasswordBlur = () => {
-    setHasBlurred(true); 
+    setHasBlurred(true);
     validatePassword();
   };
 
   useEffect(() => {
     if (hasBlurred) validatePassword();
-    
   }, [password, hasBlurred]);
 
   return {
