272bff401837f8df82e31d06ac60e0297330b80d
Kimprodp <145949635+Kimprodp@users.noreply.github.com>
2024-08-19 14:02:33 +0900

    [BE] refactor: 리뷰 상세 조회 시, 답변이 있는 리뷰만 보여주도록 변경 (#395)
    * refactor: TextAnswers에 QuestionId로 답변을 찾는 기능 추가
    
    * feat: CheckboxAnswer를 관리하는 checkboxAnswers 클래스 추가
    
    * refactor: 리뷰 상세 조회 시, 답변이 있는 질문으로만 구성하도록 변경
    
    * refactor: 사용되지 않는 필드 제거
    
    * test: TextAnswers의 hasAnswerByQuestionId 메서드 테스트 추가
    
    * test: CheckboxAnswers 테스트 추가
    
    * test: 답변이 있는 리뷰만 보여주는 기능 테스트 추가
    
    * refactor: 리뷰 상세 조회 시, 발생하는 예외의 로그 레벨을 error로 변경
    
    * test: CheckboxAnswersTest 테스트명 변경
    
    * refactor: CheckboxAnswerdp reviewId 필드 추가
    
    * refactor: else 를 사용한 가독성 향상
    
    * test: 답변이 없는 경우 섹션이 아닌 질문이 없는지를 확인하도록 변경

--- backend/src/main/java/reviewme/review/domain/exception/MissingCheckboxAnswerForQuestionException.java
+++ backend/src/main/java/reviewme/review/domain/exception/MissingCheckboxAnswerForQuestionException.java
@@ -4,10 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import reviewme.global.exception.NotFoundException;
 
 @Slf4j
-public class MissingTextAnswerForQuestionException extends NotFoundException {
+public class MissingCheckboxAnswerForQuestionException extends NotFoundException {
 
-    public MissingTextAnswerForQuestionException(long questionId) {
+    public MissingCheckboxAnswerForQuestionException(long questionId) {
         super("서버 내부에서 문제가 발생했어요. 서버에 문의해주세요.");
-        log.error("Text Answer not found for questionId: {}", questionId);
+        log.error("Checkbox Answer not found for questionId: {}", questionId);
     }
 }
