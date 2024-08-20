0a1f6164de12980d2ac8b2e2961a728a2fa38084
hyeonjilee <skylar12200@gmail.com>
2024-08-20 10:18:33 +0900

    refactor: 인증정보 불일치 예외 메시지 구체적으로 변경

--- backend/src/main/java/reviewme/review/service/exception/ReviewGroupNotFoundByCodesException.java
+++ backend/src/main/java/reviewme/review/service/exception/ReviewGroupNotFoundByCodesException.java
@@ -7,7 +7,7 @@ import reviewme.global.exception.BadRequestException;
 public class ReviewGroupNotFoundByCodesException extends BadRequestException {
 
     public ReviewGroupNotFoundByCodesException(String reviewRequestCode, String groupAccessCode) {
-        super("코드에 해당하는 리뷰 그룹이 없어요.");
+        super("인증 정보에 해당하는 리뷰 확인 코드와 리뷰 요청 코드를 통해 찾을 수 있는 리뷰 그룹이 없어요.");
         log.info("ReviewGroup not found by codes - reviewRequestCode: {}, groupAccessCode: {}",
                 reviewRequestCode, groupAccessCode);
     }
