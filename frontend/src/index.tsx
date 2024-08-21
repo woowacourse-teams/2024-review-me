8d96a02977af32521031b22f438f434c97f302f8
badahertz52 <badahertz52@gmail.com>
2024-08-21 11:01:50 +0900

    refactor: if 중괄호 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -41,9 +41,9 @@ export function retryFunction(failureCount: number, error: Error): boolean {
   const isServerError = message === API_ERROR_MESSAGE.serverError;
 
   // Fetch API로 인해 발생한 오류인지 확인
-  if (isServerError) {
-    return failureCount < 1; // 500번대 에러이면 한 번 더 재시도
-  }
+  // 500번대 에러이면 한 번 더 재시도
+  if (isServerError) return failureCount < 1;
+
   return false; // 그 외의 경우 재시도하지 않음
 }
 
