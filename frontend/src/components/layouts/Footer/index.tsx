87c7f710737959691b5eaad809e5b1e7a352877e
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2024-08-21 15:10:48 +0900

    [FE] chore: 리뷰미 저작권 표시 및 Icons8 저작권 표기를 공식 문서대로 변경 (#494)
    * chore: 리뷰미 저작권 표시 및 Icons8 저작권 표기를 공식 문서대로 변경
    
    * chore: By 대소문자 변경
    
    * chore: 리뷰미 표기 방식 수정

--- frontend/src/components/layouts/Footer/index.tsx
+++ frontend/src/components/layouts/Footer/index.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <S.Footer>
       <div>
         <S.Link href="https://github.com/woowacourse-teams/2024-review-me" aria-label="리뷰미 깃헙 저장소">
-          © review-me
+          ⓒ 2024 review-me All rights reserved
         </S.Link>
       </div>
       <div>
-        Icon By &nbsp;
-        <S.Link href="https://icons8.com/" aria-label="icon8 홈페이지">
-          icon8
+        {'Icons by '}
+        <S.Link href="https://icons8.com/" aria-label="Icons8 홈페이지">
+          Icons8
         </S.Link>
       </div>
     </S.Footer>
