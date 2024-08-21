d27dd6f916f9ebb31ae23ec6140ea80e6277686e
nayonsoso <panda0329@naver.com>
2024-08-21 16:52:14 +0900

    refactor: 로그 레벨 수정

--- backend/src/main/java/reviewme/review/service/exception/SubmittedReviewTemplateNotFoundException.java
+++ backend/src/main/java/reviewme/review/service/exception/SubmittedReviewTemplateNotFoundException.java
@@ -1,13 +1,13 @@
 package reviewme.review.service.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import reviewme.global.exception.BadRequestException;
+import reviewme.global.exception.DataInconsistencyException;
 
 @Slf4j
-public class SubmittedReviewTemplateNotFoundException extends BadRequestException {
+public class SubmittedReviewTemplateNotFoundException extends DataInconsistencyException {
 
     public SubmittedReviewTemplateNotFoundException(long templateId) {
         super("제출된 리뷰의 템플릿이 존재하지 않아요.");
-        log.warn("Submitted review's template not found. templateId: {}", templateId);
+        log.error("Submitted review's template not found. templateId: {}", templateId);
     }
 }
