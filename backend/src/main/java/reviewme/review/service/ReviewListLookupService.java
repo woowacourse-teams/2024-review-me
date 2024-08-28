3cfe514cff7e6aeed3df90d83492336d1fd136dc
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-28 23:48:49 +0900

    refactor: 리뷰 목록 조회 서비스명 변경 및 응답 객체 mapper를 활용하도록 변경

---
+++ backend/src/main/java/reviewme/review/service/ReviewListLookupService.java
@@ -0,0 +1,42 @@
+package reviewme.review.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reviewme.review.domain.exception.ReviewGroupNotFoundByReviewRequestCodeException;
+import reviewme.review.service.dto.response.list.ReceivedReviewsResponse;
+import reviewme.review.service.dto.response.list.ReviewListElementResponse;
+import reviewme.review.service.exception.ReviewGroupUnauthorizedException;
+import reviewme.review.service.module.ReviewListMapper;
+import reviewme.reviewgroup.domain.ReviewGroup;
+import reviewme.reviewgroup.repository.ReviewGroupRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewListLookupService {
+
+    private final ReviewGroupRepository reviewGroupRepository;
+    private final ReviewListMapper reviewListMapper;
+
+    @Transactional(readOnly = true)
+    public ReceivedReviewsResponse getReceivedReviews(String reviewRequestCode, String groupAccessCode) {
+        ReviewGroup reviewGroup = findReviewGroupByRequestCodeOrThrow(reviewRequestCode);
+        validateGroupAccessCode(groupAccessCode, reviewGroup);
+
+        List<ReviewListElementResponse> reviewGroupResponse = reviewListMapper.mapToReviewList(reviewGroup);
+        return new ReceivedReviewsResponse(reviewGroup.getReviewee(), reviewGroup.getProjectName(),
+                reviewGroupResponse);
+    }
+
+    private ReviewGroup findReviewGroupByRequestCodeOrThrow(String reviewRequestCode) {
+        return reviewGroupRepository.findByReviewRequestCode(reviewRequestCode)
+                .orElseThrow(() -> new ReviewGroupNotFoundByReviewRequestCodeException(reviewRequestCode));
+    }
+
+    private static void validateGroupAccessCode(String groupAccessCode, ReviewGroup reviewGroup) {
+        if (!reviewGroup.matchesGroupAccessCode(groupAccessCode)) {
+            throw new ReviewGroupUnauthorizedException(reviewGroup.getId());
+        }
+    }
+}
