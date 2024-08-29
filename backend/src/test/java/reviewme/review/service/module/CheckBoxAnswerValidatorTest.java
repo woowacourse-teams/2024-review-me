76a01c7154b1e50d0b56787b185f20fa58afff93
KIMGYUTAE <kiimwf0819@gmail.com>
2024-08-29 12:16:57 +0900

    test: Validator 테스트 작성

--- backend/src/test/java/reviewme/review/service/module/CheckBoxAnswerValidatorTest.java
+++ backend/src/test/java/reviewme/review/service/module/CheckBoxAnswerValidatorTest.java
@@ -1,4 +1,4 @@
-package reviewme.review.service;
+package reviewme.review.service.module;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -6,28 +6,27 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reviewme.fixture.OptionGroupFixture;
+import reviewme.fixture.QuestionFixture;
 import reviewme.question.domain.OptionGroup;
 import reviewme.question.domain.OptionItem;
 import reviewme.question.domain.OptionType;
 import reviewme.question.domain.Question;
-import reviewme.question.domain.QuestionType;
 import reviewme.question.repository.OptionGroupRepository;
 import reviewme.question.repository.OptionItemRepository;
 import reviewme.question.repository.QuestionRepository;
-import reviewme.review.service.dto.request.CreateReviewAnswerRequest;
+import reviewme.review.domain.CheckboxAnswer;
 import reviewme.review.service.exception.CheckBoxAnswerIncludedNotProvidedOptionItemException;
-import reviewme.review.service.exception.CheckBoxAnswerIncludedTextException;
-import reviewme.review.service.exception.RequiredQuestionNotAnsweredException;
 import reviewme.review.service.exception.SelectedOptionItemCountOutOfRangeException;
 import reviewme.review.service.exception.SubmittedQuestionNotFoundException;
 import reviewme.support.ServiceTest;
 import reviewme.template.domain.exception.OptionGroupNotFoundByQuestionIdException;
 
 @ServiceTest
-class CreateCheckBoxAnswerRequestValidatorTest {
+class CheckBoxAnswerValidatorTest {
 
     @Autowired
-    private CreateCheckBoxAnswerRequestValidator createCheckBoxAnswerRequestValidator;
+    private CheckBoxAnswerValidator checkBoxAnswerValidator;
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -38,65 +37,46 @@ class CreateCheckBoxAnswerRequestValidatorTest {
     @Autowired
     private OptionItemRepository optionItemRepository;
 
+    private QuestionFixture questionFixture = new QuestionFixture();
+    private OptionGroupFixture optionGroupFixture = new OptionGroupFixture();
     private Question savedQuestion;
 
     @BeforeEach
     void setUp() {
-        savedQuestion = questionRepository.save(new Question(true, QuestionType.CHECKBOX, "질문", null, 1));
+        savedQuestion = questionRepository.save(questionFixture.선택형_필수_질문());
     }
 
     @Test
     void 저장되지_않은_질문에_대한_응답이면_예외가_발생한다() {
         // given
         long notSavedQuestionId = 100L;
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                notSavedQuestionId, List.of(1L), null
-        );
+        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(notSavedQuestionId, List.of(1L));
 
         // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
                 .isInstanceOf(SubmittedQuestionNotFoundException.class);
-    }
-
-    @Test
-    void 선택형_질문에_텍스트_응답을_하면_예외가_발생한다() {
-        // given
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(), List.of(1L), "서술형 응답"
-        );
-
-        // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
-                .isInstanceOf(CheckBoxAnswerIncludedTextException.class);
     }
 
     @Test
     void 저장되지_않은_옵션그룹에_대해_응답하면_예외가_발생한다() {
         // given
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(), List.of(1L), null
-        );
+        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(savedQuestion.getId(), List.of(1L));
 
         // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
                 .isInstanceOf(OptionGroupNotFoundByQuestionIdException.class);
     }
 
-    @Test
-    void 필수_선택형_질문에_응답을_하지_않으면_예외가_발생한다() {
-        // given
-        optionGroupRepository.save(
-                new OptionGroup(savedQuestion.getId(), 1, 3)
-        );
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(),
-                null,
-                null);
-
-        // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
-                .isInstanceOf(RequiredQuestionNotAnsweredException.class);
-    }
+//    @Test
+//    void 필수_선택형_질문에_응답을_하지_않으면_예외가_발생한다() {
+//        // given
+//        optionGroupRepository.save(optionGroupFixture.선택지_그룹(savedQuestion.getId()));
+//        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(savedQuestion.getId(), null);
+//
+//        // when, then
+//        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
+//                .isInstanceOf(QuestionNotAnsweredException.class);
+//    }
 
     @Test
     void 옵션그룹에서_제공하지_않은_옵션아이템을_응답하면_예외가_발생한다() {
@@ -108,12 +88,11 @@ class CreateCheckBoxAnswerRequestValidatorTest {
                 new OptionItem("옵션", savedOptionGroup.getId(), 1, OptionType.KEYWORD)
         );
 
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(), List.of(savedOptionItem.getId() + 1L), null
-        );
+        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(savedQuestion.getId(),
+                List.of(savedOptionItem.getId() + 1L));
 
         // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
                 .isInstanceOf(CheckBoxAnswerIncludedNotProvidedOptionItemException.class);
     }
 
@@ -127,12 +106,10 @@ class CreateCheckBoxAnswerRequestValidatorTest {
                 new OptionItem("옵션1", savedOptionGroup.getId(), 1, OptionType.KEYWORD)
         );
 
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(), List.of(savedOptionItem1.getId()), null
-        );
+        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(savedQuestion.getId(), List.of(savedOptionItem1.getId()));
 
         // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
                 .isInstanceOf(SelectedOptionItemCountOutOfRangeException.class);
     }
 
@@ -149,12 +126,11 @@ class CreateCheckBoxAnswerRequestValidatorTest {
                 new OptionItem("옵션2", savedOptionGroup.getId(), 2, OptionType.KEYWORD)
         );
 
-        CreateReviewAnswerRequest request = new CreateReviewAnswerRequest(
-                savedQuestion.getId(), List.of(savedOptionItem1.getId(), savedOptionItem2.getId()), null
-        );
+        CheckboxAnswer checkboxAnswer = new CheckboxAnswer(
+                savedQuestion.getId(), List.of(savedOptionItem1.getId(), savedOptionItem2.getId()));
 
         // when, then
-        assertThatCode(() -> createCheckBoxAnswerRequestValidator.validate(request))
+        assertThatCode(() -> checkBoxAnswerValidator.validate(checkboxAnswer))
                 .isInstanceOf(SelectedOptionItemCountOutOfRangeException.class);
     }
 }
