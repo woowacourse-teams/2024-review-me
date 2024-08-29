d713703a8dc71d32435274c755977e1e5b91976c
badahertz52 <badahertz52@gmail.com>
2024-08-29 17:53:41 +0900

    refactor:useMultipleChoice의 handleCheckboxChange 리팩토링
    - 선택,해제의 경우에 따른 액션을 함수로 나눔
    - 객관식에서 사용하는 훅 리팩토링에 따른 결과 적용
    - updateOptionChoice 생성

--- frontend/src/hooks/review/writingCardForm/multipleChoice/useMultipleChoice.ts
+++ frontend/src/hooks/review/writingCardForm/multipleChoice/useMultipleChoice.ts
@@ -38,25 +38,48 @@ const useMultipleChoice = ({ question, handleModalOpen }: UseMultipleChoiceProps
     const { id, checked } = event.currentTarget;
     const optionId = Number(id);
 
-    if (isAboveSelectionLimit(optionId)) {
-      return handleLimitGuideOpen(true);
-    }
-    handleLimitGuideOpen(false);
-    // 답변이 달린 카테고리를 해제하려는 경우
-    const isUnCheckCategory = isAnsweredCategoryChanged(optionId);
-    setUnCheckTargetCategoryOptionId(isUnCheckCategory ? optionId : null);
-    handleModalOpen(!!isUnCheckCategory);
+    if (checked) return handleCheckOption(optionId);
 
+    handleUnCheckOption(optionId);
+  };
+
+  const handleCheckOption = (optionId: number) => {
+    // 최대 문항 개수을 넘는 추가 선택 여부인지 판단해 안내 문구를 띄우거나 지움
+    const isOpen = handleLimitGuideOpen(optionId);
+    if (isOpen) return;
+
+    updateOptionChoice({ optionId, checked: true });
+  };
+
+  const handleUnCheckOption = (optionId: number) => {
+    // 최대 문항 개수 선택 안내 문구 해제
+    handleLimitGuideOpen(optionId);
+    /**
+     * 답변이 달린 카테고리를 해제하려는 경우
+     */
+    const isUnCheckCategory = !!isAnsweredTailQuestion(optionId);
+
+    // 답변이 달린 카테고리를 해제하려는 지 여부에 따른  액션
+    // 1. 모달
+    handleCategoryUncheckModal({ isUnCheckCategory, optionId });
+    // 2. 선택된 문항과 답변 업데이트
     if (!isUnCheckCategory) {
-      updateAnswerState({ optionId, checked });
+      updateOptionChoice({ optionId, checked: false });
     }
   };
 
-  const unCheckTargetCategoryOption = () => {
-    if (unCheckTargetCategoryOptionId) {
-      updateAnswerState({ optionId: unCheckTargetCategoryOptionId, checked: false });
-    }
+  interface UpdateOptionChoiceParams {
+    optionId: number;
+    checked: boolean;
+  }
+  /**
+   * change 이벤트가 일어난 객관식 문항의 선택/해제에 따라 상태를 업데이트
+   */
+  const updateOptionChoice = ({ optionId, checked }: UpdateOptionChoiceParams) => {
+    const newSelectedOptionList = updateSelectedOptionList({ optionId, checked });
+    updateAnswerState(newSelectedOptionList);
   };
+
   return {
     isOpenLimitGuide,
     handleCheckboxChange,
