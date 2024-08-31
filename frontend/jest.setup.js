ed76b216998d5e0e710ca82735abefb721988edc
badahertz52 <badahertz52@gmail.com>
2024-08-31 19:12:16 +0900

    ci : jest에서 emotion theme을 읽을 수 있도록 관련 설정 및 패키지 추가

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,5 +1,8 @@
 import server from './src/mocks/server';
+import { matchers } from '@emotion/jest';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+expect.extend(matchers);
