816a2298224edc74fbf9b9dd12d044db6351d3c5
ImxYJL <allensain14@gmail.com>
2024-08-31 12:48:03 +0900

    chore: 불필요한 import 삭제

--- frontend/src/pages/HomePage/components/Inputs/PasswordField.tsx
+++ frontend/src/pages/HomePage/components/Inputs/PasswordField.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { EyeButton, Input } from '@/components';
 import { useEyeButton } from '@/hooks';
--- frontend/src/pages/HomePage/components/Inputs/RevieweeNameField.tsx
+++ frontend/src/pages/HomePage/components/Inputs/RevieweeNameField.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Input } from '@/components';
 import { URLGeneratorFormField } from '@/recoil/urlGeneratorForm';
