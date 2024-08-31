397a67e697d98729e8b6f6c99922041ae679f356
badahertz52 <badahertz52@gmail.com>
2024-08-31 18:27:29 +0900

    fix : jest에서 svg관련 SyntaxError 오류 수정

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
+    '^.+\\.svg$': '<rootDir>/svgTransform.js',
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
