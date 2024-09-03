2e15d95513dbbbbd263acba12cc4af973175f547
nayonsoso <panda0329@naver.com>
2024-09-03 22:13:24 +0900

    refactor: 개행

--- backend/src/test/java/reviewme/fixture/OptionItemFixture.java
+++ backend/src/test/java/reviewme/fixture/OptionItemFixture.java
@@ -5,11 +5,11 @@ import reviewme.question.domain.OptionType;
 
 public class OptionItemFixture {
 
-    public static  OptionItem 선택지(long optionGroupId) {
+    public static OptionItem 선택지(long optionGroupId) {
         return 선택지(optionGroupId, 1);
     }
 
     public static OptionItem 선택지(long optionGroupId, int position) {
-        return new OptionItem("선택지 본문", optionGroupId, 1, OptionType.CATEGORY);
+        return new OptionItem("선택지 본문", optionGroupId, position, OptionType.CATEGORY);
     }
 }
