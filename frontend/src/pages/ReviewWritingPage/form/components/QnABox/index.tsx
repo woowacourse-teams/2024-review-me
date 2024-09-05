b658e5e7c6b8b4d430e0fc9695eaaadad05baf22
ImxYJL <allensain14@gmail.com>
2024-09-05 10:04:02 +0900

    refactor: 질문별 안내 문구를 설정하는 if문 개선

--- frontend/src/pages/ReviewWritingPage/form/components/QnABox/index.tsx
+++ frontend/src/pages/ReviewWritingPage/form/components/QnABox/index.tsx
@@ -16,17 +16,17 @@ const QnABox = ({ question }: QnABoxProps) => {
    * 객관식 문항의 최소,최대 개수에 대한 안내 문구
    */
   const multipleLGuideline = (() => {
-    const { optionGroup, questionType } = question;
+    const { optionGroup } = question;
 
-    // NOTE: 객관식일 경우의 안내 문구 처리
-    if (questionType === 'TEXT') {
+    // NOTE: 주관식일 경우의 안내 문구 처리
+    if (!optionGroup) {
       const guideline = question.required
         ? `(최소 ${TEXT_ANSWER_LENGTH.min}자 ~ 최대 ${TEXT_ANSWER_LENGTH.max}자)`
         : `(최대 ${TEXT_ANSWER_LENGTH.max}자)`;
       return guideline;
     }
 
-    if (!optionGroup) return;
+    // NOTE: 객관식일 경우의 안내 문구 처리
     const { minCount, maxCount } = optionGroup;
 
     const isAllSelectAvailable = maxCount === optionGroup.options.length;
