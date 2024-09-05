96e90df6eb1f74242b7b66ff8dd134869b6a9857
soosoo22 <lllllh00@naver.com>
2024-09-05 09:32:11 +0900

    chore: 불필요한 클래스 제거

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -35,13 +35,6 @@ const globalStyles = css`
     background: #f1f2f4; /*스크롤바 배경 색상 (lightGray)*/
     border-radius: 0.8rem;
   }
-
-  .prevent-drag {
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
 `;
 
 export default globalStyles;
