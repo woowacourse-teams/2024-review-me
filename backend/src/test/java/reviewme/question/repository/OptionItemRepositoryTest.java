177e5fbc355846992226cad61568539707ab110f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-08 11:26:41 +0900

    test: OptionItemRepositoryTest 픽스처 적용

--- backend/src/test/java/reviewme/question/repository/OptionItemRepositoryTest.java
+++ backend/src/test/java/reviewme/question/repository/OptionItemRepositoryTest.java
@@ -1,17 +1,16 @@
 package reviewme.question.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static reviewme.fixture.OptionGroupFixture.선택지_그룹;
+import static reviewme.fixture.OptionItemFixture.선택지;
+import static reviewme.fixture.QuestionFixture.선택형_필수_질문;
 
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import reviewme.question.domain.OptionGroup;
 import reviewme.question.domain.OptionItem;
-import reviewme.question.domain.OptionType;
-import reviewme.question.domain.Question;
-import reviewme.question.domain.QuestionType;
 import reviewme.review.domain.CheckboxAnswer;
 import reviewme.review.domain.Review;
 import reviewme.review.repository.ReviewRepository;
@@ -34,15 +33,18 @@ class OptionItemRepositoryTest {
     @Test
     void 리뷰_아이디로_선택한_옵션_아이템_아이디를_불러온다() {
         // given
-        long optionId1 = optionItemRepository.save(new OptionItem("1", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId2 = optionItemRepository.save(new OptionItem("2", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId3 = optionItemRepository.save(new OptionItem("3", 0, 1, OptionType.KEYWORD)).getId();
-        long optionId4 = optionItemRepository.save(new OptionItem("4", 0, 1, OptionType.KEYWORD)).getId();
-        optionItemRepository.save(new OptionItem("5", 0, 1, OptionType.KEYWORD));
+        long questionId = questionRepository.save(선택형_필수_질문()).getId();
+
+        Long optionGroupId = optionGroupRepository.save(선택지_그룹(questionId)).getId();
+        long optionId1 = optionItemRepository.save(선택지(optionGroupId)).getId();
+        long optionId2 = optionItemRepository.save(선택지(optionGroupId)).getId();
+        long optionId3 = optionItemRepository.save(선택지(optionGroupId)).getId();
+        long optionId4 = optionItemRepository.save(선택지(optionGroupId)).getId();
+        optionItemRepository.save(선택지(optionGroupId));
 
         List<CheckboxAnswer> checkboxAnswers = List.of(
-                new CheckboxAnswer(1, List.of(optionId1, optionId2)),
-                new CheckboxAnswer(2, List.of(optionId3, optionId4))
+                new CheckboxAnswer(questionId, List.of(optionId1, optionId2)),
+                new CheckboxAnswer(questionId, List.of(optionId3, optionId4))
         );
         Review review = reviewRepository.save(new Review(0, 0, List.of(), checkboxAnswers));
 
@@ -56,26 +58,26 @@ class OptionItemRepositoryTest {
     @Test
     void 리뷰_아이디와_질문_아이디로_선택한_옵션_아이템을_순서대로_불러온다() {
         // given
-        Question question1 = questionRepository.save(new Question(true, QuestionType.CHECKBOX, "질문1", null, 1));
-        Question question2 = questionRepository.save(new Question(true, QuestionType.CHECKBOX, "질문2", null, 2));
+        long questionId1 = questionRepository.save(선택형_필수_질문()).getId();
+        long questionId2 = questionRepository.save(선택형_필수_질문()).getId();
 
-        long optionGroupId = optionGroupRepository.save(new OptionGroup(question1.getId(), 1, 3)).getId();
-        long optionId1 = optionItemRepository.save(new OptionItem("1", optionGroupId, 3, OptionType.KEYWORD)).getId();
-        long optionId2 = optionItemRepository.save(new OptionItem("2", optionGroupId, 2, OptionType.KEYWORD)).getId();
-        long optionId3 = optionItemRepository.save(new OptionItem("3", optionGroupId, 1, OptionType.KEYWORD)).getId();
-        long optionId4 = optionItemRepository.save(new OptionItem("4", optionGroupId, 1, OptionType.KEYWORD)).getId();
-        long optionId5 = optionItemRepository.save(new OptionItem("5", optionGroupId, 1, OptionType.KEYWORD)).getId();
+        long optionGroupId = optionGroupRepository.save(선택지_그룹(questionId1)).getId();
+        long optionId1 = optionItemRepository.save(선택지(optionGroupId, 3)).getId();
+        long optionId2 = optionItemRepository.save(선택지(optionGroupId, 2)).getId();
+        long optionId3 = optionItemRepository.save(선택지(optionGroupId, 1)).getId();
+        long optionId4 = optionItemRepository.save(선택지(optionGroupId, 1)).getId();
+        long optionId5 = optionItemRepository.save(선택지(optionGroupId, 1)).getId();
 
         List<CheckboxAnswer> checkboxAnswers = List.of(
-                new CheckboxAnswer(question1.getId(), List.of(optionId1, optionId3)),
-                new CheckboxAnswer(question2.getId(), List.of(optionId4))
+                new CheckboxAnswer(questionId1, List.of(optionId1, optionId3)),
+                new CheckboxAnswer(questionId2, List.of(optionId4))
         );
 
         Review review = reviewRepository.save(new Review(0, 0, List.of(), checkboxAnswers));
 
         // when
         List<OptionItem> actual = optionItemRepository.findSelectedOptionItemsByReviewIdAndQuestionId(
-                review.getId(), question1.getId()
+                review.getId(), questionId1
         );
 
         // then
