ab032598ae223aee3378ae1b67ab23bf37752eff
nayonsoso <panda0329@naver.com>
2024-09-09 15:49:28 +0900

    feat: 타임존 설정

--- backend/src/main/java/reviewme/ReviewMeApplication.java
+++ backend/src/main/java/reviewme/ReviewMeApplication.java
@@ -1,5 +1,6 @@
 package reviewme;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReviewMeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(ReviewMeApplication.class, args);
     }
 }
