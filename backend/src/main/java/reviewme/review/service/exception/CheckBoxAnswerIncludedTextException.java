757381174eb242a80108496e4e2b84f39f1c3ba0
hyeonjilee <skylar12200@gmail.com>
2024-09-10 17:39:59 +0900

    refactor: 로그에 부족한 입력값 보강

--- backend/src/main/java/reviewme/review/service/exception/CheckBoxAnswerIncludedTextException.java
+++ backend/src/main/java/reviewme/review/service/exception/CheckBoxAnswerIncludedTextException.java
@@ -6,8 +6,8 @@ import reviewme.global.exception.BadRequestException;
 @Slf4j
 public class CheckBoxAnswerIncludedTextException extends BadRequestException {
 
-    public CheckBoxAnswerIncludedTextException() {
-        super("체크박스형 응답은 텍스트를 포함할 수 없어요.");
-        log.warn("CheckBox type answer cannot have option items");
+    public CheckBoxAnswerIncludedTextException(long questionId) {
+        super("체크박스형 응답은 텍스트를 포함할 수 없어요.",
+                "CheckBox type answer cannot have option items - questionId: %d".formatted(questionId));
     }
 }
--- backend/src/main/java/reviewme/review/service/exception/TextAnswerIncludedOptionItemException.java
+++ backend/src/main/java/reviewme/review/service/exception/TextAnswerIncludedOptionItemException.java
@@ -4,7 +4,8 @@ import reviewme.global.exception.BadRequestException;
 
 public class TextAnswerIncludedOptionItemException extends BadRequestException {
 
-    public TextAnswerIncludedOptionItemException() {
-        super("텍스트형 응답은 옵션 항목을 포함할 수 없어요.", "Text type answer cannot have option items");
+    public TextAnswerIncludedOptionItemException(long questionId) {
+        super("텍스트형 응답은 옵션 항목을 포함할 수 없어요."
+                , "Text type answer cannot have option items - questionId: %d".formatted(questionId));
     }
 }
