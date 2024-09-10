a8b5ae55159a84ad0fd8a891ac897dd9fcf47dd5
hyeonjilee <skylar12200@gmail.com>
2024-09-10 20:30:56 +0900

    refactor: 누락된 스택 트레이스 추가

--- backend/src/main/java/reviewme/question/domain/exception/MissingOptionItemsInOptionGroupException.java
+++ backend/src/main/java/reviewme/question/domain/exception/MissingOptionItemsInOptionGroupException.java
@@ -8,6 +8,6 @@ public class MissingOptionItemsInOptionGroupException extends DataInconsistencyE
 
     public MissingOptionItemsInOptionGroupException(long optionGroupId) {
         super("서버 내부에서 문제가 발생했어요. 서버에 문의해주세요.");
-        log.error("OptionGroup has no OptionItems - optionGroupId: {}", optionGroupId);
+        log.error("OptionGroup has no OptionItems - optionGroupId: {}", optionGroupId, this);
     }
 }
