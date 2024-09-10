7c6e8da68e45e9ee62ae293857dedae4401bc26d
hyeonjilee <skylar12200@gmail.com>
2024-09-10 17:35:39 +0900

    refactor: UnexpectedRequestException를 BadRequestException으로 통합

--- backend/src/main/java/reviewme/review/service/exception/CheckBoxAnswerIncludedNotProvidedOptionItemException.java
+++ backend/src/main/java/reviewme/review/service/exception/CheckBoxAnswerIncludedNotProvidedOptionItemException.java
@@ -2,16 +2,16 @@ package reviewme.review.service.exception;
 
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import reviewme.global.exception.UnexpectedRequestException;
+import reviewme.global.exception.BadRequestException;
 
 @Slf4j
-public class CheckBoxAnswerIncludedNotProvidedOptionItemException extends UnexpectedRequestException {
+public class CheckBoxAnswerIncludedNotProvidedOptionItemException extends BadRequestException {
 
     public CheckBoxAnswerIncludedNotProvidedOptionItemException(long questionId,
                                                                 List<Long> providedOptionIds,
                                                                 List<Long> submittedOptionIds) {
-        super("제공되는 선택지에 없는 선택지를 응답했어요.");
-        log.warn("Answer included not provided options - questionId:{}, providedOptionIds: {}, submittedOptionIds: {}",
-                questionId, providedOptionIds, submittedOptionIds, this);
+        super("제공되는 선택지에 없는 선택지를 응답했어요.",
+                "Answer included not provided options - questionId: %s, providedOptionIds: %s, submittedOptionIds: %s"
+                        .formatted(questionId, providedOptionIds, submittedOptionIds));
     }
 }
