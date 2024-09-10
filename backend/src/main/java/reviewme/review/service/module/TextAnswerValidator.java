6a29c0ea3f33770106957499a1f89cb97ac6478c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-10 10:29:54 +0900

    refactor: 필수 질문이 아닌 경우 최소 글자수 제한을 받지 않도록 변경

--- backend/src/main/java/reviewme/review/service/module/TextAnswerValidator.java
+++ backend/src/main/java/reviewme/review/service/module/TextAnswerValidator.java
@@ -2,6 +2,7 @@ package reviewme.review.service.module;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import reviewme.question.domain.Question;
 import reviewme.question.repository.QuestionRepository;
 import reviewme.review.domain.TextAnswer;
 import reviewme.review.domain.exception.InvalidTextAnswerLengthException;
@@ -11,26 +12,28 @@ import reviewme.review.service.exception.SubmittedQuestionNotFoundException;
 @RequiredArgsConstructor
 public class TextAnswerValidator {
 
+    private static final int ZERO_LENGTH = 0;
     private static final int MIN_LENGTH = 20;
     private static final int MAX_LENGTH = 1_000;
 
     private final QuestionRepository questionRepository;
 
     public void validate(TextAnswer textAnswer) {
-        validateExistQuestion(textAnswer);
-        validateLength(textAnswer);
+        Question question = questionRepository.findById(textAnswer.getQuestionId())
+                .orElseThrow(() -> new SubmittedQuestionNotFoundException(textAnswer.getQuestionId()));
+
+        validateLength(textAnswer, question);
     }
 
-    private void validateExistQuestion(TextAnswer textAnswer) {
-        if (!questionRepository.existsById(textAnswer.getQuestionId())) {
-            throw new SubmittedQuestionNotFoundException(textAnswer.getQuestionId());
-        }
-    }
-
-    private void validateLength(TextAnswer textAnswer) {
+    private void validateLength(TextAnswer textAnswer, Question question) {
         int answerLength = textAnswer.getContent().length();
-        if (answerLength < MIN_LENGTH || answerLength > MAX_LENGTH) {
-            throw new InvalidTextAnswerLengthException(answerLength, MIN_LENGTH, MAX_LENGTH);
+
+        if (question.isRequired() && (answerLength < MIN_LENGTH || answerLength > MAX_LENGTH)) {
+            throw new InvalidTextAnswerLengthException(question.getId(), answerLength, MIN_LENGTH, MAX_LENGTH);
+        }
+
+        if (!question.isRequired() && answerLength > MAX_LENGTH) {
+            throw new InvalidTextAnswerLengthException(question.getId(), answerLength, ZERO_LENGTH, MAX_LENGTH);
         }
     }
 }
