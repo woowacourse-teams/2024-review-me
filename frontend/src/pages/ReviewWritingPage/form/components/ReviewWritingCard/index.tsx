c65fe19317b5adf38eda9f9ecc49b825dac069f9
badahertz52 <badahertz52@gmail.com>
2024-09-11 15:00:44 +0900

    feat: ReviewWritingCard에 memo 적용
    - 답변이 바뀌지 않은 카드의 경우 재렌더링 되지 않도록 함
    
    Co-Authored-By: Fe <64690761+chysis@users.noreply.github.com>

--- frontend/src/pages/ReviewWritingPage/form/components/ReviewWritingCard/index.tsx
+++ frontend/src/pages/ReviewWritingPage/form/components/ReviewWritingCard/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { ReviewWritingCardSection } from '@/types';
 
 import QnABox from '../QnABox';
@@ -21,4 +23,4 @@ const ReviewWritingCard = ({ cardSection }: ReviewWritingCardProps) => {
   );
 };
 
-export default ReviewWritingCard;
+export default React.memo(ReviewWritingCard);
