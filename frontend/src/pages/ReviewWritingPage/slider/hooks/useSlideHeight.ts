c99d9ca4c4a70ddb2591bbaf2de2968e2bd8ef9c
badahertz52 <badahertz52@gmail.com>
2024-09-12 11:08:02 +0900

    chore: 불필요한 주석 삭제

--- frontend/src/pages/ReviewWritingPage/slider/hooks/useSlideHeight.ts
+++ frontend/src/pages/ReviewWritingPage/slider/hooks/useSlideHeight.ts
@@ -8,7 +8,6 @@ const SLIDE_CLASS_NAME = 'reviewWritingCardSlide';
 const REM_UNIT = 10;
 
 const useSlideHeight = ({ currentCardIndex }: UseSlideHeightProps) => {
-  // TODO: slideWidth 필요없어짐 추후 삭제
   const [slideHeight, setSlideHeight] = useState<string>('auto');
   const wrapperRef = useRef<HTMLDivElement | null>(null);
   const makeId = (index: number) => `${SLIDE_CLASS_NAME}_${index}`;
