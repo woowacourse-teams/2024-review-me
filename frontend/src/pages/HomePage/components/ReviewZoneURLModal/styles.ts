32121bd7d9fe778715e52f686a5ac0b55083921a
soosoo22 <lllllh00@naver.com>
2024-09-18 22:48:08 +0900

    feat: 화면 크기에 따른 홈 페이지 내 AlertModal 반응형 구현

--- frontend/src/pages/HomePage/components/ReviewZoneURLModal/styles.ts
+++ frontend/src/pages/HomePage/components/ReviewZoneURLModal/styles.ts
@@ -1,16 +1,46 @@
 import styled from '@emotion/styled';
 
+import media from '@/utils/media';
+
 export const ReviewZoneURLModal = styled.div`
   display: flex;
   flex-direction: column;
   width: 52rem;
   height: 18rem;
+
+  ${media.small`
+    button {
+      display: none;
+    }
+  `}
+`;
+
+export const RequestURLContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  max-width: 100%;
+
+  button {
+    display: none;
+  }
+
+  ${media.small`
+    button {
+      display: block;
+    }
+  `}
 `;
 
 export const ModalTitle = styled.p`
   margin-bottom: 4.5rem;
   font-size: 2rem;
   font-weight: ${({ theme }) => theme.fontWeight.bold};
+
+  ${media.small`
+    font-size: 1.8rem;
+    margin-bottom: 2.2rem;
+  `}
 `;
 
 export const ReviewZoneURLModalItem = styled.div`
@@ -20,11 +50,21 @@ export const ReviewZoneURLModalItem = styled.div`
   justify-content: space-between;
 
   font-size: 1.5rem;
+
+  ${media.small`
+    flex-direction: column;
+    align-items: flex-start;
+  `}
 `;
 
 export const DataName = styled.span`
-  flex: 0.6;
   font-weight: ${({ theme }) => theme.fontWeight.bold};
+  white-space: nowrap;
+  margin-right: 1rem;
+
+  ${media.xSmall`
+    font-size: 1.6rem;
+  `}
 `;
 
 export const Data = styled.span`
@@ -57,4 +97,9 @@ export const WarningMessage = styled.p`
   margin-top: 1rem;
   font-size: smaller;
   color: ${({ theme }) => theme.colors.red};
+
+  ${media.small`
+    margin-top: 0;
+    font-size: 1rem;
+  `}
 `;
