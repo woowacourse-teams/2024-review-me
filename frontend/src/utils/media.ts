db40fb73fb460fa594cfc605caa048b46b61f3a1
badahertz52 <badahertz52@gmail.com>
2024-09-18 16:53:09 +0900

    refactor: BreakpointsKey 타입 추가

--- frontend/src/utils/media.ts
+++ frontend/src/utils/media.ts
@@ -2,12 +2,14 @@ import theme from '@/styles/theme';
 
 const { breakpoints } = theme;
 
+type BreakpointsKey = keyof typeof breakpoints;
+
 type Media = {
-  [key in keyof typeof breakpoints]: string;
+  [key in BreakpointsKey]: string;
 };
 
 export const media = Object.keys(breakpoints).reduce((acc, key) => {
-  const size = breakpoints[key as keyof typeof breakpoints];
+  const size = breakpoints[key as BreakpointsKey];
   const mediaType = key === 'large' ? 'min' : 'max';
 
   return {
