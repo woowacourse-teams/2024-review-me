988d2f1be3d050623151a2830d4633e906abad43
badahertz52 <badahertz52@gmail.com>
2024-09-18 10:16:12 +0900

    fix: emotion 공홈에 맞추어 media 유틸 변경
    - media 파일 속 breakpoints 삭제
    - media 타입 변경

--- frontend/src/utils/media.ts
+++ frontend/src/utils/media.ts
@@ -1,44 +1,19 @@
-import { css, SerializedStyles } from '@emotion/react';
-import { CSSObject } from '@emotion/styled';
-
 import theme from '@/styles/theme';
 
-export type Breakpoints = 'xxSmall' | 'xSmall' | 'small' | 'medium' | 'large';
+const { breakpoints } = theme;
 
-const { breakpointsWidth } = theme;
+type Media = {
+  [key in keyof typeof breakpoints]: string;
+};
 
-export const breakpoints = Object.keys(breakpointsWidth).reduce(
-  (acc, key) => {
-    const size = breakpointsWidth[key as keyof typeof breakpointsWidth];
-    const mediaType = key === 'large' ? 'min' : 'max';
+export const media = Object.keys(breakpoints).reduce((acc, key) => {
+  const size = breakpoints[key as keyof typeof breakpoints];
+  const mediaType = key === 'large' ? 'min' : 'max';
 
-    return {
-      ...acc,
-      [key]: `@media (${mediaType}-width: ${size})`,
-    };
-  },
-  {} as Record<keyof typeof breakpointsWidth, string>,
-);
-
-const media = Object.entries(breakpoints).reduce(
-  (acc, [key, value]) => {
-    acc[key as Breakpoints] = (
-      styles: CSSObject | TemplateStringsArray,
-      ...interpolations: Array<CSSObject | SerializedStyles>
-    ) => css`
-      ${value} {
-        ${css(styles, ...interpolations)}
-      }
-    `;
-    return acc;
-  },
-  {} as Record<
-    Breakpoints,
-    (
-      styles: CSSObject | TemplateStringsArray,
-      ...interpolations: Array<CSSObject | SerializedStyles>
-    ) => SerializedStyles
-  >,
-);
+  return {
+    ...acc,
+    [key]: `@media (${mediaType}-width: ${size})`,
+  };
+}, {} as Media);
 
 export default media;
