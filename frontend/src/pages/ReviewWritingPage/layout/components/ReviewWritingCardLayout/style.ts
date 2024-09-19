d40b450ab821ede7280683a412ebb0d19afed893
badahertz52 <badahertz52@gmail.com>
2024-09-19 15:59:17 +0900

    [FE] refactor:  리뷰 작성 폼의 질문,답변 레이아웃 컴포넌트를 만들어 재사용 (#623)
    * refactor: AnswerGuide,QnABoxLayout 컴포넌트 생성 및 적용
    
    * refactor: ReviewWritingCardLayout 생성 및 적용
    
    * refactor: AnswerRecheckModal에 QnABoxLayout, ReviewWritingCardLayout 적용
    
    * refactor: 컴포넌트명 수정( AnswerGuide -> QuestionGuide)
    
    * refactor: layout 컴포넌트들 layout/components폴더로 이동

--- frontend/src/pages/ReviewWritingPage/layout/components/ReviewWritingCardLayout/style.ts
+++ frontend/src/pages/ReviewWritingPage/layout/components/ReviewWritingCardLayout/style.ts
@@ -1,23 +1,21 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const ReviewWritingCardLayout = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
 export const Header = styled.div`
   width: 100%;
-  height: 5rem;
+  min-height: 5rem;
   padding: 1rem 2rem;
+
+  font-size: ${({ theme }) => theme.fontSize.mediumSmall};
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+
   background-color: ${({ theme }) => theme.colors.lightPurple};
 `;
 
 export const Main = styled.div`
-  padding: 2rem;
-`;
-
-export const Title = styled.span`
-  margin-bottom: 2rem;
-  font-size: ${({ theme }) => theme.fontSize.mediumSmall};
-  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+  padding: 2rem 2rem 1rem 2rem;
 `;
