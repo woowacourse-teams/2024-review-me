d579979a643f18f05809a6d46957c7748eefb2b4
donghoony <aru0504@naver.com>
2024-09-20 21:27:38 +0900

    feat: Mapper 질문 타입별로 추상화

--- backend/src/main/java/reviewme/review/service/mapper/AnswerMapper.java
+++ backend/src/main/java/reviewme/review/service/mapper/AnswerMapper.java
@@ -1,28 +1,12 @@
 package reviewme.review.service.mapper;
 
-import org.springframework.stereotype.Component;
-import reviewme.review.domain.CheckboxAnswer;
-import reviewme.review.domain.TextAnswer;
+import reviewme.question.domain.QuestionType;
+import reviewme.review.domain.Answer;
 import reviewme.review.service.dto.request.ReviewAnswerRequest;
-import reviewme.review.service.exception.CheckBoxAnswerIncludedTextException;
-import reviewme.review.service.exception.TextAnswerIncludedOptionItemException;
 
-@Component
-public class AnswerMapper {
+public interface AnswerMapper {
 
-    public TextAnswer mapToTextAnswer(ReviewAnswerRequest answerRequest) {
-        if (answerRequest.selectedOptionIds() != null) {
-            throw new TextAnswerIncludedOptionItemException(answerRequest.questionId());
-        }
+    boolean supports(QuestionType questionType);
 
-        return new TextAnswer(answerRequest.questionId(), answerRequest.text());
-    }
-
-    public CheckboxAnswer mapToCheckBoxAnswer(ReviewAnswerRequest answerRequest) {
-        if (answerRequest.text() != null) {
-            throw new CheckBoxAnswerIncludedTextException(answerRequest.questionId());
-        }
-
-        return new CheckboxAnswer(answerRequest.questionId(), answerRequest.selectedOptionIds());
-    }
+    Answer mapToAnswer(ReviewAnswerRequest answerRequest);
 }
