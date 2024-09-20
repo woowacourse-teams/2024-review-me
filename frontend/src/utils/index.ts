37451ad6801f73938fa80c2bcf4330d85e4ad452
badahertz52 <badahertz52@gmail.com>
2024-09-20 13:23:30 +0900

    refactor: debounce 함수 선언문을 함수 표현문으로 변경 및 export 방식 변경

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './date';
 export * from './isExistentElement';
 export { default as scrollToTop } from './scrollToTop';
+export { default as debounce } from './debounce';
 export * from './media';
