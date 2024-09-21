4e1f1a1bc0f11a101cb736ee1d211707a0c0cce8
badahertz52 <badahertz52@gmail.com>
2024-09-21 23:59:11 +0900

    design: 425px이하에서 ErrorAlerModal 느낌표 아이콘 사이즈 조정

--- frontend/src/components/common/modals/ErrorAlertModal/styles.ts
+++ frontend/src/components/common/modals/ErrorAlertModal/styles.ts
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import media from '@/utils/media';
+
 export const Contents = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,4 +14,9 @@ export const Contents = styled.div`
 export const AlertTriangle = styled.img`
   width: 6rem;
   height: 6rem;
+
+  ${media.xSmall} {
+    width: 4rem;
+    height: 4rem;
+  }
 `;
