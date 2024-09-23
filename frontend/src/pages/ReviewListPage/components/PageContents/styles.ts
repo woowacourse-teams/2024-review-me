87a3db7e1e2363cc06316dac2fa7606ebf68eaf6
ImxYJL <allensain14@gmail.com>
2024-09-23 15:53:41 +0900

    fix: min-height을 상속받도록 변경

--- frontend/src/pages/ReviewListPage/components/PageContents/styles.ts
+++ frontend/src/pages/ReviewListPage/components/PageContents/styles.ts
@@ -1,16 +1,10 @@
 import styled from '@emotion/styled';
 
-import media from '@/utils/media';
-
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-
-  ${media.medium} {
-    height: 50rem;
-  }
-
   width: 90%;
+  min-height: inherit;
 `;
 
 export const ReviewSection = styled.div`
