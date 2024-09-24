8336c2de3e85674eb1f88c97fd46eb54a74577ec
hyeonjilee <skylar12200@gmail.com>
2024-09-24 21:35:48 +0900

    refactor: profile 적용

--- backend/src/main/java/reviewme/config/ReplicationDatasourceConfig.java
+++ backend/src/main/java/reviewme/config/ReplicationDatasourceConfig.java
@@ -9,9 +9,11 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+@Profile("prod")
 @Configuration
 public class ReplicationDatasourceConfig {
     @Bean(name = "writeDataSource")
