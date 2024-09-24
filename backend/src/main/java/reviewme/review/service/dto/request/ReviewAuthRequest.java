dc27d1a2c65b266e6caae0a5a4c4037756789d27
donghoony <aru0504@naver.com>
2024-09-24 10:15:06 +0900

    style: 개행

--- backend/src/main/java/reviewme/review/service/dto/request/ReviewAuthRequest.java
+++ backend/src/main/java/reviewme/review/service/dto/request/ReviewAuthRequest.java
@@ -3,6 +3,7 @@ package reviewme.review.service.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record ReviewAuthRequest(
+
         @NotBlank(message = "리뷰 요청 코드를 입력하세요.")
         String reviewRequestCode,
 
