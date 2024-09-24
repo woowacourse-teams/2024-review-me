8b37c6e65b4fe695501220941b1130881612d314
badahertz52 <badahertz52@gmail.com>
2024-09-24 10:30:36 +0900

    feat: Brotli 압축 적용
    - Brotli를 지원하지 않는 브라우저를 대비 해, 원본 파일을 삭제하지 않도록 함

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,6 +5,7 @@ const Dotenv = require('dotenv-webpack');
 const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const TerserPlugin = require('terser-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -54,11 +55,19 @@ module.exports = (env, argv) => {
         favicon: './src/favicons/favicon.ico',
       }),
       new CleanWebpackPlugin(),
-      //new BundleAnalyzerPlugin(),
       new Dotenv({
         systemvars: true,
         path: './.env',
       }),
+      new BrotliPlugin({
+        asset: '[path].br[query]', // .br 확장자 설정
+        test: /\.(js|jsx|ts|tsx|css|html|svg)$/, // 압축할 파일 유형
+        threshold: 896, // 8KB 이상의 파일만 압축
+        minRatio: 0.8, // 압축 후 80% 이하로 줄어든 파일만 압축
+        quality: 11, // 최대 압축률 (0~11 사이, 기본값: 11)
+        deleteOriginalAssets: false, // 원본 파일을 삭제하지 않음
+      }),
+      //new BundleAnalyzerPlugin(),
       sentryWebpackPlugin({
         authToken: process.env.SENTRY_AUTH_TOKEN,
         org: 'review-me',
