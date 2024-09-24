8c238a2560211de063ab3770c45d3e98f48ee117
nayonsoso <panda0329@naver.com>
2024-09-24 14:05:29 +0900

    feat: 리뷰그룹에 대한 리뷰를 페이징해서 가져오는 함수 생성

--- backend/src/main/java/reviewme/review/repository/ReviewRepository.java
+++ backend/src/main/java/reviewme/review/repository/ReviewRepository.java
@@ -17,5 +17,14 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
             """, nativeQuery = true)
     List<Review> findAllByGroupId(long reviewGroupId);
 
+    @Query(value = """
+            SELECT r.* FROM review r
+            WHERE r.review_group_id = :reviewGroupId
+            AND r.id < :lastReviewId
+            ORDER BY r.created_at DESC 
+            LIMIT :limit
+            """, nativeQuery = true)
+    List<Review> findByReviewGroupIdWithPagination(long reviewGroupId, long lastReviewId, int limit);
+
     Optional<Review> findByIdAndReviewGroupId(long reviewId, long reviewGroupId);
 }
