fc28d7316ebd4c2a60c2c49cc989c6cfde337d3f
nayonsoso <panda0329@naver.com>
2024-09-24 15:12:19 +0900

    test: 페이지네이션 적용된 리뷰 목록 조회 테스트 작성

--- backend/src/test/java/reviewme/review/service/ReviewListLookupServiceTest.java
+++ backend/src/test/java/reviewme/review/service/ReviewListLookupServiceTest.java
@@ -21,9 +21,10 @@ import reviewme.question.repository.QuestionRepository;
 import reviewme.review.domain.CheckboxAnswer;
 import reviewme.review.domain.Review;
 import reviewme.review.domain.TextAnswer;
-import reviewme.review.service.exception.ReviewGroupNotFoundByReviewRequestCodeException;
 import reviewme.review.repository.ReviewRepository;
 import reviewme.review.service.dto.response.list.ReceivedReviewsResponse;
+import reviewme.review.service.dto.response.list.ReceivedReviewsResponseWithPagination;
+import reviewme.review.service.exception.ReviewGroupNotFoundByReviewRequestCodeException;
 import reviewme.review.service.exception.ReviewGroupUnauthorizedException;
 import reviewme.reviewgroup.domain.ReviewGroup;
 import reviewme.reviewgroup.repository.ReviewGroupRepository;
@@ -108,5 +109,39 @@ class ReviewListLookupServiceTest {
 
         // then
         assertThat(response.reviews()).hasSize(2);
+    }
+
+    @Test
+    void 내가_받은_리뷰_목록을_페이지네이션을_적용하여_반환한다() {
+        // given - 리뷰 그룹 저장
+        String reviewRequestCode = "reviewRequestCode";
+        String groupAccessCode = "groupAccessCode";
+        ReviewGroup reviewGroup = reviewGroupRepository.save(리뷰_그룹(reviewRequestCode, groupAccessCode));
+
+        // given - 질문 저장
+        Question question = questionRepository.save(선택형_필수_질문());
+
+        // given - 섹션, 템플릿 저장
+        Section section = sectionRepository.save(항상_보이는_섹션(List.of(question.getId())));
+        Template template = templateRepository.save(템플릿(List.of(section.getId())));
+
+        // given - 리뷰 답변 저장
+        TextAnswer textAnswer = new TextAnswer(question.getId(), "텍스트형 응답");
+        Review review1 = new Review(template.getId(), reviewGroup.getId(), List.of(textAnswer), List.of());
+        Review review2 = new Review(template.getId(), reviewGroup.getId(), List.of(textAnswer), List.of());
+        Review review3 = new Review(template.getId(), reviewGroup.getId(), List.of(textAnswer), List.of());
+        reviewRepository.saveAll(List.of(review1, review2, review3));
+
+        // when
+        ReceivedReviewsResponseWithPagination response
+                = reviewListLookupService.getReceivedReviewsWithPagination(reviewRequestCode, Long.MAX_VALUE, 2);
+
+        // then
+        assertThat(response.reviews())
+                .hasSize(2)
+                .extracting("reviewId")
+                .containsExactly(review3.getId(), review2.getId());
+        assertThat(response.totalSize())
+                .isEqualTo(3);
     }
 }
