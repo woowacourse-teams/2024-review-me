192cf910a7c8ff62de82a7194a07cb852d072a61
ImxYJL <allensain14@gmail.com>
2024-09-25 15:40:15 +0900

    refactor: ErrorSuspenseContainer가 fallback을 optional 매개변수로 받을 수 있게 변경 및 네비게이션 함수의 이름을 보다 일반적으로 수정

--- frontend/src/components/error/ErrorSuspenseContainer/index.tsx
+++ frontend/src/components/error/ErrorSuspenseContainer/index.tsx
@@ -1,6 +1,6 @@
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { lazy, Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
 import { EssentialPropsWithChildren } from '@/types';
 
@@ -8,11 +8,15 @@ import ErrorFallback from '../ErrorFallback';
 
 const LoadingPage = lazy(() => import('@/pages/LoadingPage'));
 
-const ErrorSuspenseContainer = ({ children }: EssentialPropsWithChildren) => {
+interface ErrorSuspenseContainerProps {
+  fallback?: React.ComponentType<FallbackProps>;
+}
+
+const ErrorSuspenseContainer = ({ children, fallback }: EssentialPropsWithChildren<ErrorSuspenseContainerProps>) => {
   return (
     <QueryErrorResetBoundary>
       {({ reset }) => (
-        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+        <ErrorBoundary FallbackComponent={fallback ?? ErrorFallback} onReset={reset}>
           <Suspense fallback={<LoadingPage />}>{children}</Suspense>
         </ErrorBoundary>
       )}
