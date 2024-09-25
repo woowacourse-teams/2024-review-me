e9af93caef556f5fe2053745ee8a771a24306311
hyeonjilee <skylar12200@gmail.com>
2024-09-25 22:02:27 +0900

    infra: 주석 기호 오류 수정

--- backend/src/main/resources/db/migration/V1__add_index.sql
+++ backend/src/main/resources/db/migration/V1__add_index.sql
@@ -1,3 +1,3 @@
-// 변경 사항 설명: https://github.com/woowacourse-teams/2024-review-me/issues/682
+-- 변경 사항 설명: https://github.com/woowacourse-teams/2024-review-me/issues/682
 CREATE INDEX review_idx_review_group_id ON review (review_group_id);
 CREATE INDEX review_group_idx_review_request_code ON review_group (review_request_code);
