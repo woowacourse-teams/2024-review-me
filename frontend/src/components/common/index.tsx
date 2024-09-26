09fa50ad295c85467111efcb2feae077720fb8b9
soosoo22 <lllllh00@naver.com>
2024-09-26 14:32:54 +0900

    chore: 불필요한 컴포넌트 제거

--- frontend/src/components/common/index.tsx
+++ frontend/src/components/common/index.tsx
@@ -1,7 +1,5 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
-export { default as DropDown } from './DropDown';
-export { default as SearchInput } from './SearchInput';
 export { default as ReviewDate } from './ReviewDate';
 export { default as RevieweeComments } from './RevieweeComments';
 export { default as MultilineTextViewer } from './MultilineTextViewer';
