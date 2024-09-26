49de637e9f7edc63a7b8ec782e5a44949e28930d
chysis <chysiss@naver.com>
2024-09-26 16:34:50 +0900

    feat: 서버에서 상세 페이지 데이터를 받아올 때 문자열 치환 과정을 거치도록 수정

--- frontend/src/pages/DetailedReviewPage/components/DetailedReviewPageContents/index.tsx
+++ frontend/src/pages/DetailedReviewPage/components/DetailedReviewPageContents/index.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { ROUTE_PARAM } from '@/constants';
 import { useGetDetailedReview, useSearchParamAndQuery } from '@/hooks';
 import { ReviewDescription, ReviewSection, KeywordSection } from '@/pages/DetailedReviewPage/components';
 import { reviewRequestCodeAtom } from '@/recoil';
+import substituteString from '@/utils/substituteString';
 
 import * as S from './styles';
 
@@ -23,17 +25,47 @@ const DetailedReviewPageContents = ({ groupAccessCode }: DetailedReviewPageConte
     reviewRequestCode: storedReviewRequestCode,
   });
 
+  const newDetailedReview = useMemo(() => {
+    return {
+      ...detailedReview,
+      sections: detailedReview.sections.map((section) => {
+        const newHeader = substituteString({
+          content: section.header,
+          variables: { revieweeName: detailedReview.revieweeName, projectName: detailedReview.projectName },
+        });
+
+        const newQuestions = section.questions.map((question) => {
+          const newContent = substituteString({
+            content: question.content,
+            variables: { revieweeName: detailedReview.revieweeName, projectName: detailedReview.projectName },
+          });
+
+          return {
+            ...question,
+            content: newContent,
+          };
+        });
+
+        return {
+          ...section,
+          header: newHeader,
+          questions: newQuestions,
+        };
+      }),
+    };
+  }, [detailedReview]);
+
   // TODO: 리뷰 공개/비공개 토글 버튼 기능
   return (
     <S.DetailedReviewPageContents>
       <ReviewDescription
-        projectName={detailedReview.projectName}
-        date={new Date(detailedReview.createdAt)}
-        revieweeName={detailedReview.revieweeName}
+        projectName={newDetailedReview.projectName}
+        date={new Date(newDetailedReview.createdAt)}
+        revieweeName={newDetailedReview.revieweeName}
         isPublic={true}
         handleClickToggleButton={() => console.log('click toggle ')}
       />
-      {detailedReview.sections.map((section) =>
+      {newDetailedReview.sections.map((section) =>
         section.questions.map((question) => (
           <S.ReviewContentContainer key={question.questionId}>
             <ReviewSection question={question.content} answer={question.answer!} />
