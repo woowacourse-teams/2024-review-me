b10f1c0b09b650b336c1f8f95d56da84bed2e87c
chysis <chysiss@naver.com>
2024-09-26 16:43:55 +0900

    refactor: 마지막 글자에 받침이 있는지 판단하는 함수 분리

--- frontend/src/utils/substituteString.ts
+++ frontend/src/utils/substituteString.ts
@@ -1,7 +1,4 @@
-const hasFinalConsonant = (word: string) => {
-  const code = word.charCodeAt(word.length - 1);
-  return (code - 0xac00) % 28 !== 0;
-};
+import hasFinalConsonant from './hasFinalConsonant';
 
 interface SubstituteStringProps {
   content: string;
