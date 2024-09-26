c94d5c2b0929658290ded5d25df45d23f7df7792
Fe <64690761+chysis@users.noreply.github.com>
2024-09-26 19:30:52 +0900

    [FE] 문자열 치환을 통해 앞말과 어울리는 조사를 계산해서 사용할 수 있도록 수정 (#708)
    * feat: 서버에서 받은 데이터를 치환하는 함수 작성
    
    * fix: 조사 치환이 안 되는 문제 수정
    
    * feat: 서버에서 리뷰 작성 질문지를 받아올 때 문자열 치환 과정을 거치도록 수정
    
    * feat: 서버에서 상세 페이지 데이터를 받아올 때 문자열 치환 과정을 거치도록 수정
    
    * refactor: 마지막 글자에 받침이 있는지 판단하는 함수 분리
    
    * feat: 앞말에 어울리는 조사를 계산하는 함수 작성
    
    * chore: 자체적으로 조사를 사용하는 곳에서 앞말에 따라 치환하도록 수정
    
    * feat: 앞말에 영어가 포함되는 경우 가능한 조사를 모두 표시하는 기능 구현
    
    * style: 상수가 아닌 변수명 수정
    
    * refactor: index 적용
    
    * refactor: 변환된 데이터를 저장하는 변수명 수정
    
    * refactor: value 값이 없는 경우 문법 변경

--- frontend/src/pages/DetailedReviewPage/components/DetailedReviewPageContents/index.tsx
+++ frontend/src/pages/DetailedReviewPage/components/DetailedReviewPageContents/index.tsx
@@ -1,6 +1,9 @@
+import { useMemo } from 'react';
+
 import { ROUTE_PARAM } from '@/constants';
 import { useGetDetailedReview, useSearchParamAndQuery } from '@/hooks';
 import { ReviewDescription, ReviewSection, KeywordSection } from '@/pages/DetailedReviewPage/components';
+import { substituteString } from '@/utils';
 
 import * as S from './styles';
 
@@ -13,17 +16,47 @@ const DetailedReviewPageContents = () => {
     reviewId: Number(reviewId),
   });
 
+  const parsedDetailedReview = useMemo(() => {
+    return {
+      ...detailedReview,
+      sections: detailedReview.sections.map((section) => {
+        const newHeader = substituteString({
+          content: section.header,
+          variables: { revieweeName: detailedReview.revieweeName, projectName: detailedReview.projectName },
+        });
+
+        const newQuestions = section.questions.map((question) => {
+          const newContent = substituteString({
+            content: question.content,
+            variables: { revieweeName: detailedReview.revieweeName, projectName: detailedReview.projectName },
+          });
+
+          return {
+            ...question,
+            content: newContent,
+          };
+        });
+
+        return {
+          ...section,
+          header: newHeader,
+          questions: newQuestions,
+        };
+      }),
+    };
+  }, [detailedReview]);
+
   // TODO: 리뷰 공개/비공개 토글 버튼 기능
   return (
     <S.DetailedReviewPageContents>
       <ReviewDescription
-        projectName={detailedReview.projectName}
-        date={new Date(detailedReview.createdAt)}
-        revieweeName={detailedReview.revieweeName}
+        projectName={parsedDetailedReview.projectName}
+        date={new Date(parsedDetailedReview.createdAt)}
+        revieweeName={parsedDetailedReview.revieweeName}
         isPublic={true}
         handleClickToggleButton={() => console.log('click toggle ')}
       />
-      {detailedReview.sections.map((section) =>
+      {parsedDetailedReview.sections.map((section) =>
         section.questions.map((question) => (
           <S.ReviewContentContainer key={question.questionId}>
             <ReviewSection question={question.content} answer={question.answer!} />
---
+++ frontend/src/utils/substituteString.ts
@@ -0,0 +1,32 @@
+import hasFinalConsonant from './hasFinalConsonant';
+
+interface SubstituteStringProps {
+  content: string;
+  variables: { [key: string]: string };
+}
+
+/**
+ * 문자열을 치환하는 함수
+ * 영어를 포함할 경우 두 조사를 모두 반환
+ * ${content/받침없는조사:받침있는조사}의 형식일 경우 조사 계산 후 치환
+ * ${content}의 형식일 경우 단순 치환
+ */
+const substituteString = ({ content, variables }: SubstituteStringProps) => {
+  const regex = /\$\{(\w+)(?:\/([^:]+):([^}]+))?\}/g;
+  const hasAlphabet = /[a-zA-Z]/;
+
+  return content.replace(regex, (_, variableName, particleWithoutFinalConsonant, particleWithFinalConsonant) => {
+    const value = variables[variableName];
+
+    if (hasAlphabet.test(value)) {
+      return value + `${particleWithFinalConsonant}(${particleWithoutFinalConsonant})`;
+    }
+
+    if (!value && particleWithoutFinalConsonant && particleWithFinalConsonant) {
+      return value + (hasFinalConsonant(value) ? particleWithFinalConsonant : particleWithoutFinalConsonant);
+    }
+    return value;
+  });
+};
+
+export default substituteString;
