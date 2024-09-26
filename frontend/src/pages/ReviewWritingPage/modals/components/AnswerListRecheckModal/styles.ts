86a4d7c0be4e478dbe6c447dca0e36935e26ca89
soosoo22 <lllllh00@naver.com>
2024-09-26 22:08:25 +0900

    refactor: 가로 스크롤을 숨기기 위해 AnswerListModalContent 추가

--- frontend/src/pages/ReviewWritingPage/modals/components/AnswerListRecheckModal/styles.ts
+++ frontend/src/pages/ReviewWritingPage/modals/components/AnswerListRecheckModal/styles.ts
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 
 import media from '@/utils/media';
 
+export const AnswerListModalContent = styled.div`
+  width: 100%;
+  overflow-x: hidden;
+`;
+
 export const AnswerListContainer = styled.div`
   position: relative;
 
