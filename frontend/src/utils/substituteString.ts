56ffc6aad8e6276b4e59f08897de4272d38554d6
chysis <chysiss@naver.com>
2024-09-26 15:16:12 +0900

    feat: 서버에서 받은 데이터를 치환하는 함수 작성

---
+++ frontend/src/utils/substituteString.ts
@@ -0,0 +1,24 @@
+const hasFinalConsonant = (word: string) => {
+  const code = word.charCodeAt(word.length - 1);
+  return (code - 0xac00) % 28 != 0;
+};
+
+interface SubstituteStringProps {
+  content: string;
+  variables: { [key: string]: string };
+}
+
+const substituteString = ({ content, variables }: SubstituteStringProps) => {
+  const regex = /\$\{(\w+)(?:\/([^:]+):([^}]+))?\}/g;
+
+  return content.replace(regex, (_, variableName, particleWithoutFinalConsonant, particleWithFinalConsonant) => {
+    const value = variables[variableName];
+    if (value !== undefined && particleWithoutFinalConsonant && particleWithFinalConsonant) {
+      return value + hasFinalConsonant(value) ? particleWithFinalConsonant : particleWithoutFinalConsonant;
+    }
+
+    return value;
+  });
+};
+
+export default substituteString;
