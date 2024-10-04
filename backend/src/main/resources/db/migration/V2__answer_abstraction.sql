d42106f186ee9444e46e0693d9b6fdb5010be8f3
donghoony <aru0504@naver.com>
2024-10-04 11:16:17 +0900

    feat: 테이블 대응 Repository 추가 (복사)

---
+++ backend/src/main/resources/db/migration/V2__answer_abstraction.sql
@@ -0,0 +1,38 @@
+-- answer 테이블을 추상화합니다. 아래와 같은 5개의 마이그레이션 테이블을 생성합니다.
+
+CREATE TABLE new_review (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6) NOT NULL,
+    review_group_id BIGINT NOT NULL,
+    template_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE answer (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    question_id BIGINT NOT NULL,
+    review_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (review_id) REFERENCES new_review (id),
+);
+
+CREATE TABLE new_checkbox_answer (
+    id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES answer (id),
+);
+
+CREATE TABLE new_text_answer (
+    id BIGINT NOT NULL,
+    content varchar(5000) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES answer (id),
+);
+
+CREATE TABLE new_checkbox_answer_selected_option (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    checkbox_answer_id BIGINT NOT NULL,
+    selected_option_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (checkbox_answer_id) REFERENCES new_checkbox_answer (id),
+)
