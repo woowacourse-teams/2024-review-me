d501de87c0626f2536c6a943b7e83ce31d4cbb94
chysis <chysiss@naver.com>
2024-10-06 15:29:33 +0900

    feat: Dropdown 외부 클릭 시 닫히도록 하는 기능 구현

--- frontend/src/components/common/Dropdown/index.tsx
+++ frontend/src/components/common/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import DownArrowIcon from '@/assets/downArrow.svg';
 
@@ -13,12 +13,33 @@ interface DropdownProps {
 const Dropdown = ({ options, selectedOption, handleSelect }: DropdownProps) => {
   const [isOpened, setIsOpened] = useState(false);
 
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
   const handleDropdownButtonClick = () => {
     setIsOpened((prev) => !prev);
   };
 
+  const handleOptionClick = (option: string) => {
+    handleSelect(option);
+    setIsOpened(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpened(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
-    <S.DropdownContainer>
+    <S.DropdownContainer ref={dropdownRef}>
       <S.DropdownButton onClick={handleDropdownButtonClick}>
         <span>{selectedOption}</span>
         <S.ArrowIcon src={DownArrowIcon} $isOpened={isOpened} alt="" />
@@ -27,7 +48,7 @@ const Dropdown = ({ options, selectedOption, handleSelect }: DropdownProps) => {
         <S.OptionContainer>
           {options.map((option, index) => {
             return (
-              <S.OptionItem key={index} id={option} onClick={(e) => handleSelect((e.target as HTMLLIElement).id)}>
+              <S.OptionItem key={index} id={option} onClick={(e) => handleOptionClick((e.target as HTMLLIElement).id)}>
                 {option}
               </S.OptionItem>
             );
