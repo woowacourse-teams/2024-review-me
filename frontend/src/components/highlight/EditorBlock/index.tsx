18c789b3ec6cbe0da0bd53d65066c0be040598bb
badahertz52 <badahertz52@gmail.com>
2024-10-06 23:55:51 +0900

    refactor: splitTextWidthHighlightList 위치 변경(uitls-> EditorBlock)

--- frontend/src/components/highlight/EditorBlock/index.tsx
+++ frontend/src/components/highlight/EditorBlock/index.tsx
@@ -17,6 +17,31 @@ const EditorBlock = ({ block, blockIndex }: EditorBlockProps) => {
     }
     return renderStyledSentenceList();
   };
+  interface SplitTextWithHighlightListParams {
+    text: string;
+    highlightList: Highlight[];
+  }
+
+  /**
+   * 하이라이트에 따라, 블록의 글자를 하이라이트 적용되는 부분과 그렇지 않은 부분으로 나누는 함수
+   */
+  const splitTextWithHighlightList = ({ text, highlightList }: SplitTextWithHighlightListParams) => {
+    const result: { isHighlight: boolean; text: string }[] = [];
+    let currentIndex = 0;
+
+    highlightList.forEach(({ start, end }) => {
+      if (currentIndex < start) {
+        result.push({ isHighlight: false, text: text.slice(currentIndex, start) });
+      }
+      result.push({ isHighlight: true, text: text.slice(start, end + 1) });
+      currentIndex = end + 1;
+    });
+
+    if (currentIndex < text.length) {
+      result.push({ isHighlight: false, text: text.slice(currentIndex) });
+    }
+    return result;
+  };
 
   /**
    * 하이라이트 적용 여부를 반영한 Sentence 컴포넌트를 렌더링하는 함수
