20e266a766ee16a23cf61f980c5e148b001060d5
nayonsoso <panda0329@naver.com>
2024-10-07 12:29:26 +0900

    feat: 중복 요청 처리 인터셉터 생성
    - URI, IP, UserAgent 를 기준으로 1초에 3개보다 많은 요청이 오는 경우, 429를 응답한다. 즉, 4번째 요청부터 거부된다.

---
+++ backend/src/main/java/reviewme/global/DuplicateRequestInterceptor.java
@@ -0,0 +1,57 @@
+package reviewme.global;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import reviewme.global.exception.RequestFrequencyNonNumericException;
+import reviewme.global.exception.TooManyDuplicateRequestException;
+
+@Component
+@RequiredArgsConstructor
+public class DuplicateRequestInterceptor implements HandlerInterceptor {
+
+    private static final int MAX_FREQUENCY = 3;
+    private static final Duration DURATION = Duration.of(1, ChronoUnit.SECONDS);
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (!HttpMethod.POST.matches(request.getMethod())) {
+            return true;
+        }
+
+        String key = generateRequestKey(request);
+        Object value = redisTemplate.opsForValue().get(key);
+
+        if (value == null) {
+            redisTemplate.opsForValue().set(key, 1, DURATION);
+            return true;
+        }
+
+        if (!(value instanceof Integer)) {
+            throw new RequestFrequencyNonNumericException(value);
+        }
+
+        int frequency = (int) value;
+        if (frequency > MAX_FREQUENCY) {
+            throw new TooManyDuplicateRequestException(key);
+        }
+        redisTemplate.opsForValue().set(key, frequency + 1);
+        return true;
+    }
+
+    private String generateRequestKey(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        String remoteAddr = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+
+        return String.format("RequestURI: %s, RemoteAddr: %s, UserAgent: %s", requestURI, remoteAddr, userAgent);
+    }
+}
---
+++ backend/src/main/java/reviewme/global/exception/TooManyDuplicateRequestException.java
@@ -0,0 +1,12 @@
+package reviewme.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TooManyDuplicateRequestException extends ReviewMeException {
+
+    public TooManyDuplicateRequestException(String requestKey) {
+        super("짧은 시간 안에 너무 많은 동일한 요청이 일어났어요");
+        log.info("Too many duplicate request arrived: {}", requestKey);
+    }
+}
