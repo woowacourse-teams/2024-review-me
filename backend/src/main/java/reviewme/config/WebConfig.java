d06ff0f840603ed8fd24a78037a5f321594a87d7
nayonsoso <panda0329@naver.com>
2024-10-07 12:29:49 +0900

    feat: 중복 요청 처리 인터셉터 등록

--- backend/src/main/java/reviewme/config/WebConfig.java
+++ backend/src/main/java/reviewme/config/WebConfig.java
@@ -3,19 +3,29 @@ package reviewme.config;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reviewme.reviewgroup.controller.ReviewGroupSessionResolver;
 import reviewme.reviewgroup.service.ReviewGroupService;
+import reviewme.global.DuplicateRequestInterceptor;
+import reviewme.global.HeaderPropertyArgumentResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final ReviewGroupService reviewGroupService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new ReviewGroupSessionResolver(reviewGroupService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new DuplicateRequestInterceptor(redisTemplate));
     }
 }
