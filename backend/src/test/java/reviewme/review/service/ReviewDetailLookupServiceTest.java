ee09fd0fe27d71ccb29d00dd4cdbb2b179043ad0
nayonsoso <panda0329@naver.com>
2024-10-08 14:25:01 +0900

    refactor: Nested 클래스 이름 영어로 변경

--- backend/src/test/java/reviewme/review/service/ReviewDetailLookupServiceTest.java
+++ backend/src/test/java/reviewme/review/service/ReviewDetailLookupServiceTest.java
@@ -13,6 +13,7 @@ import static reviewme.fixture.SectionFixture.항상_보이는_섹션;
 import static reviewme.fixture.TemplateFixture.템플릿;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -143,7 +144,8 @@ class ReviewDetailLookupServiceTest {
     }
 
     @Nested
-    class 필수가_아닌_답변에_응답하지_않았을_때 {
+    @DisplayName("필수가 아닌 답변에 응답하지 않았을 때")
+    class notAnsweredOptionalQuestion {
 
         @Test
         void 섹션에_필수가_아닌_질문만_있다면_섹션_자체를_반환하지_않는다() {
