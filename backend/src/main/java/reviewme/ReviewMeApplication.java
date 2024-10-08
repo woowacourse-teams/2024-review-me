4cb71d56bf60834cc1d5c21d02327bf6fc7dadd9
donghoony <aru0504@naver.com>
2024-10-08 17:21:31 +0900

    refactor: `local`, `dev`, `prod`에서만 캐싱을 활용하도록 변경

--- backend/src/main/java/reviewme/ReviewMeApplication.java
+++ backend/src/main/java/reviewme/ReviewMeApplication.java
@@ -2,10 +2,8 @@ package reviewme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ReviewMeApplication {
 
     public static void main(String[] args) {
