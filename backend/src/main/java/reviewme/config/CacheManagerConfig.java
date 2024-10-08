b35d7afe00cfd722f5ed557dc356441a15a21df4
donghoony <aru0504@naver.com>
2024-10-08 17:21:31 +0900

    refactor: `local`, `dev`, `prod`에서만 캐싱을 활용하도록 변경

--- backend/src/main/java/reviewme/config/CacheManagerConfig.java
+++ backend/src/main/java/reviewme/config/CacheManagerConfig.java
@@ -1,11 +1,15 @@
 package reviewme.config;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile({"local", "dev", "prod"})
 @Configuration
+@EnableCaching
 public class CacheManagerConfig {
 
     @Bean
