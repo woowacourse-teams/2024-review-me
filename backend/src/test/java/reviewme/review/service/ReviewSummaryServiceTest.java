73f587d0311503e3015f4f24e769cec7509455cb
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-08 17:49:10 +0900

    test: 리뷰 요약 조회 기능 테스트 작성

---
+++ backend/src/test/java/reviewme/review/service/ReviewSummaryServiceTest.java
@@ -0,0 +1,95 @@
+package reviewme.review.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static reviewme.fixture.QuestionFixture.서술형_필수_질문;
+import static reviewme.fixture.SectionFixture.항상_보이는_섹션;
+import static reviewme.fixture.TemplateFixture.템플릿;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reviewme.question.domain.Question;
+import reviewme.question.repository.QuestionRepository;
+import reviewme.review.domain.Review;
+import reviewme.review.repository.ReviewRepository;
+import reviewme.review.service.dto.response.list.ReceivedReviewsSummaryResponse;
+import reviewme.review.service.exception.ReviewGroupNotFoundByReviewRequestCodeException;
+import reviewme.reviewgroup.domain.ReviewGroup;
+import reviewme.reviewgroup.repository.ReviewGroupRepository;
+import reviewme.support.ServiceTest;
+import reviewme.template.domain.Section;
+import reviewme.template.domain.Template;
+import reviewme.template.repository.SectionRepository;
+import reviewme.template.repository.TemplateRepository;
+
+@ServiceTest
+class ReviewSummaryServiceTest {
+
+    @Autowired
+    private ReviewSummaryService reviewSummaryService;
+
+    @Autowired
+    private ReviewGroupRepository reviewGroupRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private TemplateRepository templateRepository;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void 리뷰_그룹에_등록된_리뷰_요약_정보를_반환한다() {
+        // given
+        Question question = questionRepository.save(서술형_필수_질문());
+        Section section = sectionRepository.save(항상_보이는_섹션(List.of(question.getId())));
+        Template template = templateRepository.save(템플릿(List.of(section.getId())));
+
+        ReviewGroup reviewGroup = reviewGroupRepository.save(new ReviewGroup(
+                "ted",
+                "review-me",
+                "reviewRequestCode",
+                "groupAccessCode"
+        ));
+
+        Review review1 = new Review(template.getId(), reviewGroup.getId(), List.of());
+        Review review2 = new Review(template.getId(), reviewGroup.getId(), List.of());
+        Review review3 = new Review(template.getId(), reviewGroup.getId(), List.of());
+        reviewRepository.saveAll(List.of(review1, review2, review3));
+        long numberOfReviews = reviewRepository.count();
+
+        // when
+        ReceivedReviewsSummaryResponse actual = reviewSummaryService.getReviewSummary(
+                reviewGroup.getReviewRequestCode());
+
+        // then
+        assertAll(
+                () -> assertThat(actual.projectName()).isEqualTo(reviewGroup.getProjectName()),
+                () -> assertThat(actual.revieweeName()).isEqualTo(reviewGroup.getReviewee()),
+                () -> assertThat(actual.totalReviewCount()).isEqualTo(numberOfReviews)
+        );
+    }
+
+    @Test
+    void 리뷰_요약_정보_조회시_리뷰_요청_코드가_존재하지_않는_경우_예외가_발생한다() {
+        // given
+        ReviewGroup reviewGroup = reviewGroupRepository.save(new ReviewGroup(
+                "ted",
+                "review-me",
+                "reviewRequestCode",
+                "groupAccessCode"
+        ));
+
+        // when, then
+        assertThatThrownBy(() -> reviewSummaryService.getReviewSummary(
+                reviewGroup.getReviewRequestCode() + "wrong"))
+                .isInstanceOf(ReviewGroupNotFoundByReviewRequestCodeException.class);
+    }
+}
