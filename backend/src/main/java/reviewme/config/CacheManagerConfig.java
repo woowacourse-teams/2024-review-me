cdd2c45a9487b4087def9533a0e56067e3c6ed44
donghoony <aru0504@naver.com>
2024-10-08 16:42:06 +0900

    feat: `ConcurrentHashMapCacheManager` 적용

---
+++ backend/src/main/java/reviewme/config/CacheManagerConfig.java
@@ -0,0 +1,15 @@
+package reviewme.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheManagerConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager();
+    }
+}
