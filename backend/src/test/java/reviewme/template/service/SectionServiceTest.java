be6424bec31b2fa3dc8d24c1dd2d98980e2ac183
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-10 00:58:56 +0900

    test: 섹션 이름 목록 반환 테스트 시, 검증하려는 섹션명을 따로 빼는 것으로 변경

--- backend/src/test/java/reviewme/template/service/SectionServiceTest.java
+++ backend/src/test/java/reviewme/template/service/SectionServiceTest.java
@@ -37,12 +37,16 @@ class SectionServiceTest {
     @Test
     void 템플릿에_있는_섹션_이름_목록을_응답한다() {
         // given
+        String sectionName1 = "섹션1";
+        String sectionName2 = "섹션2";
+        String sectionName3 = "섹션3";
+
         Section visibleSection1 = sectionRepository.save(
-                new Section(VisibleType.ALWAYS, List.of(1L), null, "섹션1", "헤더", 1));
+                new Section(VisibleType.ALWAYS, List.of(1L), null, sectionName1, "헤더", 1));
         Section visibleSection2 = sectionRepository.save(
-                new Section(VisibleType.ALWAYS, List.of(2L), null, "섹션2", "헤더", 2));
+                new Section(VisibleType.ALWAYS, List.of(2L), null, sectionName2, "헤더", 2));
         Section nonVisibleSection = sectionRepository.save(
-                new Section(VisibleType.CONDITIONAL, List.of(1L), 1L, "섹션3", "헤더", 3));
+                new Section(VisibleType.CONDITIONAL, List.of(1L), 1L, sectionName3, "헤더", 3));
         templateRepository.save(
                 템플릿(List.of(nonVisibleSection.getId(), visibleSection2.getId(), visibleSection1.getId())));
 
@@ -53,8 +57,7 @@ class SectionServiceTest {
 
         // then
         assertThat(actual.sections()).extracting(SectionNameResponse::name)
-                .containsExactly(visibleSection1.getSectionName(), visibleSection2.getSectionName(),
-                        nonVisibleSection.getSectionName());
+                .containsExactly(sectionName1, sectionName2, sectionName3);
     }
 
     @Test
