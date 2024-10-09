bc23d3f61324fed9db6e7d840b73e163fb22ba2b
donghoony <aru0504@naver.com>
2024-10-09 15:58:49 +0900

    fix: 세션 값 추가

--- backend/src/main/java/reviewme/highlight/controller/HighlightController.java
+++ backend/src/main/java/reviewme/highlight/controller/HighlightController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import reviewme.highlight.service.HighlightService;
 import reviewme.highlight.service.dto.HighlightsRequest;
 
@@ -16,7 +17,8 @@ public class HighlightController {
     private final HighlightService highlightService;
 
     @PostMapping("/v2/highlight")
-    public ResponseEntity<Void> highlight(@Valid @RequestBody HighlightsRequest request) {
+    public ResponseEntity<Void> highlight(@Valid @RequestBody HighlightsRequest request,
+                                          @SessionAttribute("reviewRequestCode") String reviewRequestCode) {
         highlightService.highlight(request);
         return ResponseEntity.ok().build();
     }
