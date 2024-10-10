cd6a16cf19c91a9385ddd7a2965f7da79ea66855
hyeonjilee <skylar12200@gmail.com>
2024-10-10 17:52:30 +0900

    refactor: 하이라이트 위치 속성 객체에 추가 설정

--- backend/src/main/java/reviewme/highlight/domain/HighlightPosition.java
+++ backend/src/main/java/reviewme/highlight/domain/HighlightPosition.java
@@ -1,16 +1,27 @@
 package reviewme.highlight.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import reviewme.highlight.domain.exception.InvalidHighlightRangeException;
 
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode
 public class HighlightPosition {
 
-    private final long lineIndex;
-    private final long startIndex;
-    private final long endIndex;
+    @Column(name = "line_index", nullable = false)
+    private long lineIndex;
+
+    @Column(name = "start_index", nullable = false)
+    private long startIndex;
+
+    @Column(name = "end_index", nullable = false)
+    private long endIndex;
 
     public HighlightPosition(long lineIndex, long startIndex, long endIndex) {
         validateRange(startIndex, endIndex);
