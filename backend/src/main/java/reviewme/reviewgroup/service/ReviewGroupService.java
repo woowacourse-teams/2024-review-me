168b0f7434e6d92095ab46c2b67c13a4a63623a2
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-10 13:24:29 +0900

    refactor: 리뷰 그룹 생성 시, 템플릿 id를 설정하도록 변경

--- backend/src/main/java/reviewme/reviewgroup/service/ReviewGroupService.java
+++ backend/src/main/java/reviewme/reviewgroup/service/ReviewGroupService.java
@@ -10,15 +10,20 @@ import reviewme.reviewgroup.repository.ReviewGroupRepository;
 import reviewme.reviewgroup.service.dto.CheckValidAccessRequest;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationRequest;
 import reviewme.reviewgroup.service.dto.ReviewGroupCreationResponse;
+import reviewme.template.domain.Template;
+import reviewme.template.repository.TemplateRepository;
+import reviewme.template.service.exception.TemplateNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewGroupService {
 
     private static final int REVIEW_REQUEST_CODE_LENGTH = 8;
+    private static final long DEFAULT_TEMPLATE_ID = 1L;
 
     private final ReviewGroupRepository reviewGroupRepository;
     private final RandomCodeGenerator randomCodeGenerator;
+    private final TemplateRepository templateRepository;
 
     @Transactional
     public ReviewGroupCreationResponse createReviewGroup(ReviewGroupCreationRequest request) {
@@ -27,9 +32,13 @@ public class ReviewGroupService {
             reviewRequestCode = randomCodeGenerator.generate(REVIEW_REQUEST_CODE_LENGTH);
         } while (reviewGroupRepository.existsByReviewRequestCode(reviewRequestCode));
 
+        Template template = templateRepository.findById(DEFAULT_TEMPLATE_ID)
+                .orElseThrow(() -> new TemplateNotFoundException(DEFAULT_TEMPLATE_ID));
+
         ReviewGroup reviewGroup = reviewGroupRepository.save(
                 new ReviewGroup(
-                        request.revieweeName(), request.projectName(), reviewRequestCode, request.groupAccessCode()
+                        request.revieweeName(), request.projectName(), reviewRequestCode, request.groupAccessCode(),
+                        template.getId()
                 )
         );
         return new ReviewGroupCreationResponse(reviewGroup.getReviewRequestCode());
