cbbc95005a476842b2fba58a8a76459079dc846d
hyeonjilee <skylar12200@gmail.com>
2024-10-10 10:24:16 +0900

    db: 하이라이트 조회 성능과 동시성을 위한 복합 인덱스 추가

--- backend/src/main/resources/db/migration/V3__create_highlight_table.sql
+++ backend/src/main/resources/db/migration/V3__create_highlight_table.sql
@@ -1,4 +1,5 @@
 -- highlight 테이블을 생성합니다.
+-- 조회의 성능을 높이고, 인덱스 단위의 잠금으로 여러 사용자가 동시에 테이블에 접근해 수정할 수 있게 review_group_id + questionId 복합 인덱스를 추가합니다.
 
 CREATE TABLE highlight
 (
@@ -11,3 +12,5 @@ CREATE TABLE highlight
     end_index       BIGINT NOT NULl,
     PRIMARY KEY (id)
 );
+
+CREATE INDEX highlight_idx_review_group_id_question_id ON highlight(review_group_id, questionId);
