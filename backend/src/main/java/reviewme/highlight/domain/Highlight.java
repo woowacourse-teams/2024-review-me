55895dbbf4fb82ac8ea8eed60d59af3458a47010
hyeonjilee <skylar12200@gmail.com>
2024-10-11 06:56:25 +0900

    refactor: HighlightPosition 검증, 객체 생성 후 하이라이트 객체 생성하도록 변경

--- backend/src/main/java/reviewme/highlight/domain/Highlight.java
+++ backend/src/main/java/reviewme/highlight/domain/Highlight.java
@@ -29,8 +29,8 @@ public class Highlight {
     @Embedded
     private HighlightPosition highlightPosition;
 
-    public Highlight(long answerId, long lineIndex, long startIndex, long endIndex) {
+    public Highlight(long answerId, HighlightPosition highlightPosition) {
         this.answerId = answerId;
-        this.highlightPosition = new HighlightPosition(lineIndex, startIndex, endIndex);
+        this.highlightPosition = highlightPosition;
     }
 }
