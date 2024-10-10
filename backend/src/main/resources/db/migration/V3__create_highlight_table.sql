1c16507830fcd899a9d0a7f1024fcb70a7e8dbf4
hyeonjilee <skylar12200@gmail.com>
2024-10-10 17:59:27 +0900

    db: 테이블에 questionId, review_group_id 컬럼 제거 반영

--- backend/src/main/resources/db/migration/V3__create_highlight_table.sql
+++ backend/src/main/resources/db/migration/V3__create_highlight_table.sql
@@ -1,16 +1,14 @@
 -- highlight 테이블을 생성합니다.
--- 조회의 성능을 높이고, 인덱스 단위의 잠금으로 여러 사용자가 동시에 테이블에 접근해 수정할 수 있게 review_group_id + questionId 복합 인덱스를 추가합니다.
+-- 조회의 성능을 높이고, 인덱스 단위의 잠금으로 여러 사용자가 동시에 테이블에 접근해 수정할 수 있게 answer_id 컬럼에 인덱스를 추가합니다.
 
 CREATE TABLE highlight
 (
-    id              BIGINT AUTO_INCREMENT,
-    questionId      BIGINT NOT NULL,
-    review_group_id BIGINT NOT NULL,
-    answer_id       BIGINT NOT NULL,
-    line_index      INT NOT NULL,
-    start_index     INT NOT NULL,
-    end_index       INT NOT NULL,
+    id          BIGINT AUTO_INCREMENT,
+    answer_id   BIGINT NOT NULL,
+    line_index  INT    NOT NULL,
+    start_index INT    NOT NULL,
+    end_index   INT    NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE INDEX highlight_idx_review_group_id_question_id ON highlight(review_group_id, questionId);
+CREATE INDEX highlight_idx_answer_id ON highlight (answer_id);
