48264de77bae5e8002ec6054e7d7e904f615faf3
chysis <chysiss@naver.com>
2024-10-10 22:36:34 +0900

    design: 답변 간 간격 설정

--- frontend/src/components/highlight/HighlightEditor/style.ts
+++ frontend/src/components/highlight/HighlightEditor/style.ts
@@ -1,5 +1,15 @@
 import styled from '@emotion/styled';
 
+export const HighlightEditorContainer = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 1rem;
+`;
+
 export const SwitchButtonWrapper = styled.div`
   display: flex;
   justify-content: end;
