d3abfd39d2b6ca87dc5a8c4cc448b013f82dddf1
hyeonjilee <skylar12200@gmail.com>
2024-10-10 15:59:20 +0900

    refactor: jpa 기본 메서드로 쿼리 여러개 나가는 부분을 @Query로 변경

--- backend/src/main/java/reviewme/highlight/repository/HighlightRepository.java
+++ backend/src/main/java/reviewme/highlight/repository/HighlightRepository.java
@@ -1,9 +1,17 @@
 package reviewme.highlight.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import reviewme.highlight.domain.HighLight;
 
 public interface HighlightRepository extends JpaRepository<HighLight, Long> {
 
+    @Modifying
+    @Query(value = """
+            DELETE FROM HighLight h
+            WHERE h.reviewGroupId = :reviewGroupId
+            AND h.questionId = :questionId
+            """)
     void deleteByReviewGroupIdAndQuestionId(long reviewGroupId, long questionId);
 }
