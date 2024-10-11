ad7f0521a161571efd0ed833064b98190be507c4
hyeonjilee <skylar12200@gmail.com>
2024-10-11 17:30:47 +0900

    refactor: null을 처리하도록 DTO의 필드 데이터 타입을 Integer로 변경

--- backend/src/main/java/reviewme/highlight/service/dto/HighlightIndexRangeRequest.java
+++ backend/src/main/java/reviewme/highlight/service/dto/HighlightIndexRangeRequest.java
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.NotNull;
 public record HighlightIndexRangeRequest(
 
         @NotNull(message = "시작 인덱스를 입력해주세요.")
-        int startIndex,
+        Integer startIndex,
 
         @NotNull(message = "끝 인덱스를 입력해주세요.")
-        int endIndex
+        Integer endIndex
 ) {
 }
