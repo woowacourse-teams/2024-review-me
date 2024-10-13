58285ccdd9cd07cef51a440d43c3b284139feaa1
ImxYJL <allensain14@gmail.com>
2024-10-13 18:07:31 +0900

    feat: useNavigateBlocker에 새로고침 및 페이지 삭제 전 alert를 띄우는 기능 추가

--- frontend/src/pages/ReviewWritingPage/form/hooks/useNavigateBlocker.ts
+++ frontend/src/pages/ReviewWritingPage/form/hooks/useNavigateBlocker.ts
@@ -8,6 +8,7 @@ interface UseNavigateBlockerProps {
   isOpenModalDisablingBlocker: boolean;
   openNavigateConfirmModal: () => void;
 }
+
 /**
  * @param isOpenModalDisablingBlocker : 이동 확인 모달과 리뷰 제출 확인 모달 모달들이 열려있는 지 여부 (모달의 이동/제출 버튼 클릭 시 페이지 이동해야하기 때문에 필요)
  * 작성한 답변이 있는 상태에서 작성한 페이지에서 다른 페이지로 이동하려할때 이동을 막거나, 이동을 진행하는 blocker를 반환하는 훅
@@ -17,10 +18,17 @@ const useNavigateBlocker = ({ isOpenModalDisablingBlocker, openNavigateConfirmMo
 
   const isAnswerInProgress = () => {
     if (!answerMap) return false;
-
     return [...answerMap.values()].some((answer) => !!answer.selectedOptionIds?.length || !!answer.text?.length);
   };
 
+  // 페이지 새로고침 및 닫기에 대한 처리 by BeforeUnloadEvent
+  const handleNavigationBlock = (event: BeforeUnloadEvent) => {
+    if (isAnswerInProgress() && !isOpenModalDisablingBlocker) {
+      event.preventDefault();
+    }
+  };
+
+  // 페이지 히스토리에 영향을 주는 페이지 이동은 useBlocker가 처리
   const blocker = useBlocker(({ currentLocation, nextLocation }) => {
     if (isOpenModalDisablingBlocker) return false;
     const isLeavingPage = currentLocation.pathname !== nextLocation.pathname;
@@ -32,6 +40,14 @@ const useNavigateBlocker = ({ isOpenModalDisablingBlocker, openNavigateConfirmMo
       openNavigateConfirmModal();
     }
   }, [blocker]);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleNavigationBlock);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleNavigationBlock);
+    };
+  }, [answerMap, isOpenModalDisablingBlocker]);
 
   return {
     blocker,
