bf8d91bb3a8799fc8b8d7fb1727db69e2d20a89d
badahertz52 <badahertz52@gmail.com>
2024-10-13 20:51:01 +0900

    feat: postMockHighlight 가 쿠키 인증 하도록 기능 추가

--- frontend/src/mocks/handlers/highlight.ts
+++ frontend/src/mocks/handlers/highlight.ts
@@ -2,9 +2,11 @@ import { http, HttpResponse } from 'msw';
 
 import endPoint from '@/apis/endpoints';
 
+import { withAuthCookie } from './cookies';
+
 const postMockHighlight = () =>
-  http.post(endPoint.postingHighlight, async () => {
-    return HttpResponse.json({ status: 200 });
+  http.post(endPoint.postingHighlight, ({ cookies }) => {
+    return withAuthCookie(cookies, () => HttpResponse.json({ status: 200 }));
   });
 
 const highlightHandler = [postMockHighlight()];
