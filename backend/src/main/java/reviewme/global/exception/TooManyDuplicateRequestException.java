a57be0f13474ae3dbc39344b3d417cd79a32da36
nayonsoso <panda0329@naver.com>
2024-10-15 09:35:36 +0900

    refactor: 로그 레벨 수정
    - info -> warn

--- backend/src/main/java/reviewme/global/exception/TooManyDuplicateRequestException.java
+++ backend/src/main/java/reviewme/global/exception/TooManyDuplicateRequestException.java
@@ -7,6 +7,6 @@ public class TooManyDuplicateRequestException extends ReviewMeException {
 
     public TooManyDuplicateRequestException(String requestKey) {
         super("짧은 시간 안에 너무 많은 동일한 요청이 일어났어요");
-        log.info("Too many duplicate request received: {}", requestKey);
+        log.warn("Too many duplicate request received: {}", requestKey);
     }
 }
