206f712330f676f2da90dc1815e710b106dbd01c
hyeonjilee <skylar12200@gmail.com>
2024-10-15 09:54:15 +0900

    refactor: dto 메서드로 대체

--- backend/src/main/java/reviewme/highlight/service/validator/HighlightValidator.java
+++ backend/src/main/java/reviewme/highlight/service/validator/HighlightValidator.java
@@ -1,11 +1,9 @@
 
 package reviewme.highlight.service.validator;
 
-import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import reviewme.highlight.service.dto.HighlightRequest;
 import reviewme.highlight.service.dto.HighlightsRequest;
 import reviewme.highlight.service.exception.SubmittedAnswerAndProvidedAnswerMismatchException;
 import reviewme.review.repository.AnswerRepository;
@@ -24,10 +22,7 @@ public class HighlightValidator {
 
     private void validateQuestionContainsAnswer(HighlightsRequest request) {
         Set<Long> providedAnswerIds = answerRepository.findIdsByQuestionId(request.questionId());
-        List<Long> submittedAnswerIds = request.highlights()
-                .stream()
-                .map(HighlightRequest::answerId)
-                .toList();
+        Set<Long> submittedAnswerIds = request.getUniqueAnswerIds();
 
         if (!providedAnswerIds.containsAll(submittedAnswerIds)) {
             throw new SubmittedAnswerAndProvidedAnswerMismatchException(providedAnswerIds, submittedAnswerIds);
