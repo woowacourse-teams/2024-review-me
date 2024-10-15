c44f104d9491f2b7e9c0f032943a98cc9f1b218a
badahertz52 <badahertz52@gmail.com>
2024-10-15 17:31:35 +0900

    feat: 쿠키 인증 필요한 테스트에서 쿠키 생성 및 삭제하는 유틸 함수 구현 및 적용

--- frontend/src/hooks/highlight/useMutateHighlight/test.tsx
+++ frontend/src/hooks/highlight/useMutateHighlight/test.tsx
@@ -2,9 +2,9 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { act } from 'react';
 
 import { isValidPayload, transformHighlightData } from '@/apis/highlight';
-import { MOCK_AUTH_TOKEN_NAME } from '@/mocks/mockData';
 import QueryClientWrapper from '@/queryTestSetup/QueryClientWrapper';
 import { EditorAnswer, EditorAnswerMap } from '@/types';
+import { testWithAuthCookie } from '@/utils';
 
 import useMutateHighlight, { UseMutateHighlightProps } from '.';
 
@@ -27,22 +27,20 @@ describe('하이라이트 요청 테스트', () => {
       handleErrorModal: () => {},
     };
 
-    //쿠키 생성
-    document.cookie = `${MOCK_AUTH_TOKEN_NAME}=2024-review-me`;
+    const testHighlightAPI = async () => {
+      const data = transformHighlightData(EDITOR_ANSWER_MAP, QUESTION_ID);
+      expect(isValidPayload(data)).toBeTruthy();
 
-    const data = transformHighlightData(EDITOR_ANSWER_MAP, QUESTION_ID);
-    expect(isValidPayload(data)).toBeTruthy();
+      const { result } = renderHook(() => useMutateHighlight(props), {
+        wrapper: QueryClientWrapper,
+      });
 
-    const { result } = renderHook(() => useMutateHighlight(props), {
-      wrapper: QueryClientWrapper,
-    });
+      await act(async () => {
+        await result.current.mutateAsync(EDITOR_ANSWER_MAP);
+        waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+      });
+    };
 
-    await act(async () => {
-      await result.current.mutateAsync(EDITOR_ANSWER_MAP);
-      waitFor(() => expect(result.current.isSuccess).toBeTruthy());
-    });
-
-    // 쿠키 삭제
-    document.cookie = `${MOCK_AUTH_TOKEN_NAME}=; max-age=-1`;
+    await testWithAuthCookie(testHighlightAPI);
   });
 });
---
+++ frontend/src/utils/testUtils.ts
@@ -0,0 +1,13 @@
+import { MOCK_AUTH_TOKEN_NAME } from '@/mocks/mockData';
+
+export const testWithAuthCookie = async (callback: () => Promise<void> | void) => {
+  // 쿠키 추가
+  document.cookie = `${MOCK_AUTH_TOKEN_NAME}=2024-review-me`;
+
+  try {
+    await callback();
+  } finally {
+    // 쿠키 삭제
+    document.cookie = `${MOCK_AUTH_TOKEN_NAME}=; max-age=-1`;
+  }
+};
