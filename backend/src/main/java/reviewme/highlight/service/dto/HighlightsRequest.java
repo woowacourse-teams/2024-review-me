8046c1a764b894c4b884de3a55e1e6253d9f13a8
hyeonjilee <skylar12200@gmail.com>
2024-10-15 09:29:44 +0900

    refactor: dto 필드의 속성을 가져오는 메서드 추가

--- backend/src/main/java/reviewme/highlight/service/dto/HighlightsRequest.java
+++ backend/src/main/java/reviewme/highlight/service/dto/HighlightsRequest.java
@@ -3,6 +3,8 @@ package reviewme.highlight.service.dto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record HighlightsRequest(
 
@@ -12,4 +14,10 @@ public record HighlightsRequest(
         @Valid @NotNull(message = "하이라이트할 부분을 입력해주세요.")
         List<HighlightRequest> highlights
 ) {
+    public Set<Long> getUniqueAnswerIds() {
+        return highlights()
+                .stream()
+                .map(HighlightRequest::answerId)
+                .collect(Collectors.toSet());
+    }
 }
