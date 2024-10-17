bacbf7a5b46a8deb4d941cf6549b49f1430502a8
chysis <chysiss@naver.com>
2024-10-17 17:53:19 +0900

    feat: ReviewInfoData를 관리하는 context 및 provider 정의
    - 리뷰이 이름
    - 프로젝트 이름
    - 총 받은 리뷰 개수
    를 관리합니다
    
    Co-Authored-By: ImxYJL <111052302+ImxYJL@users.noreply.github.com>

---
+++ frontend/src/components/layouts/ReviewDisplayLayout/ReviewInfoDataProvider.tsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+
+import { useReviewInfoData } from './hooks';
+
+interface ReviewInfoData {
+  revieweeName: string;
+  projectName: string;
+  totalReviewCount: number;
+}
+
+// export const ReviewInfoDataContext = createContext<ReviewInfoData | null>(null);
+export const ReviewInfoDataContext = createContext<ReviewInfoData>({
+  revieweeName: '',
+  projectName: '',
+  totalReviewCount: 0,
+});
+
+export const ReviewInfoDataProvider = ({ children }: { children: React.ReactNode }) => {
+  const reviewInfoData = useReviewInfoData();
+
+  return <ReviewInfoDataContext.Provider value={reviewInfoData}>{children}</ReviewInfoDataContext.Provider>;
+};
