d817bddeb4c984a5cf9fa7b857a2513a3ab19c66
badahertz52 <badahertz52@gmail.com>
2024-10-18 15:25:41 +0900

    design: 하이라이트 메뉴 버튼 스타일 변경 (사이즈 조정 및 hover시 배경색 변경)

--- frontend/src/components/highlight/components/HighlightButton/style.ts
+++ frontend/src/components/highlight/components/HighlightButton/style.ts
@@ -1,15 +1,22 @@
 import styled from '@emotion/styled';
 
+import { HIGHLIGHT_BUTTON_WIDTH } from '@/constants';
+
 export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
 
+  width: ${`${HIGHLIGHT_BUTTON_WIDTH / 10}rem`};
+  padding: 0.5rem;
+
+  border-radius: ${({ theme }) => theme.borderRadius.basic};
+
   &:hover {
-    background-color: ${({ theme }) => theme.colors.primaryHover};
+    background-color: ${({ theme }) => theme.colors.lightPurple};
   }
 `;
 export const ButtonIcon = styled.img`
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 1.6rem;
+  height: 1.6rem;
 `;
