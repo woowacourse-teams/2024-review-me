9a5f4c00968d67538227d97c674223514f685949
chysis <chysiss@naver.com>
2024-10-20 01:43:07 +0900

    chore: 뒤로가기를 통해 리뷰 작성 페이지로 갈 수 없게 수정

--- frontend/src/components/common/Breadcrumb/index.tsx
+++ frontend/src/components/common/Breadcrumb/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
+
+import { ROUTE } from '@/constants/route';
 
 import UndraggableWrapper from '../UndraggableWrapper';
 
 import * as S from './styles';
 
-type PathType = string | number;
-
 export interface Path {
   pageName: string;
-  path: PathType;
+  path: string;
 }
 
 interface BreadcrumbProps {
@@ -18,12 +18,13 @@ interface BreadcrumbProps {
 
 const Breadcrumb = ({ pathList }: BreadcrumbProps) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleNavigation = (path: PathType) => {
+  const handleNavigation = (path: string) => {
     if (typeof path === 'number') {
       navigate(path);
     } else {
-      navigate(path);
+      navigate(path, { replace: location.pathname.includes(`/${ROUTE.reviewWritingComplete}`) });
     }
   };
 
