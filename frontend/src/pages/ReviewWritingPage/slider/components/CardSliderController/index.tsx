4c01a9a5aa19bbf241433e7b35e3ec3412c6b19a
soosoo22 <lllllh00@naver.com>
2024-10-22 09:50:05 +0900

    refactor: 활성화 유무 aria-label 추가

--- frontend/src/pages/ReviewWritingPage/slider/components/CardSliderController/index.tsx
+++ frontend/src/pages/ReviewWritingPage/slider/components/CardSliderController/index.tsx
@@ -27,6 +27,7 @@ const NextButton = ({ isAbleNextStep, handleCurrentCardIndex, ...rest }: NextBut
       styleType={styledType}
       type={'button'}
       onClick={() => handleCurrentCardIndex('next')}
+      aria-label={isAbleNextStep ? '활성화된 다음' : '비활성화된 다음'}
       {...rest}
     >
       다음
@@ -50,7 +51,7 @@ const ConfirmModalOpenButton = ({
       styleType={styleType}
       type={'button'}
       onClick={handleSubmitConfirmModalOpenButtonClick}
-      aria-label="제출 버튼입니다, 클릭 시 작성한 내용을 제출합니다"
+      aria-label="제출 버튼, 클릭 시 작성한 내용을 제출합니다"
       {...rest}
     >
       제출
@@ -70,7 +71,7 @@ const RecheckButton = ({ isAbleNextStep, handleRecheckButtonClick, ...rest }: Re
       styleType={styledType}
       type={'button'}
       onClick={handleRecheckButtonClick}
-      aria-label="작성 내용 확인 버튼입니다, 클릭 시 작성한 내용을 한눈에 볼 수 있는 모달이 열립니다"
+      aria-label="작성 내용 확인 버튼, 클릭 시 작성한 내용을 한눈에 볼 수 있는 모달이 열립니다"
       {...rest}
     >
       작성 내용 확인
