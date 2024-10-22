00aa309fb0e192d1e394a47fb6319f7f10cd9d97
badahertz52 <badahertz52@gmail.com>
2024-10-22 15:58:34 +0900

    feat: Amplitude의 OS를 사용하기로 하고, 브라우저 정보를 가져오는 함수 삭제

--- frontend/src/utils/analytics/amplitude.ts
+++ frontend/src/utils/analytics/amplitude.ts
@@ -6,35 +6,6 @@ export const startAmplitude = () => {
   amplitude.init(process.env.APMLITUDE_KEY, { autocapture: false });
 };
 
-const getBrowserInfo = () => {
-  let browserName = 'Unknown';
-  let browserVersion = 'Unknown';
-  const userAgent = navigator.userAgent;
-
-  const BROWSER_LIST = [
-    { name: 'Firefox', versionToken: 'Firefox' },
-    { name: 'Chrome', versionToken: 'Chrome' },
-    { name: 'Safari', versionToken: 'Version', exclude: 'Chrome' },
-    { name: 'Edge', versionToken: 'Edg' },
-    { name: 'Internet Explorer', versionToken: 'MSIE' },
-  ];
-
-  for (const browser of BROWSER_LIST) {
-    if (
-      userAgent.indexOf(browser.versionToken) > -1 &&
-      (!browser.exclude || userAgent.indexOf(browser.exclude) === -1)
-    ) {
-      browserName = browser.name;
-      browserVersion = userAgent
-        .substring(userAgent.indexOf(browser.versionToken) + browser.versionToken.length + 1)
-        .split(' ')[0];
-      break;
-    }
-  }
-
-  return { browserName, browserVersion };
-};
-
 /**
  * 사용자가 사용한 이벤트를 추적하는 메서드
  * @param eventName 이벤트 이름
@@ -55,12 +26,8 @@ export const trackEventInAmplitude = (eventName: string, eventProps: Record<stri
   const { hostname } = window.location;
   const domainName = DOMAIN_MAPPING[hostname] || 'local';
 
-  const { browserName, browserVersion } = getBrowserInfo();
-
   amplitude.track(eventName, {
     domain: domainName,
-    browserName,
-    browserVersion,
     ...eventProps,
   });
 };
