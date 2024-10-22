e101338486d86464140d59047f6a03c3dcb33235
badahertz52 <badahertz52@gmail.com>
2024-10-22 21:04:12 +0900

    fix: 리뷰 목록,리뷰 모아보기 스위치 오류 해결

--- frontend/src/components/layouts/ReviewDisplayLayout/hooks/useReviewDisplayLayoutOptions/index.ts
+++ frontend/src/components/layouts/ReviewDisplayLayout/hooks/useReviewDisplayLayoutOptions/index.ts
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
 import { OptionSwitchOption } from '@/components/common/OptionSwitch';
@@ -17,28 +16,26 @@ const useReviewDisplayLayoutOptions = () => {
 
   const isReviewCollection = pathname.includes(ROUTE.reviewCollection);
 
-  const navigatePage = useCallback(() => {
-    // 리뷰 리스트로 이동
-    if (isReviewCollection) {
-      trackEventInAmplitude(COLLECTION_LIST_SWITCH_EVENT_NAME.list);
-      navigate(`/${ROUTE.reviewList}/${reviewRequestCode}`);
-      return;
-    }
-    // 리뷰 모아보기로 이동
+  const navigateReviewListPage = () => {
+    trackEventInAmplitude(COLLECTION_LIST_SWITCH_EVENT_NAME.list);
+    navigate(`/${ROUTE.reviewList}/${reviewRequestCode}`);
+  };
+
+  const navigateReviewCollectionPage = () => {
     trackEventInAmplitude(COLLECTION_LIST_SWITCH_EVENT_NAME.collection);
     navigate(`/${ROUTE.reviewCollection}/${reviewRequestCode}`);
-  }, [isReviewCollection]);
+  };
 
   const reviewDisplayLayoutOptions: OptionSwitchOption[] = [
     {
       label: '목록보기',
       isChecked: !isReviewCollection,
-      handleOptionClick: navigatePage,
+      handleOptionClick: navigateReviewListPage,
     },
     {
       label: '모아보기',
       isChecked: isReviewCollection,
-      handleOptionClick: navigatePage,
+      handleOptionClick: navigateReviewCollectionPage,
     },
   ];
 
