04eb1b3bd4a1fa8babbe030a8dfa439f88be61a3
badahertz52 <badahertz52@gmail.com>
2024-10-24 21:28:49 +0900

    [FE] fix: 질문 섹션 변경 시, 변경된 형광펜 상태 적용되지 않는 오류 수정 및 리뷰 모아보기 목 핸들러,목 데이터 변경 (#932)

--- frontend/src/pages/ReviewCollectionPage/index.tsx
+++ frontend/src/pages/ReviewCollectionPage/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from 'react';
+
 import { AuthAndServerErrorFallback, ErrorSuspenseContainer, TopButton } from '@/components';
 import ReviewDisplayLayout from '@/components/layouts/ReviewDisplayLayout';
+import { SESSION_STORAGE_KEY } from '@/constants';
 
 import ReviewCollectionPageContents from './components/ReviewCollectionPageContents';
 
 const ReviewCollectionPage = () => {
+  const clearEditorAnswerMapStorage = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+
+      // 키에 특정 문자열이 포함되어 있는지 확인
+      if (key?.includes(SESSION_STORAGE_KEY.editorAnswerMap)) {
+        localStorage.removeItem(key); // 해당 키 삭제
+        i--; // removeItem 후에 인덱스가 변경되므로 i를 감소시켜야 함
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      clearEditorAnswerMapStorage();
+    };
+  }, []);
+
   return (
     <ErrorSuspenseContainer fallback={AuthAndServerErrorFallback}>
       <ReviewDisplayLayout isReviewList={false}>
