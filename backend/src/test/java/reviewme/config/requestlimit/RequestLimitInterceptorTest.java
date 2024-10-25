18e01f3ecdef7c9d43e85dbb4b8beaf0fcad16b1
nayonsoso <panda0329@naver.com>
2024-10-25 09:24:56 +0900

    fix: 깨지는 테스트 봉합
    - stream -> list
    - 테스트에 사용되는 로그 메세지 수정

--- backend/src/test/java/reviewme/config/requestlimit/RequestLimitInterceptorTest.java
+++ backend/src/test/java/reviewme/config/requestlimit/RequestLimitInterceptorTest.java
@@ -22,7 +22,7 @@ class RequestLimitInterceptorTest {
     private final ValueOperations<String, Long> valueOperations = mock(ValueOperations.class);
     private final RequestLimitProperties requestLimitProperties = mock(RequestLimitProperties.class);
     private final RequestLimitInterceptor interceptor = new RequestLimitInterceptor(redisTemplate, requestLimitProperties);
-    private final String requestKey = "RequestURI: /api/v2/reviews, RemoteAddr: localhost, UserAgent: Postman";
+    private final String requestKey = "RequestURI: /api/v2/reviews, IP: localhost, UserAgent: Postman";
 
     @BeforeEach
     void setUp() {
