2a4022d0dceb7d19538a3646127f34a370200d11
donghoony <aru0504@naver.com>
2024-11-10 13:20:20 +0900

    refactor: Config는 상위 패키지, Resolver/Interceptor는 각 도메인에 두기

--- backend/src/main/java/reviewme/config/WebConfig.java
+++ backend/src/main/java/reviewme/config/WebConfig.java
@@ -1,15 +1,16 @@
-package reviewme.reviewgroup.controller;
+package reviewme.config;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reviewme.reviewgroup.controller.ReviewGroupSessionResolver;
 import reviewme.reviewgroup.service.ReviewGroupService;
 
 @Configuration
 @RequiredArgsConstructor
-public class ReviewGroupWebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     private final ReviewGroupService reviewGroupService;
 
