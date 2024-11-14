36d558c1c7d1c19808513b66461ecdcd7e13e319
donghoony <aru0504@naver.com>
2024-11-15 02:10:34 +0900

    refactor: `NamedParameterJdbcTemplate` 사용하도록 수정

--- backend/src/main/java/reviewme/highlight/repository/HighlightJdbcRepositoryImpl.java
+++ backend/src/main/java/reviewme/highlight/repository/HighlightJdbcRepositoryImpl.java
@@ -2,25 +2,23 @@ package reviewme.highlight.repository;
 
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import reviewme.highlight.domain.Highlight;
 
 @RequiredArgsConstructor
 public class HighlightJdbcRepositoryImpl implements HighlightJdbcRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public void saveAll(Collection<Highlight> highlights) {
-        jdbcTemplate.batchUpdate(
-                "INSERT INTO highlight (answer_id, line_index, start_index, end_index) VALUES (?, ?, ?, ?)",
-                highlights,
-                highlights.size(),
-                (ps, highlight) -> {
-                    ps.setLong(1, highlight.getAnswerId());
-                    ps.setInt(2, highlight.getLineIndex());
-                    ps.setInt(3, highlight.getHighlightRange().getStartIndex());
-                    ps.setInt(4, highlight.getHighlightRange().getEndIndex());
-                });
+        SqlParameterSource[] parameterSources = SqlParameterSourceUtils.createBatch(highlights.toArray());
+        String insertSql = """
+                INSERT INTO highlight (answer_id, line_index, start_index, end_index)
+                VALUES (:answerId, :lineIndex, :highlightRange.startIndex, :highlightRange.endIndex)
+                """;
+        namedParameterJdbcTemplate.batchUpdate(insertSql, parameterSources);
     }
 }
