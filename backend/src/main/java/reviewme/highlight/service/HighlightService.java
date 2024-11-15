c6a0b3bb441e270417fe59272bdf86779b8a56c5
donghoony <aru0504@naver.com>
2024-11-15 15:17:21 +0900

    chore: add todos

--- backend/src/main/java/reviewme/highlight/service/HighlightService.java
+++ backend/src/main/java/reviewme/highlight/service/HighlightService.java
@@ -23,6 +23,14 @@ public class HighlightService {
 
     @Transactional
     public void editHighlight(HighlightsRequest highlightsRequest, ReviewGroup reviewGroup) {
+        // TODO: request으로부터 아래 두 가지를 검증한다.
+        // 1. answerId들이 각각 question에 포함되는지 확인한다.
+        // 2. answerId들이 각각 reviewGroup에 포함되는지 확인한다.
+        // validator 내부의 메서드도 재사용 가능하도록 작성되어야 한다.
+        // 하지만 현재 각 validator들은 하나의 기능을 위해 존재하며, 리팩터링에 어려움이 있다.
+
+        List<Long> requestedAnswerIds = highlightsRequest.getUniqueAnswerIds();
+
         List<Highlight> highlights = highlightMapper.mapToHighlights(highlightsRequest);
 
         Set<Long> answerIds = answerRepository.findIdsByQuestionId(highlightsRequest.questionId());
