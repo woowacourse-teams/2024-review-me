3dde794df4af5972110aedef13e48b6e7f9d3414
hyeonjilee <skylar12200@gmail.com>
2024-11-17 10:13:48 +0900

    refactor: caffeine 설정 제거 및 명시적으로 캐시 이름을 config에서 지정하지 않는 방식으로 변경
    - config에서 지정하지 않으면 새로운 @Cacheable value 추가할때 동적으로 ConcurrentHashMap을 생성함

--- backend/src/main/java/reviewme/config/CacheManagerConfig.java
+++ backend/src/main/java/reviewme/config/CacheManagerConfig.java
@@ -1,10 +1,8 @@
 package reviewme.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import java.time.Duration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,17 +13,7 @@ import org.springframework.context.annotation.Profile;
 public class CacheManagerConfig {
 
     @Bean
-    public CacheManager cacheManager(Caffeine caffeine) {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("templateCache");
-        caffeineCacheManager.setCaffeine(caffeine);
-        return caffeineCacheManager;
-    }
-
-    @Bean
-    public Caffeine caffeineConfig() {
-        return Caffeine.newBuilder()
-                .expireAfterAccess(Duration.ofDays(30))
-                .maximumSize(1000)
-                .recordStats();
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager();
     }
 }
