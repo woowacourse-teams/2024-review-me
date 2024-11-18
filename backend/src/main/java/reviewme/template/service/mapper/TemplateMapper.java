ad9360b128fb682ec7f68d241e7dd7122b6e8cf3
hyeonjilee <skylar12200@gmail.com>
2024-11-18 14:34:44 +0900

    refactor: value로 캐시를 구분하고 key는 기본 형태로 저장하게 변경

--- backend/src/main/java/reviewme/template/service/mapper/TemplateMapper.java
+++ backend/src/main/java/reviewme/template/service/mapper/TemplateMapper.java
@@ -39,7 +39,7 @@ public class TemplateMapper {
     private final OptionGroupRepository optionGroupRepository;
     private final OptionItemRepository optionItemRepository;
 
-    @Cacheable(value = "template_response", key = "#root.targetClass.simpleName + '_' + #root.methodName + '_' + #reviewGroup.t emplateId")
+    @Cacheable(value = "template_response", key = "#reviewGroup.templateId")
     public TemplateResponse mapToTemplateResponse(ReviewGroup reviewGroup) {
         Template template = templateRepository.findById(reviewGroup.getTemplateId())
                 .orElseThrow(() -> new TemplateNotFoundByReviewGroupException(
