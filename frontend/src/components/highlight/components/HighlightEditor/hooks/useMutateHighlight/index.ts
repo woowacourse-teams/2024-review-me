f7fc305ca50ea35fb972fb991f49983d4409dae2
badahertz52 <badahertz52@gmail.com>
2024-11-18 22:15:10 +0900

    feat : 형광펜 API 요청 성공 후, 해당 질문 쿼리 무효화하는 기능 추가

--- frontend/src/components/highlight/components/HighlightEditor/hooks/useMutateHighlight/index.ts
+++ frontend/src/components/highlight/components/HighlightEditor/hooks/useMutateHighlight/index.ts
@@ -1,7 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { postHighlight } from '@/apis/highlight';
-import { LOCAL_STORAGE_KEY } from '@/constants';
+import { LOCAL_STORAGE_KEY, REVIEW_QUERY_KEY, SESSION_STORAGE_KEY } from '@/constants';
 import { EditorAnswerMap } from '@/types';
 
 export interface UseMutateHighlightProps {
@@ -17,6 +17,21 @@ const useMutateHighlight = ({
   updateEditorAnswerMap,
   resetHighlightMenu,
 }: UseMutateHighlightProps) => {
+  const queryClient = useQueryClient();
+  /**
+   * 형광펜 API 성공 후, 현재 질문에 대한 쿼리 캐시 무효화해서, 변경된 형광펜 데이터 불러오도록 함
+   */
+  const invalidateCurrentSectionQuery = () => {
+    const sectionId = sessionStorage.getItem(SESSION_STORAGE_KEY.currentReviewCollectionSectionId);
+
+    if (sectionId) {
+      queryClient.invalidateQueries({
+        predicate: (query) =>
+          query.queryKey[0] === REVIEW_QUERY_KEY.groupedReviews && query.queryKey[1] === Number(sectionId),
+      });
+    }
+  };
+
   const mutation = useMutation({
     mutationFn: (newEditorAnswerMap: EditorAnswerMap) => postHighlight(newEditorAnswerMap, questionId),
     onMutate: () => {
@@ -28,6 +43,8 @@ const useMutateHighlight = ({
       // 토스트 모달 지우기
       handleErrorModal(false);
       localStorage.removeItem(LOCAL_STORAGE_KEY.isHighlightError);
+      // 해당 질문 쿼리 캐시 무효화
+      invalidateCurrentSectionQuery();
     },
     onError: (error) => {
       //토스트 모달 띄움
