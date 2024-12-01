61647cc7eba1201ad2b188eebbccd7bedbefdeb8
ImxYJL <allensain14@gmail.com>
2024-12-01 20:45:15 +0900

    refactor: useModals에 모달 기본값을 optional로 추가

--- frontend/src/pages/ReviewWritingPage/modals/hooks/useCardFormModal.ts
+++ frontend/src/pages/ReviewWritingPage/modals/hooks/useCardFormModal.ts
@@ -1,9 +1,14 @@
 import { useModals } from '@/hooks';
+import { Modals } from '@/hooks/modal/useModals';
 
 import { CARD_FORM_MODAL_KEY } from '../../constants';
 
-const useCardFormModal = () => {
-  const { isOpen, openModal, closeModal } = useModals();
+interface useCardFormModalProps {
+  initialStates: Modals;
+}
+
+const useCardFormModal = ({ initialStates }: useCardFormModalProps) => {
+  const { isOpen, openModal, closeModal } = useModals({ initialStates });
 
   const handleOpenModal = (key: keyof typeof CARD_FORM_MODAL_KEY) => {
     openModal(CARD_FORM_MODAL_KEY[key]);
