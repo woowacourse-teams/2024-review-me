772589ecfe362ad5a6eb330640eb6cdd44ae970d
donghoony <aru0504@naver.com>
2024-12-19 15:16:26 +0900

    fix: 문서에서 최신순 정렬

--- backend/src/test/java/reviewme/api/ReviewGroupApiTest.java
+++ backend/src/test/java/reviewme/api/ReviewGroupApiTest.java
@@ -131,8 +131,8 @@ class ReviewGroupApiTest extends ApiTest {
     void 회원이_생성한_프로젝트_목록을_반환한다() {
         ReviewGroupListResponse response = new ReviewGroupListResponse(true,
                 List.of(
-                        new ReviewGroupDetailResponse("아루", "리뷰미", LocalDate.of(2024, 1, 5), "ABCD1234"),
-                        new ReviewGroupDetailResponse("이동훈", "우테코", LocalDate.of(2024, 1, 30), "WOOTECO1")
+                        new ReviewGroupDetailResponse("이동훈", "우테코", LocalDate.of(2024, 1, 30), "WOOTECO1"),
+                        new ReviewGroupDetailResponse("아루", "리뷰미", LocalDate.of(2024, 1, 5), "ABCD1234")
                 )
         );
         BDDMockito.given(reviewGroupLookupService.getMyReviewGroups())
