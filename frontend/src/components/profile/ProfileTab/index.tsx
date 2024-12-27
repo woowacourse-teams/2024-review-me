86806d1d806d4f6a6f9b392545c46fb91154d4ac
chysis <chysiss@naver.com>
2024-12-28 03:06:15 +0900

    chore: 프로필 탭의 모든 요소가 드래그 할 수 없도록 수정

--- frontend/src/components/profile/ProfileTab/index.tsx
+++ frontend/src/components/profile/ProfileTab/index.tsx
@@ -1,3 +1,4 @@
+import UndraggableWrapper from '@/components/common/UndraggableWrapper';
 import { ProfileTabElement } from '@/types/profile';
 
 import * as S from './styles';
@@ -9,33 +10,35 @@ interface ProfileTabProps {
 const ProfileTab = ({ items }: ProfileTabProps) => {
   return (
     <S.ProfileTabContainer>
-      {items.map((item, index) => {
-        switch (item.elementType) {
-          case 'readonly':
-            return (
-              <S.ReadonlyItemWrapper
-                key={`${item.elementType}_${index}`}
-                $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
-              >
-                {item.content}
-              </S.ReadonlyItemWrapper>
-            );
-          case 'action':
-            return (
-              <S.ActionItemWrapper
-                key={`${item.elementType}_${index}`}
-                onClick={item.handleClick}
-                $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
-              >
-                {item.content}
-              </S.ActionItemWrapper>
-            );
-          case 'divider':
-            return (
-              <S.Divider key={`${item.elementType}_${index}`} $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile} />
-            );
-        }
-      })}
+      <UndraggableWrapper>
+        {items.map((item, index) => {
+          switch (item.elementType) {
+            case 'readonly':
+              return (
+                <S.ReadonlyItemWrapper
+                  key={`${item.elementType}_${index}`}
+                  $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
+                >
+                  {item.content}
+                </S.ReadonlyItemWrapper>
+              );
+            case 'action':
+              return (
+                <S.ActionItemWrapper
+                  key={`${item.elementType}_${index}`}
+                  onClick={item.handleClick}
+                  $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
+                >
+                  {item.content}
+                </S.ActionItemWrapper>
+              );
+            case 'divider':
+              return (
+                <S.Divider key={`${item.elementType}_${index}`} $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile} />
+              );
+          }
+        })}
+      </UndraggableWrapper>
     </S.ProfileTabContainer>
   );
 };
