b644c49b4cb87512c59b5bc398d6380838e156a2
ImxYJL <allensain14@gmail.com>
2025-01-04 16:08:15 +0900

    feat: 로그인 요청 모달에 에러 메세지 구역 추가

--- frontend/src/components/login/LoginRequestModal/index.tsx
+++ frontend/src/components/login/LoginRequestModal/index.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@emotion/react';
+import { useState } from 'react';
 
 import { ContentModal, GithubLoginButton } from '@/components';
 
@@ -12,6 +13,7 @@ interface LoginRequestModalProps {
 }
 
 const LoginRequestModal = ({ titleType, closeModal }: LoginRequestModalProps) => {
+  const [errorMessage, setErrorMessage] = useState('');
   const theme = useTheme();
 
   const getTitleLabel = (titleType: LoginRequestTitle) => {
@@ -19,15 +21,28 @@ const LoginRequestModal = ({ titleType, closeModal }: LoginRequestModalProps) =>
     if (titleType === 'membershipCheck') return '회원이신가요?';
   };
 
+  // 에러 메세지 확인용. 추후 API 호출로 변경
+  const handleClickLoginButton = () => {
+    setErrorMessage('에러 메세지');
+  };
+
   return (
-    <ContentModal title={getTitleLabel(titleType)} handleClose={closeModal} isClosableOnBackground={true} $style={{}}>
+    <ContentModal
+      title={getTitleLabel(titleType)}
+      handleClose={closeModal}
+      isClosableOnBackground={true}
+      $style={{
+        paddingBottom: '0.8rem',
+      }}
+    >
       <S.LoginRequestModal>
         <S.LoginRequestLabel>로그인 후 간편하게 받은 리뷰를 확인하세요!</S.LoginRequestLabel>
         <GithubLoginButton
-          handleClick={() => {}}
+          handleClick={handleClickLoginButton}
           $logoStyle={{ height: '3rem' }}
           $style={{ fontSize: theme.fontSize.small, height: '4rem', width: '100%' }}
         />
+        {errorMessage && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
       </S.LoginRequestModal>
     </ContentModal>
   );
