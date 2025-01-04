70634cc08fa59cc9b7295fd5aea7dde7feaf9fea
soosoo22 <lllllh00@naver.com>
2025-01-04 20:17:28 +0900

    refactor: formatKeyword 함수에서 반환 타입을 명시하지 않도록 변경

--- frontend/src/utils/formatKeyword.ts
+++ frontend/src/utils/formatKeyword.ts
@@ -1,4 +1,4 @@
-const formatKeyword = (content: string): string => {
+const formatKeyword = (content: string) => {
   const contentWithoutExample = Array.from(content.split(' (예: ')[0]);
   const emoji = contentWithoutExample.shift();
   const keyword = contentWithoutExample.join('');
