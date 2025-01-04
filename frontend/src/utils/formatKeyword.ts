330db3bebbc66568296cc0e058e4d85ff30ee5c1
soosoo22 <lllllh00@naver.com>
2025-01-04 19:48:21 +0900

    refactor: 배열을 처리하던 함수를 단일 문자열을 포맷팅하는 formatKeyword 함수로 변경

---
+++ frontend/src/utils/formatKeyword.ts
@@ -0,0 +1,9 @@
+const formatKeyword = (content: string): string => {
+  const contentWithoutExample = Array.from(content.split(' (예: ')[0]);
+  const emoji = contentWithoutExample.shift();
+  const keyword = contentWithoutExample.join('');
+
+  return `${emoji} ${keyword}`;
+};
+
+export default formatKeyword;
