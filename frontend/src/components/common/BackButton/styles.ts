3f26cdb7df8caa3b94c13ef7aa139c6dd43040f5
chysis <chysiss@naver.com>
2025-01-05 12:50:31 +0900

    chore: 누락된 스타일 코드 추가

--- frontend/src/components/common/BackButton/styles.ts
+++ frontend/src/components/common/BackButton/styles.ts
@@ -4,11 +4,17 @@ interface BackButtonStyleProps {
   $style?: React.CSSProperties;
 }
 
+export const BackButtonWrapper = styled.div<BackButtonStyleProps>`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+
+  ${({ $style }) => $style && { ...$style }}
+`;
+
 export const BackButton = styled.button<BackButtonStyleProps>`
   width: 3.5rem;
   height: 3.5rem;
 
   ${({ $style }) => $style && { ...$style }}
 `;
-
-export const BackButtonImage = styled.img``;
