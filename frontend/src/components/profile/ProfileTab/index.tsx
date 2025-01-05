46010d0f2435dfbfdd532c4c2bfe798f2f5c5419
chysis <chysiss@naver.com>
2025-01-06 02:03:05 +0900

    refactor: 프로필 탭 element 렌더링 함수 분리 및 리팩토링

--- frontend/src/components/profile/ProfileTab/index.tsx
+++ frontend/src/components/profile/ProfileTab/index.tsx
@@ -1,6 +1,9 @@
 import UndraggableWrapper from '@/components/common/UndraggableWrapper';
 import { ProfileTabElement } from '@/types/profile';
 
+import ActionItem from './components/ActionItem';
+import Divider from './components/Divider';
+import ReadonlyItem from './components/ReadonlyItem';
 import * as S from './styles';
 
 interface ProfileTabProps {
@@ -8,37 +11,33 @@ interface ProfileTabProps {
 }
 
 const ProfileTab = ({ items }: ProfileTabProps) => {
+  const renderProfileTabItem = (item: ProfileTabElement) => {
+    switch (item.elementType) {
+      case 'readonly':
+        return (
+          <ReadonlyItem
+            key={item.elementId}
+            isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
+            content={item.content!}
+          />
+        );
+      case 'action':
+        return (
+          <ActionItem
+            key={item.elementId}
+            handleItemClick={() => item.handleClick}
+            isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
+            content={item.content!}
+          />
+        );
+      case 'divider':
+        return <Divider key={item.elementId} isDisplayedOnlyMobile={item.isDisplayedOnlyMobile} />;
+    }
+  };
+
   return (
     <S.ProfileTabContainer>
-      <UndraggableWrapper>
-        {items.map((item, index) => {
-          switch (item.elementType) {
-            case 'readonly':
-              return (
-                <S.ReadonlyItemWrapper
-                  key={`${item.elementType}_${index}`}
-                  $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
-                >
-                  {item.content}
-                </S.ReadonlyItemWrapper>
-              );
-            case 'action':
-              return (
-                <S.ActionItemWrapper
-                  key={`${item.elementType}_${index}`}
-                  onClick={item.handleClick}
-                  $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile}
-                >
-                  {item.content}
-                </S.ActionItemWrapper>
-              );
-            case 'divider':
-              return (
-                <S.Divider key={`${item.elementType}_${index}`} $isDisplayedOnlyMobile={item.isDisplayedOnlyMobile} />
-              );
-          }
-        })}
-      </UndraggableWrapper>
+      <UndraggableWrapper>{items.map((item) => renderProfileTabItem(item))}</UndraggableWrapper>
     </S.ProfileTabContainer>
   );
 };
--- frontend/src/components/profile/ProfileTab/styles.ts
+++ frontend/src/components/profile/ProfileTab/styles.ts
@@ -1,11 +1,5 @@
 import styled from '@emotion/styled';
 
-import media from '@/utils/media';
-
-interface ProfileTabStyleProps {
-  $isDisplayedOnlyMobile: boolean;
-}
-
 export const ProfileTabContainer = styled.section`
   position: absolute;
   z-index: ${({ theme }) => theme.zIndex.profileTab};
@@ -26,53 +20,4 @@ export const ProfileTabContainer = styled.section`
     0 0.5rem 0.5rem -0.3rem rgba(0, 0, 0, 0.2),
     0 0.8rem 1rem 0.1rem rgba(0, 0, 0, 0.14),
     0 0.3rem 1.4rem 0.2rem rgba(0, 0, 0, 0.12);
-`;
-
-export const ReadonlyItemWrapper = styled.div<ProfileTabStyleProps>`
-  cursor: default;
-
-  display: ${({ $isDisplayedOnlyMobile }) => ($isDisplayedOnlyMobile ? 'none' : 'flex')};
-  align-items: center;
-
-  height: 3rem;
-  padding: 1rem;
-
-  ${media.small} {
-    display: ${({ $isDisplayedOnlyMobile }) => $isDisplayedOnlyMobile && 'flex'};
-  }
-`;
-
-export const ActionItemWrapper = styled.div<ProfileTabStyleProps>`
-  cursor: pointer;
-
-  display: ${({ $isDisplayedOnlyMobile }) => ($isDisplayedOnlyMobile ? 'none' : 'flex')};
-  align-items: center;
-
-  height: 3rem;
-  padding: 1rem;
-
-  border-radius: 0.8rem;
-
-  :hover {
-    background-color: ${({ theme }) => theme.colors.lightGray};
-  }
-
-  ${media.small} {
-    display: ${({ $isDisplayedOnlyMobile }) => $isDisplayedOnlyMobile && 'flex'};
-  }
-`;
-
-export const Divider = styled.hr<ProfileTabStyleProps>`
-  display: ${({ $isDisplayedOnlyMobile }) => ($isDisplayedOnlyMobile ? 'none' : 'block')};
-
-  width: 100%;
-  height: 0;
-  margin: 0.5rem 0;
-  padding: 0;
-
-  border: 0.1rem solid ${({ theme }) => theme.colors.placeholder};
-
-  ${media.small} {
-    display: ${({ $isDisplayedOnlyMobile }) => $isDisplayedOnlyMobile && 'block'};
-  }
 `;
