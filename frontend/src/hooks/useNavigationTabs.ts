52edf28e0df5e88cf117989544a1f3b16a6a3637
soosoo22 <lllllh00@naver.com>
2025-01-09 19:12:15 +0900

    feat: ROUTE 상수에 리뷰 링크 관리, 작성한 리뷰 확인 페이지 경로 추가

--- frontend/src/hooks/useNavigationTabs.ts
+++ frontend/src/hooks/useNavigationTabs.ts
@@ -1,27 +1,28 @@
 import { useLocation, useNavigate } from 'react-router';
 
+import { ROUTE } from '@/constants';
+
 const useNavigationTabs = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
-  // TODO: '리뷰 링크 확인', '작성한 리뷰 확인' 페이지 URL 경로 확정되면 변경
   const navigateReviewLinkManagementPage = () => {
-    navigate('/user/review-link-management');
+    navigate(`/${ROUTE.reviewLinks}`);
   };
 
   const navigateWrittenReviewConfirmPage = () => {
-    navigate('/user/written-review-confirm');
+    navigate(`/${ROUTE.writtenReview}`);
   };
 
   const tabList = [
     {
       label: '리뷰 링크 관리',
-      path: '/user/review-link-management',
+      path: `/${ROUTE.reviewLinks}`,
       handleTabClick: navigateReviewLinkManagementPage,
     },
     {
       label: '작성한 리뷰 확인',
-      path: '/user/written-review-confirm',
+      path: `/${ROUTE.writtenReview}`,
       handleTabClick: navigateWrittenReviewConfirmPage,
     },
   ];
