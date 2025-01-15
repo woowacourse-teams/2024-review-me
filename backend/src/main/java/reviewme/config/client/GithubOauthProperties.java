832cd9579c5881c40e8eb5c3e8d90e8ed967911a
nayonsoso <panda0329@naver.com>
2025-01-16 08:20:53 +0900

    chore: github oauth 변수 추가

---
+++ backend/src/main/java/reviewme/config/client/GithubOauthProperties.java
@@ -0,0 +1,10 @@
+package reviewme.config.client;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "github.oauth")
+public record GithubOauthProperties(
+        String clientId,
+        String clientSecret
+) {
+}
