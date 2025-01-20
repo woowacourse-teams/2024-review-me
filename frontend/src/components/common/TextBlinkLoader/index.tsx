b53a97b2fad7275eaebb3c470c658a3284002d44
badahertz52 <badahertz52@gmail.com>
2025-01-20 16:21:48 +0900

    feat : TextBlinkLoader 구현

---
+++ frontend/src/components/common/TextBlinkLoader/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const TextBlinkLoader = (props: S.LoaderProps) => {
+  return <S.Loader {...props} />;
+};
+
+export default TextBlinkLoader;
---
+++ frontend/src/components/common/TextBlinkLoader/style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export interface LoaderProps {
+  $content?: string;
+}
+
+export const Loader = styled.div<LoaderProps>`
+  animation: l1 1.5s linear infinite alternate;
+
+  &::before {
+    content: '${({ $content }) => $content ?? 'Loading...'}';
+  }
+
+  @keyframes l1 {
+    to {
+      opacity: 0;
+    }
+  }
+`;
