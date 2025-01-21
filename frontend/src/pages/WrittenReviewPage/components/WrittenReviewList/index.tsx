3ef91c2773652fb378536a349de2db5c95701027
ImxYJL <allensain14@gmail.com>
2025-01-21 19:19:20 +0900

    feat: WrittenReviewList 및 Page에 작성한 리뷰 API 연결
    1. API 함수 적용
    2. EmptyContent 컴포넌트 추가
    3. 페이지 렌더링 방식 리팩토링

--- frontend/src/pages/WrittenReviewPage/components/WrittenReviewList/index.tsx
+++ frontend/src/pages/WrittenReviewPage/components/WrittenReviewList/index.tsx
@@ -1,5 +1,9 @@
+import { ReviewCard } from '@/components';
+import UndraggableWrapper from '@/components/common/UndraggableWrapper';
 import ReviewListItem from '@/components/ReviewListItem';
+import { useInfiniteScroll } from '@/pages/ReviewListPage/hooks'; // 경로 수정하기
 
+import { useGetWrittenReviewList } from '../../hooks';
 import { PageContentLayout } from '../layouts';
 
 import * as S from './styles';
@@ -9,19 +13,40 @@ interface WrittenReviewListProps {
 }
 
 const WrittenReviewList = ({ handleClick }: WrittenReviewListProps) => {
-  // 리뷰 리스트 받아오기
-  const reviewIdList = [5, 1, 2, 3, 4];
+  const { data, fetchNextPage, isLoading, isSuccess } = useGetWrittenReviewList();
+
+  const isLastPage = data.pages[data.pages.length - 1].isLastPage;
+  const reviewList = data.pages.flatMap((page) => page.reviews) || [];
+
+  const lastReviewElementRef = useInfiniteScroll({
+    fetchNextPage,
+    isLoading,
+    isLastPage,
+  });
 
   return (
     <PageContentLayout title="작성한 리뷰 목록">
-      <S.WrittenReviewList>
-        {/** 추후 이벤트 위임 형식으로 변경 가능 */}
+      {isSuccess && (
+        <S.WrittenReviewList>
+          {/** TODO: 추후 이벤트 위임 형식으로 변경  */}
+          {reviewList.map((review, index) => {
+            const isLastReview = reviewList.length === index + 1;
 
-        {/** TODO: 작성한 리뷰 없을 때의 컴포넌트 추가*/}
-        {reviewIdList.map((reviewId) => (
-          <ReviewListItem key={reviewId} handleClick={() => handleClick(reviewId)} />
-        ))}
-      </S.WrittenReviewList>
+            // TODO: ReviewCard 확장. 프로젝트 이름, 리뷰이 이름 추가
+            return (
+              <UndraggableWrapper key={review.reviewId}>
+                <ReviewCard
+                  createdAt={review.createdAt}
+                  contentPreview={review.contentPreview}
+                  categories={review.categories}
+                  handleClick={() => handleClick(review.reviewId)}
+                />
+                <div ref={isLastReview ? lastReviewElementRef : null} style={{ height: '0.1rem' }} />
+              </UndraggableWrapper>
+            );
+          })}
+        </S.WrittenReviewList>
+      )}
     </PageContentLayout>
   );
 };
--- frontend/src/pages/WrittenReviewPage/index.tsx
+++ frontend/src/pages/WrittenReviewPage/index.tsx
@@ -1,22 +1,25 @@
+import { useLayoutEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { ErrorSuspenseContainer, AuthAndServerErrorFallback } from '@/components';
+import { ErrorSuspenseContainer, AuthAndServerErrorFallback, EmptyContent } from '@/components';
 import { useSearchParamAndQuery } from '@/hooks';
 
 import DetailedWrittenReview from './components/DetailedWrittenReview';
 import WrittenReviewList from './components/WrittenReviewList';
-import { useDeviceBreakpoints } from './hooks';
+import { useDeviceBreakpoints, useGetWrittenReviewList } from './hooks';
 import * as S from './styles';
 
 const WrittenReviewPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { deviceType } = useDeviceBreakpoints();
+  const { data, isLoading } = useGetWrittenReviewList();
 
   const { queryString: reviewIdString } = useSearchParamAndQuery({
     queryStringKey: 'reviewId',
   });
 
+  const reviewList = data?.pages.flatMap((page) => page.reviews) || [];
   const selectedReviewId = reviewIdString ? Number(reviewIdString) : null;
 
   const handleReviewItemClick = (reviewId: number) => {
@@ -26,23 +29,44 @@ const WrittenReviewPage = () => {
     navigate(`${location.pathname}?${params.toString()}`);
   };
 
-  const renderContent = () => {
-    // 노트북, 보통 사이즈 이상의 태블릿 가로모드: 목록, 상세 모두 렌더링
-    if (deviceType.isDesktop) {
-      return (
-        <S.PageContainer>
-          <WrittenReviewList handleClick={handleReviewItemClick} />
-          <DetailedWrittenReview $isDisplayable={true} selectedReviewId={selectedReviewId} />
-        </S.PageContainer>
-      );
-    }
+  // NOTE: 임시 로그인 인증 쿠키 설정
+  // 추후 로그인 쿠키값/비로그인 쿠키값을 분리하고,
+  // (이 페이지에서 쿠키를 설정하지 않고) 이전 페이지(url 관리 페이지)의 값을 가져와야 함
+  useLayoutEffect(() => {
+    document.cookie = 'mockAuthToken=2024-review-me';
+  }, []);
 
-    // 이외의 경우: queryString 없으면 목록, 있으면 상세보기 렌더링
-    return selectedReviewId ? (
+  const renderEmptyContent = () => (
+    <EmptyContent
+      iconWidth={deviceType.isDesktop ? '30vw' : '60vw'}
+      messageFontSize={deviceType.isTablet ? '2rem' : undefined}
+      iconHeight="45vh"
+    >
+      <p>아직 작성한 리뷰가 없어요...</p>
+    </EmptyContent>
+  );
+
+  // 노트북, 보통 사이즈 이상의 태블릿 가로모드: 목록, 상세 모두 렌더링
+  const renderDesktopView = () => (
+    <S.PageContainer>
+      <WrittenReviewList handleClick={handleReviewItemClick} />
+      <DetailedWrittenReview $isDisplayable={true} selectedReviewId={selectedReviewId} />
+    </S.PageContainer>
+  );
+
+  // 이외의 경우: queryString 없으면 목록, 있으면 상세보기 렌더링
+  const renderMobileOrTabletView = () =>
+    selectedReviewId ? (
       <DetailedWrittenReview $isDisplayable={!!selectedReviewId} selectedReviewId={selectedReviewId} />
     ) : (
       <WrittenReviewList handleClick={handleReviewItemClick} />
     );
+
+  const renderContent = () => {
+    if (isLoading) return;
+    if (reviewList.length === 0) return renderEmptyContent();
+
+    return deviceType.isDesktop ? renderDesktopView() : renderMobileOrTabletView();
   };
 
   return <ErrorSuspenseContainer errorFallback={AuthAndServerErrorFallback}>{renderContent()}</ErrorSuspenseContainer>;
