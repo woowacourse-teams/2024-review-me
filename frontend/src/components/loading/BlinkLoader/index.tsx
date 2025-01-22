5e6747148bd25b37cffa9778fab9c2d59e9e9e71
badahertz52 <badahertz52@gmail.com>
2025-01-22 19:51:02 +0900

    feat : props에 애니메이션 지속 시간 추가

--- frontend/src/components/loading/BlinkLoader/index.tsx
+++ frontend/src/components/loading/BlinkLoader/index.tsx
@@ -4,8 +4,10 @@ import { EssentialPropsWithChildren } from '@/types';
 
 import * as S from './style';
 
-const BlinkLoader = ({ children }: EssentialPropsWithChildren) => {
-  return <S.Loader>{children}</S.Loader>;
+interface BlinkLoaderProps extends EssentialPropsWithChildren, S.LoaderProps {}
+
+const BlinkLoader = ({ children, $animationDurationTime }: BlinkLoaderProps) => {
+  return <S.Loader $animationDurationTime={$animationDurationTime}>{children}</S.Loader>;
 };
 
 export default BlinkLoader;
--- frontend/src/components/loading/BlinkLoader/style.ts
+++ frontend/src/components/loading/BlinkLoader/style.ts
@@ -1,7 +1,11 @@
 import styled from '@emotion/styled';
 
-export const Loader = styled.div`
-  animation: l1 1s linear infinite alternate;
+export interface LoaderProps {
+  $animationDurationTime?: string;
+}
+
+export const Loader = styled.div<LoaderProps>`
+  animation: l1 ${(props) => props.$animationDurationTime ?? '1s'} linear infinite alternate;
 
   @keyframes l1 {
     to {
