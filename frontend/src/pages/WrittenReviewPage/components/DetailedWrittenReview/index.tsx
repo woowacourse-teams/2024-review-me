1548a42cb9dcf4dc2dd8cd11eae942de432d1268
ImxYJL <allensain14@gmail.com>
2025-01-23 14:29:59 +0900

    refactor: DetailedWrittenReview 렌더링 로직 분리

--- frontend/src/pages/WrittenReviewPage/components/DetailedWrittenReview/index.tsx
+++ frontend/src/pages/WrittenReviewPage/components/DetailedWrittenReview/index.tsx
@@ -17,37 +17,39 @@ export interface DetailedWrittenReviewProps {
 const DetailedWrittenReview = ({ $isDisplayable, selectedReviewId }: DetailedWrittenReviewProps) => {
   const { deviceType } = useDeviceBreakpoints();
 
+  const renderMobileItems = () =>
+    !deviceType.isDesktop && (
+      <>
+        <BackButton prevPath={`/${ROUTE.writtenReview}`} wrapperStyle={{ marginBottom: '2rem' }} />
+        <TopButton />
+      </>
+    );
+
+  const renderDetailedReview = () =>
+    selectedReviewId ? (
+      <DetailedReview selectedReviewId={selectedReviewId} $layoutStyle={detailedReviewLayoutStyle} />
+    ) : (
+      <NoSelectedReviewGuide />
+    );
+
   return (
     <PageContentLayout title={deviceType.isDesktop ? '작성한 리뷰 상세보기' : ''}>
       <S.DetailedWrittenReview $isDisplayable={$isDisplayable}>
-        {!deviceType.isDesktop && (
-          <>
-            <BackButton prevPath={`/${ROUTE.writtenReview}`} wrapperStyle={{ marginBottom: '2rem' }} />
-            <TopButton />
-          </>
-        )}
-
+        {renderMobileItems()}
         <S.Outline>
-          <Suspense fallback={<LoadingBar />}>
-            {selectedReviewId ? (
-              <DetailedReview
-                selectedReviewId={selectedReviewId}
-                $layoutStyle={{
-                  width: '100%',
-                  height: '100%',
-                  marginTop: '0',
-                  padding: '3rem 1.5rem',
-                  border: 'none',
-                }}
-              />
-            ) : (
-              <NoSelectedReviewGuide />
-            )}
-          </Suspense>
+          <Suspense fallback={<LoadingBar />}>{renderDetailedReview()}</Suspense>
         </S.Outline>
       </S.DetailedWrittenReview>
     </PageContentLayout>
   );
+};
+
+const detailedReviewLayoutStyle = {
+  width: '100%',
+  height: '100%',
+  marginTop: '0',
+  padding: '3rem 1.5rem',
+  border: 'none',
 };
 
 export default DetailedWrittenReview;
