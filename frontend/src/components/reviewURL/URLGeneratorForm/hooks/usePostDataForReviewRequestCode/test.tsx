0d4eec6b3c4fba75ecd524e1ee4cd8f9e318f371
badahertz52 <badahertz52@gmail.com>
2025-01-23 13:04:19 +0900

    test : 회원용 reviewRequestCode 테스트 추가

--- frontend/src/components/reviewURL/URLGeneratorForm/hooks/usePostDataForReviewRequestCode/test.tsx
+++ frontend/src/components/reviewURL/URLGeneratorForm/hooks/usePostDataForReviewRequestCode/test.tsx
@@ -1,12 +1,15 @@
 import { renderHook, act, waitFor } from '@testing-library/react';
 
-import { VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE } from '@/mocks/mockData/group';
+import {
+  MEMBER_VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE,
+  VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE,
+} from '@/mocks/mockData/group';
 import QueryClientWrapper from '@/queryTestSetup/QueryClientWrapper';
 
 import usePostDataForReviewRequestCode from '.';
 
 describe('usePostDataForReviewRequestCode', () => {
-  it('ReviewRequestCode를 발급받을 수 있다.', async () => {
+  it('비회원 - ReviewRequestCode를 발급받을 수 있다.', async () => {
     // given
     const dataForReviewRequestCode = {
       revieweeName: 'ollie',
@@ -34,5 +37,34 @@ describe('usePostDataForReviewRequestCode', () => {
 
     // then
     expect(result.current.data.reviewRequestCode).toEqual(VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE);
+  });
+
+  it('회원용 - ReviewRequestCode를 발급받을 수 있다.', async () => {
+    // given
+    const dataForReviewRequestCode = {
+      revieweeName: 'ollie',
+      projectName: 'review-me',
+    };
+
+    const { result } = renderHook(
+      () =>
+        usePostDataForReviewRequestCode({
+          handleAPIError: (error: Error) => {
+            console.error(error);
+          },
+          handleAPISuccess: (data: any) => {},
+        }),
+      { wrapper: QueryClientWrapper },
+    );
+
+    // when
+    act(() => {
+      result.current.mutate(dataForReviewRequestCode);
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    // then
+    expect(result.current.data.reviewRequestCode).toEqual(MEMBER_VALID_REVIEW_GROUP_REVIEW_REQUEST_CODE);
   });
 });
