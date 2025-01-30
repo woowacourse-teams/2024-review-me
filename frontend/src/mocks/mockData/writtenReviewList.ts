ea74f7a29a50280b88aa35bbd0dadb421c86a75f
ImxYJL <allensain14@gmail.com>
2025-01-30 20:23:30 +0900

    chore: 목데이터 projectName 일부 수정

--- frontend/src/mocks/mockData/writtenReviewList.ts
+++ frontend/src/mocks/mockData/writtenReviewList.ts
@@ -22,7 +22,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 2,
       createdAt: '2023-08-29',
       revieweeName: '쑤쑤',
-      projectName: '우테코',
+      projectName: '다만세',
       contentPreview: `2. 전해주고 싶어 슬픈 시간이 다 흩어진 후에야 들리지만 눈을 감고 느껴봐 움직이는 마음 너를 향한 내 눈빛을 특별한 기적을 기다리지마 눈 앞에선 우리의 거친 길은 알 수 없는 미래와 벽 바꾸지 않아 포기할 수 없어 변치 않을 사랑으로 지켜줘 상처 입은 내 맘까지 시선 속에서 말은 필요 없어 멈춰져 버린 이 시간 사랑해 널 이 느낌 이대로 그려왔던 헤매임의 끝 이 세상 속에서 반복되는 슬픔 이젠 안녕 수많은 알 수 없는 길 속에 희미한 빛을 난 쫓아가 언제까지라도 함께 하는거야 다시 만난 나의 세계`,
       categories: [
         { optionId: 3, content: '⏰시간 관리 능력 (예: 일정과 마감 기한 준수, 업무의 우선 순위 분배)' },
@@ -33,7 +33,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 3,
       createdAt: '2021-08-01',
       revieweeName: '에프이',
-      projectName: 'wooteco',
+      projectName: 'Dynamite',
       contentPreview: `3. 'Cause, ah-ah, I'm in the stars tonight
         So watch me bring the fire and set the night alight
         Shoes on, get up in the morn
@@ -61,7 +61,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 4,
       createdAt: '2021-08-01',
       revieweeName: '아루',
-      projectName: 'WTC',
+      projectName: '한 페이지가 될 수 있게',
       contentPreview: `4. 솔직히, 말할게 많이 기다려 왔어
           너도 그랬을 거라 믿어
           오늘이 오길 매일같이 달력을 보면서
@@ -83,7 +83,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 1,
       createdAt: '2021-08-01',
       revieweeName: '올리',
-      projectName: '스파게티',
+      projectName: 'Supernova',
       contentPreview: `5. I'm like some kind of supernova
         Watch out
         Look at me go, 재미 좀 볼
@@ -104,7 +104,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 6,
       createdAt: '2021-08-01',
       revieweeName: '산초',
-      projectName: '축구선수',
+      projectName: 'Love Dive',
       contentPreview: `6. 네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Ooh-ooh, ooh-ooh 눈동자 아래로 Ooh-ooh, ooh-ooh 감추고 있는 거 Narcissistic, my God, I love it
         서로를 비춘 밤
         아름다운 까만 눈빛 더 빠져 깊이
@@ -125,7 +125,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 7,
       createdAt: '2021-08-01',
       revieweeName: '커비',
-      projectName: '닌텐도',
+      projectName: 'WTC',
       contentPreview: `7. 나는 짧은 데이터`,
       categories: [
         { optionId: 3, content: '⏰시간 관리 능력 (예: 일정과 마감 기한 준수, 업무의 우선 순위 분배)' },
@@ -136,7 +136,7 @@ export const WRITTEN_REVIEW_LIST: WrittenReviewList = {
       reviewId: 8,
       createdAt: '2021-08-01',
       revieweeName: '테드',
-      projectName: '곰돌이',
+      projectName: '우테코',
       contentPreview: `8. 공간을 한 눈에, 예약은 한 번에! 맞춤형 공간예약 서비스 제작 플랫폼 찜꽁입니다! 공간 제공자(관리자)는 에디터를 통해 공간을 생성할 수 있습니다! 생성한 공간은 링크를 통해 사용자에게 제공할 수 있으며, 사용자는 링크를 통해 간편하게 공간을 확인하고 예약을...`,
       categories: [
         { optionId: 4, content: '💻기술적 역량, 전문 지식 (예: 요구 사항을 이해하고 이를 구현하는 능력)' },
