0281d03ebf5dbe5d753c97652663d98c63374721
ImxYJL <allensain14@gmail.com>
2025-01-31 20:04:50 +0900

    chore: fileTransform 추가

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,12 +1,9 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
-
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   moduleNameMapper: {
-    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }), // 기존 TypeScript alias 유지
-    '\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/ImageMock.js',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   },
   setupFiles: ['./jest.polyfills.js'],
   testEnvironment: 'jsdom',
@@ -19,6 +16,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
     '^.+\\.svg$': '<rootDir>/svgTransform.js',
+    '^.+\\.(png|jpg|jpeg|gif|webp|ico)$': '<rootDir>/fileTransform.js',
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
