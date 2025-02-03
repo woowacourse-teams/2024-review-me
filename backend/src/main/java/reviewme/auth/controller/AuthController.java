ad3ced618ad15da500a4094bc0943617ed57d893
nayonsoso <panda0329@naver.com>
2025-02-03 11:55:27 +0900

    refactor: 세션 작업을 컨트롤러에서 하도록

--- backend/src/main/java/reviewme/auth/controller/AuthController.java
+++ backend/src/main/java/reviewme/auth/controller/AuthController.java
@@ -1,26 +1,31 @@
 package reviewme.auth.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reviewme.auth.domain.GitHubMember;
 import reviewme.auth.service.AuthService;
+import reviewme.global.session.SessionManager;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final SessionManager sessionManager;
     private final AuthService authService;
 
     @GetMapping("/v2/auth/github")
     public ResponseEntity<Void> authWithGithub(
             @RequestParam String code,
-            HttpServletRequest httpRequest
+            HttpSession session
     ) {
-        authService.authWithGithub(httpRequest, code);
+        GitHubMember gitHubMember = authService.authWithGithub(code);
+        sessionManager.saveGitHubMember(session, gitHubMember);
         return ResponseEntity.ok().build();
     }
 
--- backend/src/main/java/reviewme/auth/service/AuthService.java
+++ backend/src/main/java/reviewme/auth/service/AuthService.java
@@ -1,15 +1,12 @@
 
 package reviewme.auth.service;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reviewme.auth.domain.GitHubMember;
 import reviewme.auth.infrastructure.GitHubOAuthClient;
 import reviewme.auth.infrastructure.dto.response.GitHubUserInfoResponse;
-import reviewme.global.session.SessionManager;
 import reviewme.member.domain.Member;
 import reviewme.member.repository.MemberRepository;
 
@@ -18,23 +15,19 @@ import reviewme.member.repository.MemberRepository;
 public class AuthService {
 
     private final GitHubOAuthClient githubOAuthClient;
-    private final SessionManager sessionManager;
     private final GitHubMemberService gitHubMemberService;
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void authWithGithub(HttpServletRequest httpRequest, String code) {
+    public GitHubMember authWithGithub(String code) {
         GitHubUserInfoResponse userInfo = githubOAuthClient.getUserInfo(code);
-        Member member = getOrSaveMember(userInfo.email());
-        GitHubMember gitHubMember = gitHubMemberService.createGitHubMember(userInfo.userName());
-
-        HttpSession session = httpRequest.getSession();
-        sessionManager.saveMember(session, member);
-        sessionManager.saveGitHubMember(session, gitHubMember);
+        saveMemberIfNotExists(userInfo.email());
+        return gitHubMemberService.createGitHubMember(userInfo.userName());
     }
 
-    private Member getOrSaveMember(String email) {
-        return memberRepository.findByEmail(email)
-                .orElseGet(() -> memberRepository.save(new Member(email)));
+    private void saveMemberIfNotExists(String email) {
+        if(!memberRepository.existsByEmail(email)) {
+            memberRepository.save(new Member(email));
+        }
     }
 }
