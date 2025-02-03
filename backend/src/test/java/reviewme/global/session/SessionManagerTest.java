08635881ab7a8ad9ed7690fadaf5464548ff5c08
nayonsoso <panda0329@naver.com>
2025-02-03 13:44:21 +0900

    test: SessionManager 테스트 작성

---
+++ backend/src/test/java/reviewme/global/session/SessionManagerTest.java
@@ -0,0 +1,52 @@
+package reviewme.global.session;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import reviewme.auth.domain.GitHubMember;
+import reviewme.support.ServiceTest;
+
+@ServiceTest
+class SessionManagerTest {
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    private HttpSession session;
+
+    @BeforeEach
+    void setUp() {
+        session = new MockHttpSession();
+    }
+
+    @Test
+    void 세션에_깃허브_멤버를_저장하고_조회한다() {
+        // given
+        GitHubMember gitHubMember = mock(GitHubMember.class);
+
+        // when
+        sessionManager.saveGitHubMember(session, gitHubMember);
+        GitHubMember savedGitHubMember = sessionManager.getGitHubMember(session);
+
+        // then
+        assertThat(savedGitHubMember).isEqualTo(gitHubMember);
+    }
+
+    @Test
+    void 세션에_리뷰_그룹을_저장하고_조회한다() {
+        // given
+        String reviewRequestCode = "reviewRequestCode";
+
+        // when
+        sessionManager.saveReviewRequestCode(session, reviewRequestCode);
+        String savedReviewGroup = sessionManager.getReviewRequestCode(session);
+
+        // then
+        assertThat(savedReviewGroup).isEqualTo(reviewRequestCode);
+    }
+}
