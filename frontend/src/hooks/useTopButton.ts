c380635693397808c239954202a3cd39895fa443
ImxYJL <allensain14@gmail.com>
2025-02-09 17:42:13 +0900

    fix: useTopButton에서 containerRef가 있는 경우에도 window에 이벤트 리스너를 부착하던 문제

--- frontend/src/hooks/useTopButton.ts
+++ frontend/src/hooks/useTopButton.ts
@@ -5,21 +5,20 @@ const TOP_BUTTON_DISPLAY_THRESHOLD = 500;
 const useTopButton = (containerRef?: RefObject<HTMLElement>) => {
   const [showTopButton, setShowTopButton] = useState(false);
 
-  const containerScrollTop = containerRef?.current?.scrollTop;
-
   useEffect(() => {
     const handleShowTopButton = () => {
-      const currentScroll = containerScrollTop ?? window.scrollY;
-      setShowTopButton(currentScroll > TOP_BUTTON_DISPLAY_THRESHOLD);
+      const containerElement = containerRef?.current;
+      const currentScrollTop = containerElement ? containerElement.scrollTop : window.scrollY;
+      setShowTopButton(currentScrollTop > TOP_BUTTON_DISPLAY_THRESHOLD);
     };
-    handleShowTopButton();
 
-    window.addEventListener('scroll', handleShowTopButton);
+    const containerElement = containerRef?.current || window;
+    containerElement.addEventListener('scroll', handleShowTopButton);
 
     return () => {
-      window.removeEventListener('scroll', handleShowTopButton);
+      containerElement.removeEventListener('scroll', handleShowTopButton);
     };
-  }, [containerScrollTop]);
+  }, [containerRef?.current]);
 
   return { showTopButton };
 };
