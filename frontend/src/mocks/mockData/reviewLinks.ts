aacd795d19d0b260dca4f9baa4d05f62a7c1bc5f
soosoo22 <lllllh00@naver.com>
2025-02-09 16:26:46 +0900

    feat: 리뷰 링크 목록 목 데이터 추가

---
+++ frontend/src/mocks/mockData/reviewLinks.ts
@@ -0,0 +1,50 @@
+import { ReviewLinks } from '@/types';
+
+export const REVIEW_LINKS: ReviewLinks = {
+  lastReviewGroupId: 6,
+  isLastPage: true,
+  reviewGroups: [
+    {
+      revieweeName: '현수연',
+      projectName: '우아한테크코스',
+      createdAt: '2025-02-05',
+      reviewRequestCode: 'WOOTECO1',
+      reviewCount: 3,
+    },
+    {
+      revieweeName: '쑤쑤',
+      projectName: '리뷰미',
+      createdAt: '2025-01-05',
+      reviewRequestCode: 'ABCD1234',
+      reviewCount: 7,
+    },
+    {
+      revieweeName: 'soosoo',
+      projectName: '수수',
+      createdAt: '2024-11-05',
+      reviewRequestCode: 'ABCD1234',
+      reviewCount: 5,
+    },
+    {
+      revieweeName: '성장하는 괴물',
+      projectName: '성괴',
+      createdAt: '2024-08-05',
+      reviewRequestCode: 'ABCD1234',
+      reviewCount: 7,
+    },
+    {
+      revieweeName: '쑤',
+      projectName: '쑤쑤쑤',
+      createdAt: '2024-05-05',
+      reviewRequestCode: 'ABCD1234',
+      reviewCount: 10,
+    },
+    {
+      revieweeName: '수연',
+      projectName: '수연아 정신차려라',
+      createdAt: '2024-02-05',
+      reviewRequestCode: 'ABCD1234',
+      reviewCount: 11,
+    },
+  ],
+};
