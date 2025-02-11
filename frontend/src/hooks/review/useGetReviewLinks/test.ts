32bee72950c908f34c46c3739537cee3ca7d3e5d
soosoo22 <lllllh00@naver.com>
2025-02-11 22:53:41 +0900

    test: 리뷰 그룹 목록 조회 및 비회원일 경우 revieweeId가 null로 반환되는지 테스트 추가

---
+++ frontend/src/hooks/review/useGetReviewLinks/test.ts
@@ -0,0 +1,24 @@
+import { renderHook, waitFor } from '@testing-library/react';
+
+import QueryClientWrapper from '@/queryTestSetup/QueryClientWrapper';
+import { testWithAuthCookie } from '@/utils';
+
+import useGetReviewLinks from '.';
+
+describe('회원이 생성한 리뷰 그룹 목록 조회 테스트', () => {
+  it('회원이 생성한 리뷰 그룹 목록을 성공적으로 불러온다.', async () => {
+    const testReviewLinksAPI = async () => {
+      const { result } = renderHook(() => useGetReviewLinks(), {
+        wrapper: QueryClientWrapper,
+      });
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBe(true);
+      });
+
+      expect(result.current.data).toBeDefined();
+    };
+
+    await testWithAuthCookie(testReviewLinksAPI);
+  });
+});
