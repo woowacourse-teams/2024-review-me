720db841ae54b5470fa7a24ac25bfb76c822d8c5
nayonsoso <panda0329@naver.com>
2025-02-12 17:03:49 +0900

    refactor: 하이라이트 수정 시, ReviewGroupSession 을 사용하지 않도록

--- backend/src/main/java/reviewme/highlight/service/HighlightService.java
+++ backend/src/main/java/reviewme/highlight/service/HighlightService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reviewme.global.authorization.RequireReviewGroupAccess;
 import reviewme.highlight.domain.Highlight;
 import reviewme.highlight.repository.HighlightRepository;
 import reviewme.highlight.service.dto.HighlightsRequest;
@@ -23,6 +24,7 @@ public class HighlightService {
     private final HighlightMapper highlightMapper;
     private final AnswerValidator answerValidator;
 
+    @RequireReviewGroupAccess(target = "#highlightsRequest.reviewGroupId()")
     @Transactional
     public void editHighlight(HighlightsRequest highlightsRequest) {
         ReviewGroup reviewGroup = reviewGroupRepository.findById(highlightsRequest.reviewGroupId())
