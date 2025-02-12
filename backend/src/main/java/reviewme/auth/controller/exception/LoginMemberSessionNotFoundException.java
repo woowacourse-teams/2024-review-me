5c0aacbb7f7df44812efdaf47755f8aeb9fb560c
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-12 23:18:47 +0900

    feat: LoginMemberSessionResolver 구현

---
+++ backend/src/main/java/reviewme/auth/controller/exception/LoginMemberSessionNotFoundException.java
@@ -0,0 +1,13 @@
+package reviewme.auth.controller.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import reviewme.global.exception.BadRequestException;
+
+@Slf4j
+public class LoginMemberSessionNotFoundException extends BadRequestException {
+
+    public LoginMemberSessionNotFoundException() {
+        super("로그인 회원 세션이 존재하지 않아요.");
+        log.info("Login Member session does not exist.");
+    }
+}
