bece0e4b5b68c03ce1623b0dd23f6c0180045b28
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-12 23:24:32 +0900

    feat: 리졸버 config 추가

--- backend/src/main/java/reviewme/config/WebConfig.java
+++ backend/src/main/java/reviewme/config/WebConfig.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reviewme.auth.controller.GuestReviewGroupSessionResolver;
+import reviewme.auth.controller.LoginMemberSessionResolver;
+import reviewme.global.session.SessionManager;
 import reviewme.reviewgroup.controller.ReviewGroupSessionResolver;
 import reviewme.reviewgroup.service.ReviewGroupService;
 
@@ -13,9 +16,12 @@ import reviewme.reviewgroup.service.ReviewGroupService;
 public class WebConfig implements WebMvcConfigurer {
 
     private final ReviewGroupService reviewGroupService;
+    private final SessionManager sessionManager;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new ReviewGroupSessionResolver(reviewGroupService));
+        resolvers.add(new LoginMemberSessionResolver(sessionManager));
+        resolvers.add(new GuestReviewGroupSessionResolver(sessionManager));
     }
 }
