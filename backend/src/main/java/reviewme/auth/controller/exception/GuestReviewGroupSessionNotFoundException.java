f796e53940017cd124e3b3c41f3fbfb7e130ccad
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-12 23:23:09 +0900

    feat: GuestReviewGroupSessionResolver 구현

---
+++ backend/src/main/java/reviewme/auth/controller/exception/GuestReviewGroupSessionNotFoundException.java
@@ -0,0 +1,13 @@
+package reviewme.auth.controller.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import reviewme.global.exception.BadRequestException;
+
+@Slf4j
+public class GuestReviewGroupSessionNotFoundException extends BadRequestException {
+
+    public GuestReviewGroupSessionNotFoundException() {
+        super("비회원 리뷰 그룹 세션이 존재하지 않아요.");
+        log.info("Guest ReviewGroup session does not exist.");
+    }
+}
