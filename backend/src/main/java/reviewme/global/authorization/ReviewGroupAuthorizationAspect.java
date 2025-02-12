79ea4e8e4e812fb5fe65437fb55d24f4c4e2e701
nayonsoso <panda0329@naver.com>
2025-02-12 17:00:16 +0900

    feat: 리뷰 그룹 권한 aop 구현

---
+++ backend/src/main/java/reviewme/global/authorization/ReviewGroupAuthorizationAspect.java
@@ -0,0 +1,53 @@
+package reviewme.global.authorization;
+
+import static reviewme.global.authorization.ResourceAuthorizationUtils.getCurrentSession;
+import static reviewme.global.authorization.ResourceAuthorizationUtils.getTarget;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import reviewme.auth.domain.GitHubMember;
+import reviewme.global.authorization.exception.UnauthorizedReviewGroupAccessException;
+import reviewme.global.session.SessionManager;
+import reviewme.reviewgroup.domain.ReviewGroup;
+import reviewme.reviewgroup.domain.exception.ReviewGroupNotFoundException;
+import reviewme.reviewgroup.repository.ReviewGroupRepository;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ReviewGroupAuthorizationAspect {
+
+    private final SessionManager sessionManager;
+    private final ReviewGroupRepository reviewGroupRepository;
+
+    @Transactional
+    @Around("@annotation(requireReviewGroupAccess)")
+    public Object checkReviewGroupAccess(ProceedingJoinPoint joinPoint,
+                                         RequireReviewGroupAccess requireReviewGroupAccess) throws Throwable {
+        long reviewGroupId = getTarget(joinPoint, requireReviewGroupAccess.target(), Long.class);
+        ReviewGroup reviewGroup = reviewGroupRepository.findById(reviewGroupId)
+                .orElseThrow(() -> new ReviewGroupNotFoundException(reviewGroupId));
+
+        HttpSession session = getCurrentSession();
+        if (session == null || !(isMemberAuthorized(reviewGroup, session) || isGuestAuthorized(reviewGroup, session))) {
+            throw new UnauthorizedReviewGroupAccessException();
+        }
+
+        return joinPoint.proceed();
+    }
+
+    private boolean isMemberAuthorized(ReviewGroup reviewGroup, HttpSession session) {
+        GitHubMember gitHubMember = sessionManager.getGitHubMember(session);
+        return gitHubMember != null && reviewGroup.getMemberId() == gitHubMember.getMemberId();
+    }
+
+    private boolean isGuestAuthorized(ReviewGroup reviewGroup, HttpSession session) {
+        String reviewRequestCode = sessionManager.getReviewRequestCode(session);
+        return reviewGroup.getReviewRequestCode().equals(reviewRequestCode);
+    }
+}
