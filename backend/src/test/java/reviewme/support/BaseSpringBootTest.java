61f5260ed14225181fd5f5c2c5de968ca14b5f2d
nayonsoso <panda0329@naver.com>
2025-02-12 17:03:26 +0900

    test: 서비스 코드의 테스트에 aop가 동작하지 않도록 설정 변경

--- backend/src/test/java/reviewme/support/BaseSpringBootTest.java
+++ backend/src/test/java/reviewme/support/BaseSpringBootTest.java
@@ -1,5 +1,6 @@
 package reviewme.support;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,11 +8,11 @@ import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import reviewme.config.ServiceTestConfig;
+import reviewme.config.TestConfig;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = ServiceTestConfig.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = TestConfig.class)
 @ExtendWith({DatabaseCleanerExtension.class, CacheCleanerExtension.class})
-public @interface ServiceTest {
+public @interface BaseSpringBootTest {
 }
