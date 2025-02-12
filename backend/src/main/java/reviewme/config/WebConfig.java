6a42558f8834f8363fa80905b3c70655fa17aa5e
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-12 10:06:21 +0900

    refactor: 기존 ReviewGroupSession 제거

--- backend/src/main/java/reviewme/config/WebConfig.java
+++ backend/src/main/java/reviewme/config/WebConfig.java
@@ -8,19 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reviewme.auth.controller.GuestReviewGroupSessionResolver;
 import reviewme.auth.controller.LoginMemberSessionResolver;
 import reviewme.global.session.SessionManager;
-import reviewme.reviewgroup.controller.ReviewGroupSessionResolver;
-import reviewme.reviewgroup.service.ReviewGroupService;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final ReviewGroupService reviewGroupService;
     private final SessionManager sessionManager;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new ReviewGroupSessionResolver(reviewGroupService));
         resolvers.add(new LoginMemberSessionResolver(sessionManager));
         resolvers.add(new GuestReviewGroupSessionResolver(sessionManager));
     }
