819c7e02e4858e3aebca362ff3fabe7e70ef90a7
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-13 23:27:11 +0900

    flyway: 트랜잭션 제거

--- backend/src/main/resources/db/migration/V7__review_table_change.sql
+++ backend/src/main/resources/db/migration/V7__review_table_change.sql
@@ -2,8 +2,6 @@
 ALTER TABLE new_review ADD COLUMN member_id BIGINT NULL;
 
 -- 기존 review 테이블을 삭제하고, new_review 테이블의 이름을 "review"로 변경합니다.
--- 운영 환경에서의 오류 발생 시, 작업 롤백을 위해 트랜잭션 실행합니다.
-START TRANSACTION;
-    DROP TABLE IF EXISTS review;
-    ALTER TABLE new_review RENAME TO review;
-COMMIT;
+-- flyway는 각 마이그레이션 스크립트가 자체적으로 하나의 트랜잭션으로 실행되어서 트랜잭션 처리는 따로 하지 않습니다.
+DROP TABLE IF EXISTS review;
+ALTER TABLE new_review RENAME TO review;
