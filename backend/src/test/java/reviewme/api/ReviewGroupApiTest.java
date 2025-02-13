30fdbf24587f97ef847e8ca1e52ce6f392d19485
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-13 17:38:02 +0900

    test: 수정사항 테스트 반영

--- backend/src/test/java/reviewme/api/ReviewGroupApiTest.java
+++ backend/src/test/java/reviewme/api/ReviewGroupApiTest.java
@@ -2,6 +2,7 @@ package reviewme.api;
 
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
@@ -13,7 +14,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -173,11 +174,11 @@ class ReviewGroupApiTest extends ApiTest {
 
         ReviewGroupPageResponse response = new ReviewGroupPageResponse(2L, true,
                 List.of(
-                        new ReviewGroupPageElementResponse("이동훈", "우테코", LocalDate.of(2024, 1, 30), "WOOTECO1", 1),
-                        new ReviewGroupPageElementResponse("아루", "리뷰미", LocalDate.of(2024, 1, 5), "ABCD1234", 2)
+                        new ReviewGroupPageElementResponse(1L, "이동훈", "우테코", "WOOTECO1", LocalDateTime.of(2024, 1, 30, 0, 0), 1),
+                        new ReviewGroupPageElementResponse(2L, "아루", "리뷰미", "ABCD1234", LocalDateTime.of(2024, 1, 5, 0, 0), 2)
                 )
         );
-        BDDMockito.given(reviewGroupLookupService.getMyReviewGroups())
+        BDDMockito.given(reviewGroupLookupService.getMyReviewGroups(nullable(Long.class), nullable(Integer.class), anyLong()))
                 .willReturn(response);
 
         CookieDescriptor[] cookieDescriptors = {
@@ -188,10 +189,11 @@ class ReviewGroupApiTest extends ApiTest {
                 fieldWithPath("lastReviewGroupId").description("해당 페이지의 마지막 리뷰 그룹 ID"),
                 fieldWithPath("isLastPage").description("마지막 페이지 여부"),
                 fieldWithPath("reviewGroups[]").description("리뷰 그룹 목록 (생성일 기준 내림차순 정렬)"),
+                fieldWithPath("reviewGroups[].reviewGroupId").description("리뷰 그룹 ID"),
                 fieldWithPath("reviewGroups[].revieweeName").description("리뷰이 이름"),
                 fieldWithPath("reviewGroups[].projectName").description("프로젝트 이름"),
-                fieldWithPath("reviewGroups[].createdAt").description("생성일"),
                 fieldWithPath("reviewGroups[].reviewRequestCode").description("리뷰 요청 코드"),
+                fieldWithPath("reviewGroups[].createdAt").description("생성일"),
                 fieldWithPath("reviewGroups[].reviewCount").description("작성된 리뷰 수")
         };
 
