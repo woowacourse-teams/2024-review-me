6143a8a3ace7b0ce9531ad3c5c4615e5eca1b60b
nayonsoso <panda0329@naver.com>
2025-02-14 02:06:22 +0900

    docs: 마이그레이션 스크립트 작성

--- backend/src/main/resources/db/migration/V7__review_table_change.sql
+++ backend/src/main/resources/db/migration/V7__review_table_change.sql
@@ -1,6 +1,21 @@
--- 이전에 review 테이블 변경 사항 중 new_review가 아닌 review에 적용된 사항을 다시 반영합니다.
+-- 기존 테이블을 마이그레이션이 완료된 테이블로 대체합니다.
+
+-- 마이그레이션이 테이블에 기존 테이블과 동일한 인덱스를 생성합니다.
+CREATE INDEX review_idx_review_group_id ON new_review (review_group_id);
+
+-- 마이그레이션이 테이블에 기존 테이블의 변경 사항을 반영합니다.
 ALTER TABLE new_review ADD COLUMN member_id BIGINT NULL;
 
--- 기존 review 테이블을 삭제하고, new_review 테이블의 이름을 "review"로 변경합니다.
+-- 기존 테이블을 삭제합니다.
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS checkbox_answer;
+DROP TABLE IF EXISTS checkbox_answer_selected_option;
 DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS text_answer;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- 마이그레이션이 완료된 테이블의 이름을 변경합니다.
+ALTER TABLE new_checkbox_answer RENAME TO checkbox_answer;
+ALTER TABLE new_checkbox_answer_selected_option RENAME TO checkbox_answer_selected_option;
 ALTER TABLE new_review RENAME TO review;
+ALTER TABLE new_text_answer RENAME TO text_answer;
