2392c2a63e30591eacac4fd70fd022823b5cb67b
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-13 10:30:44 +0900

    refactor: 리뷰 그룹 조회에서 memberId를 응답하도록 변경

--- backend/src/main/java/reviewme/reviewgroup/service/ReviewGroupLookupService.java
+++ backend/src/main/java/reviewme/reviewgroup/service/ReviewGroupLookupService.java
@@ -3,11 +3,11 @@ package reviewme.reviewgroup.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reviewme.reviewgroup.service.dto.ReviewGroupPageResponse;
-import reviewme.reviewgroup.service.exception.ReviewGroupNotFoundByReviewRequestCodeException;
 import reviewme.reviewgroup.domain.ReviewGroup;
 import reviewme.reviewgroup.repository.ReviewGroupRepository;
+import reviewme.reviewgroup.service.dto.ReviewGroupPageResponse;
 import reviewme.reviewgroup.service.dto.ReviewGroupSummaryResponse;
+import reviewme.reviewgroup.service.exception.ReviewGroupNotFoundByReviewRequestCodeException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,8 +20,8 @@ public class ReviewGroupLookupService {
         ReviewGroup reviewGroup = reviewGroupRepository.findByReviewRequestCode(reviewRequestCode)
                 .orElseThrow(() -> new ReviewGroupNotFoundByReviewRequestCodeException(reviewRequestCode));
 
-        // todo: 리뷰어 id 가 있다면 같이 응답해야 함
-        return new ReviewGroupSummaryResponse(null, reviewGroup.getReviewee(), reviewGroup.getProjectName());
+        return new ReviewGroupSummaryResponse(reviewGroup.getMemberId(), reviewGroup.getReviewee(),
+                reviewGroup.getProjectName());
     }
 
     public ReviewGroupPageResponse getMyReviewGroups() {
