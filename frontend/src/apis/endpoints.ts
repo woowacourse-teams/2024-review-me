a837bcb8ca19800f966d562df91ba8dbfb044e27
ImxYJL <allensain14@gmail.com>
2025-02-14 15:37:47 +0900

    fix: 누락된 return문 추가

--- frontend/src/apis/endpoints.ts
+++ frontend/src/apis/endpoints.ts
@@ -93,7 +93,7 @@ const endPoint = {
   gettingWrittenReviewList: (lastReviewId: number | null, size: number) => {
     const defaultEndpoint = `${WRITTEN_REVIEW_LIST_API_URL}?${WRITTEN_REVIEW_PARAMS.queryString.size}=${size}`;
 
-    if (!lastReviewId) defaultEndpoint;
+    if (!lastReviewId) return defaultEndpoint;
     return defaultEndpoint + '&' + `${WRITTEN_REVIEW_PARAMS.queryString.lastReviewId}=${lastReviewId}`;
   },
 };
