2de7b318351788de703cc8dd7b97ec448804c982
nayonsoso <panda0329@naver.com>
2025-02-16 01:06:29 +0900

    feat: 사용자 프로필 반환 api 구현

--- backend/src/main/java/reviewme/member/controller/MemberController.java
+++ backend/src/main/java/reviewme/member/controller/MemberController.java
@@ -1,21 +1,28 @@
 package reviewme.member.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reviewme.auth.domain.GitHubMember;
 import reviewme.member.service.MemberService;
 import reviewme.member.service.dto.ProfileResponse;
+import reviewme.security.session.SessionManager;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/v2/members/profile")
-    public ResponseEntity<ProfileResponse> getProfile() {
-        ProfileResponse response = memberService.getProfile();
+    public ResponseEntity<ProfileResponse> getProfile(
+            HttpSession session
+    ) {
+        GitHubMember gitHubMember = sessionManager.getGitHubMember(session);
+        ProfileResponse response = memberService.getProfile(gitHubMember);
         return ResponseEntity.ok(response);
     }
 }
