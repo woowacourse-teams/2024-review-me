d05a8055d4fb5eaea48817a8cf3e1217178b602c
ImxYJL <allensain14@gmail.com>
2025-02-16 22:13:59 +0900

    fix: LargeContentContainer 스타일에 large 인근 경계값 추가

--- frontend/src/pages/WrittenReviewPage/components/layouts/LargeContent/styles.ts
+++ frontend/src/pages/WrittenReviewPage/components/layouts/LargeContent/styles.ts
@@ -1,17 +1,12 @@
 import styled from '@emotion/styled';
 
-import media from '@/utils/media';
-
+// large 초반 사이즈에서 레이아웃이 잘리는 것을 막기 위해 전용 미디어 쿼리 추가
 export const LargeContentContainer = styled.div`
   display: flex;
   gap: 6rem;
   justify-content: center;
 
-  ${media.medium} {
-    gap: 4rem;
-  }
-
-  ${media.small} {
-    margin: 0 2rem;
+  @media (min-width: 1025px) and (max-width: 1100px) {
+    gap: 1.4rem;
   }
 `;
