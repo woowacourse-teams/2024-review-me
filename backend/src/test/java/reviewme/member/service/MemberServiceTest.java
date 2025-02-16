387026c25fc41ad651b4466662571918c4566ff6
nayonsoso <panda0329@naver.com>
2025-02-17 01:19:49 +0900

    test: 테스트 메서드 이름 변경

--- backend/src/test/java/reviewme/member/service/MemberServiceTest.java
+++ backend/src/test/java/reviewme/member/service/MemberServiceTest.java
@@ -14,7 +14,7 @@ import reviewme.security.resolver.exception.LoginMemberSessionNotExistsException
 class MemberServiceTest {
 
     @Nested
-    @DisplayName("사용자 프로필을 조회한다.")
+    @DisplayName("사용자 프로필을 반환한다.")
     class getProfile {
 
         @Test
@@ -28,7 +28,7 @@ class MemberServiceTest {
         }
 
         @Test
-        void sessionExists() {
+        void 세션에_저장된_사용자_프로필을_반환한다() {
             // given
             long memberId = 1L;
             String nickname = "user";
