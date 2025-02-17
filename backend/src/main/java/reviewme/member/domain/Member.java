c315f43ab19a2c2a39a380a7c8d4c5d62454b0c4
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-17 20:49:05 +0900

    refactor: 사용되지 않는 메서드 제거

--- backend/src/main/java/reviewme/member/domain/Member.java
+++ backend/src/main/java/reviewme/member/domain/Member.java
@@ -2,10 +2,15 @@ package reviewme.member.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +28,29 @@ public class Member {
     @Column(name = "external_id", nullable = false, unique = true)
     private String externalId;
 
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
     public Member(String externalId) {
         this.externalId = externalId;
+    }
+
+    @PrePersist
+    void created() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    @PreUpdate
+    void updated() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
