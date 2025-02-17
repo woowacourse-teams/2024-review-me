fd0319735b3d9cbb946137250d63943781ea9f76
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-17 20:29:14 +0900

    test: getAuthoredReviews 테스트 작성

--- backend/src/test/java/reviewme/review/service/ReviewListLookupServiceTest.java
+++ backend/src/test/java/reviewme/review/service/ReviewListLookupServiceTest.java
@@ -3,7 +3,9 @@ package reviewme.review.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static reviewme.fixture.ReviewFixture.비회원_작성_리뷰;
+import static reviewme.fixture.ReviewFixture.회원_작성_리뷰;
 import static reviewme.fixture.ReviewGroupFixture.비회원_리뷰_그룹;
+import static reviewme.fixture.ReviewGroupFixture.회원_리뷰_그룹;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import reviewme.review.domain.CheckboxAnswer;
 import reviewme.review.domain.Review;
 import reviewme.review.domain.TextAnswer;
 import reviewme.review.repository.ReviewRepository;
+import reviewme.review.service.dto.response.list.AuthoredReviewElementResponse;
+import reviewme.review.service.dto.response.list.AuthoredReviewsResponse;
 import reviewme.review.service.dto.response.list.ReceivedReviewPageResponse;
 import reviewme.reviewgroup.domain.ReviewGroup;
 import reviewme.reviewgroup.repository.ReviewGroupRepository;
@@ -80,6 +84,37 @@ class ReviewListLookupServiceTest {
                         .isEqualTo(review2.getId()),
                 () -> assertThat(response.isLastPage())
                         .isFalse()
+        );
+    }
+
+    @Test
+    void 내가_작성한_리뷰_목록을_페이지네이션을_적용하여_반환한다() {
+        // given - 회원 설정
+        long memberId = 1L;
+
+        // given - 리뷰 그룹 설정
+        ReviewGroup reviewGroup1 = reviewGroupRepository.save(회원_리뷰_그룹());
+        Review review1 = reviewRepository.save(회원_작성_리뷰(memberId, 1L, reviewGroup1.getId(), List.of()));
+        Review review2 = reviewRepository.save(회원_작성_리뷰(memberId, 1L, reviewGroup1.getId(), List.of()));
+
+        ReviewGroup reviewGroup2 = reviewGroupRepository.save(회원_리뷰_그룹());
+        Review review3 = reviewRepository.save(회원_작성_리뷰(memberId, 1L, reviewGroup2.getId(), List.of()));
+        Review review4 = reviewRepository.save(회원_작성_리뷰(memberId, 1L, reviewGroup2.getId(), List.of()));
+        Review review5 = reviewRepository.save(회원_작성_리뷰(memberId, 1L, reviewGroup2.getId(), List.of()));
+
+        // given - 페이지네이션 설정
+        long lastReviewId = review4.getId();
+        int size = 2;
+
+        // when
+        AuthoredReviewsResponse response = reviewListLookupService.getAuthoredReviews(memberId, lastReviewId, size);
+
+        // then
+        assertAll(
+                () -> assertThat(response.lastReviewId()).isEqualTo(reviewGroup2.getId()),
+                () -> assertThat(response.isLastPage()).isFalse(),
+                () -> assertThat(response.reviews()).extracting(AuthoredReviewElementResponse::reviewId)
+                        .containsExactly(review3.getId(), review2.getId())
         );
     }
 }
