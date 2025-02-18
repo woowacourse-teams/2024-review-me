d499971e8b501adb1711a4a380df0268a736a1d1
soosoo22 <lllllh00@naver.com>
2025-02-18 11:20:36 +0900

    refactor: 로그인 훅에서 onSuccess 제거 후, OAuthCallbackPage에 적용
    Co-Authored-By: Fe <64690761+chysis@users.noreply.github.com>

--- frontend/src/pages/OAuthCallbackPage/index.tsx
+++ frontend/src/pages/OAuthCallbackPage/index.tsx
@@ -1,11 +1,11 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 import { LoadingBar } from '@/components';
-import { StateType } from '@/components/login/GitHubLoginButton';
-import { useToastContext } from '@/components/toast/ToastProvider';
-import { ROUTE } from '@/constants';
-import { useSearchParamAndQuery } from '@/hooks';
+import { LoginActionContextType } from '@/components/login/GitHubLoginButton';
+import { OAUTH_QUERY_KEY, ROUTE } from '@/constants';
+import { useSearchParamAndQuery, useToastContext } from '@/hooks';
 import { useOAuthLogin } from '@/hooks/oAuth';
 
 const OAuthCallbackPage = () => {
@@ -14,36 +14,43 @@ const OAuthCallbackPage = () => {
   });
 
   const navigate = useNavigate();
-  const mutation = useOAuthLogin();
+  const { showToast } = useToastContext();
+
+  const queryClient = useQueryClient();
 
   // 쿼리 파라미터로 전달된 state 파싱
   const params = new URLSearchParams(location.search);
   const stateParam = params.get('state');
-  const parsedState: StateType = JSON.parse(decodeURIComponent(stateParam!));
+  const parsedState: LoginActionContextType = JSON.parse(decodeURIComponent(stateParam!));
   const { prevUrl, action } = parsedState;
 
   // 연결 페이지에서 로그인 하는 경우 이전 동작을 담아 리다이렉트
   const redirectUrl =
     prevUrl && prevUrl.includes('review-zone') ? `${prevUrl}?login_action=${action}` : `/${ROUTE.reviewLinks}`;
 
-  const { showToast } = useToastContext();
+  const mutation = useOAuthLogin();
 
   useEffect(() => {
-    if (gitHubAuthCode) {
-      mutation.mutate(
-        { gitHubAuthCode },
-        {
-          onSuccess: () => {
-            navigate(redirectUrl, { replace: true });
-            showToast({ type: 'success', message: '환영합니다! 첫 리뷰를 받아보세요!' });
-          },
-          onError: () => {
-            navigate(prevUrl, { replace: true });
-            showToast({ type: 'error', message: '로그인에 실패했어요. 다시 시도해주세요!' });
-          },
-        },
-      );
+    if (!gitHubAuthCode) {
+      return;
     }
+
+    mutation.mutate(
+      { gitHubAuthCode },
+      {
+        onSuccess: () => {
+          navigate(redirectUrl, { replace: true });
+          showToast({ type: 'success', message: '환영합니다! 첫 리뷰를 받아보세요!' });
+
+          // 로그인 성공 시 프로필 정보를 받아오도록 함
+          queryClient.invalidateQueries({ queryKey: [OAUTH_QUERY_KEY.userProfile] });
+        },
+        onError: () => {
+          navigate(prevUrl, { replace: true });
+          showToast({ type: 'error', message: '로그인에 실패했어요. 다시 시도해주세요!' });
+        },
+      },
+    );
   }, []);
 
   return <LoadingBar />;
