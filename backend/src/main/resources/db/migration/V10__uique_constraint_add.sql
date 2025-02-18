3ba437b89c6d6cc9a0887c6618d300dcded6e5d3
KIMGYUTAE <kiimwf0819@gmail.com>
2025-02-18 10:50:02 +0900

    flyway: 중복 데이터를 삭제하는 스크립트 작성

---
+++ backend/src/main/resources/db/migration/V10__uique_constraint_add.sql
@@ -0,0 +1,15 @@
+-- 기존에 중복된 데이터를 제거합니다.
+-- 중복된 review_request_code 중 가장 오래된 하나만 남기고 삭제합니다.
+
+DELETE FROM review_group
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM review_group
+    GROUP BY review_request_code
+);
+
+
+-- 중복이 허용되지 않는 컬럼에 UNIQUE 제약 조건을 추가합니다.
+
+ALTER TABLE review_group MODIFY COLUMN review_request_code VARCHAR(255) NOT NULL UNIQUE;
+ALTER TABLE member MODIFY COLUMN external_id VARCHAR(225) NOT NULL UNIQUE;
