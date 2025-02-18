5510288280baeb54cc4577f80fdedbd98ecea38e
Fe <chysiss@naver.com>
2025-02-19 03:35:27 +0900

    feat: 로그아웃 시 리다이렉트 기능 구현

--- frontend/src/hooks/oAuth/useOAuthLogout.ts
+++ frontend/src/hooks/oAuth/useOAuthLogout.ts
@@ -1,13 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useLocation, useNavigate } from 'react-router';
 
 import { postOAuthLogoutApi } from '@/apis/oAuth';
-import { OAUTH_QUERY_KEY } from '@/constants';
+import { OAUTH_QUERY_KEY, ROUTE } from '@/constants';
 
 import useToastContext from '../useToastContext';
 
 const useOAuthLogout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const queryClient = useQueryClient();
   const { showToast } = useToastContext();
+
+  const redirectOnSuccess = () => {
+    if (location.pathname.includes('review-zone')) return;
+    return navigate(ROUTE.home, { replace: true });
+  };
 
   const mutation = useMutation({
     mutationKey: [OAUTH_QUERY_KEY.gitHubLogout],
@@ -18,6 +26,7 @@ const useOAuthLogout = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [OAUTH_QUERY_KEY.userProfile] });
       showToast({ type: 'success', message: '로그아웃이 완료되었어요!' });
+      redirectOnSuccess();
     },
     onError: () => {
       showToast({ type: 'error', message: '로그아웃에 실패했어요. 다시 시도해주세요!' });
