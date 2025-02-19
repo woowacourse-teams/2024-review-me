50326893a92ecd959c931139684c17b5f5b2e23b
badahertz52 <badahertz52@gmail.com>
2025-02-19 20:41:05 +0900

    refactor: 멤버 리뷰 그룹 확인 목 데이터에서 리뷰 아이디로 MOCK_USER_PROFILE.memberId 사용
    - 기존 매직 넘버로 작성된 숫자를 사용자 프로필 목 데이터를 활용해 수정

--- frontend/src/mocks/mockData/group.ts
+++ frontend/src/mocks/mockData/group.ts
@@ -1,5 +1,7 @@
 import { ReviewGroupData } from '@/types';
 
+import { MOCK_USER_PROFILE } from './userProfileData';
+
 export const VALIDATED_PASSWORD = '1234';
 
 export const MOCK_AUTH_TOKEN_NAME = 'mockAuthToken';
@@ -15,7 +17,7 @@ export const bothCookie = [MOCK_AUTH_TOKEN_NAME, MOCK_LOGIN_TOKEN_NAME];
 
 // 변수명이 카멜 케이스인 이유는 회원/비회원에 따라 revieweeId가 달라지기 때문에 reviewRequestCode보고 판단해서 회원이면 number, 비회원이면 null로 설정
 export const MEMBER_REVIEW_GROUP_DATA: ReviewGroupData = {
-  revieweeId: 2024,
+  revieweeId: MOCK_USER_PROFILE.memberId, //💡 본인이 만든 리뷰 링크가 아닐 때 토스트 기능을 확인하고 싶다면 다른 리뷰 아이디를 넣어서 확인해보세요
   revieweeName: '바다',
   projectName: '2024-review-me',
 };
