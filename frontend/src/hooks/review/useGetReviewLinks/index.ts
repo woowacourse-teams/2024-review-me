8089f74828c488b922aea3ed3146b091ca83870b
chysis <chysiss@naver.com>
2025-02-20 17:14:04 +0900

    chore: 생성한 링크 목록, 작성한 리뷰 목록 쿼리에 사용자 아이디를 queryKey로 추가

--- frontend/src/hooks/review/useGetReviewLinks/index.ts
+++ frontend/src/hooks/review/useGetReviewLinks/index.ts
@@ -2,10 +2,13 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { getReviewLinksApi } from '@/apis/review';
 import { REVIEW_QUERY_KEY } from '@/constants';
+import { useGetUserProfile } from '@/hooks/oAuth';
 
 const useGetReviewLinks = () => {
+  const { userProfile, isUserLoggedIn } = useGetUserProfile();
+
   const result = useSuspenseQuery({
-    queryKey: [REVIEW_QUERY_KEY.reviewLinks],
+    queryKey: [REVIEW_QUERY_KEY.reviewLinks, isUserLoggedIn && userProfile && userProfile.memberId],
     queryFn: () => getReviewLinksApi(),
     staleTime: 60 * 60 * 1000,
   });
--- frontend/src/pages/WrittenReviewPage/hooks/useGetWrittenReviewList/index.ts
+++ frontend/src/pages/WrittenReviewPage/hooks/useGetWrittenReviewList/index.ts
@@ -2,10 +2,13 @@ import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 import { getWrittenReviewListApi } from '@/apis/review';
 import { DEFAULT_SIZE_PER_PAGE, REVIEW_QUERY_KEY } from '@/constants';
+import { useGetUserProfile } from '@/hooks/oAuth';
 
 const useGetWrittenReviewList = () => {
+  const { userProfile, isUserLoggedIn } = useGetUserProfile();
+
   const { data, ...rest } = useSuspenseInfiniteQuery({
-    queryKey: [REVIEW_QUERY_KEY.writtenReviewList],
+    queryKey: [REVIEW_QUERY_KEY.writtenReviewList, isUserLoggedIn && userProfile && userProfile.memberId],
     queryFn: ({ pageParam }) =>
       getWrittenReviewListApi({
         lastReviewId: pageParam === 0 ? null : pageParam, // 첫 요청일 때 null으로 보냄
