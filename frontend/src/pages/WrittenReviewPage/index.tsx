d22471352efcd47d986913a5f699c3921a94527e
badahertz52 <badahertz52@gmail.com>
2025-02-20 23:59:23 +0900

    fix: 작성한 리뷰 페이지 에러 바운더리 캐치하지 못하는 오류 수정

--- frontend/src/pages/WrittenReviewPage/index.tsx
+++ frontend/src/pages/WrittenReviewPage/index.tsx
@@ -1,30 +1,11 @@
-import { ErrorSuspenseContainer, AuthAndServerErrorFallback, TopButton } from '@/components';
-import { useSearchParamAndQuery } from '@/hooks';
+import { ErrorSuspenseContainer, AuthAndServerErrorFallback } from '@/components';
 
-import { EmptyWrittenReview } from './components';
-import { LargeContent, UnderLargeContent } from './components/layouts';
-import { useDeviceBreakpoints, useGetWrittenReviewList } from './hooks';
+import PageContent from './components/PageContent';
 
 const WrittenReviewPage = () => {
-  const { deviceType } = useDeviceBreakpoints();
-  const { reviewList } = useGetWrittenReviewList();
-
-  const { queryString: reviewIdString } = useSearchParamAndQuery({
-    queryStringKey: 'reviewId',
-  });
-
-  const selectedReviewId = reviewIdString ? Number(reviewIdString) : null;
-
   return (
     <ErrorSuspenseContainer errorFallback={AuthAndServerErrorFallback}>
-      <TopButton />
-      {reviewList.length === 0 ? (
-        <EmptyWrittenReview />
-      ) : deviceType.isDesktop ? (
-        <LargeContent selectedReviewId={selectedReviewId} />
-      ) : (
-        <UnderLargeContent selectedReviewId={selectedReviewId} />
-      )}
+      <PageContent />
     </ErrorSuspenseContainer>
   );
 };
