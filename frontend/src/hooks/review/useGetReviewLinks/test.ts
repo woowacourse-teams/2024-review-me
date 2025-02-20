222147897224edc4daf44c8526a160186992ac1a
chysis <chysiss@naver.com>
2025-02-20 21:00:19 +0900

    fix: 테스트 코드에서 불필요한 인자 제거

--- frontend/src/hooks/review/useGetReviewLinks/test.ts
+++ frontend/src/hooks/review/useGetReviewLinks/test.ts
@@ -1,6 +1,5 @@
 import { renderHook, waitFor } from '@testing-library/react';
 
-import { MOCK_USER_PROFILE } from '@/mocks/mockData';
 import QueryClientWrapper from '@/queryTestSetup/QueryClientWrapper';
 import { testWithAuthCookie } from '@/utils';
 
@@ -9,7 +8,7 @@ import useGetReviewLinks from '.';
 describe('회원이 생성한 리뷰 그룹 목록 조회 테스트', () => {
   it('회원이 생성한 리뷰 그룹 목록을 성공적으로 불러온다.', async () => {
     const testReviewLinksAPI = async () => {
-      const { result } = renderHook(() => useGetReviewLinks({ memberId: MOCK_USER_PROFILE.memberId }), {
+      const { result } = renderHook(() => useGetReviewLinks(), {
         wrapper: QueryClientWrapper,
       });
 
