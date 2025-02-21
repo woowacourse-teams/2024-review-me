59f8f4fb183e56aee087ff3126d2949e36628657
badahertz52 <badahertz52@gmail.com>
2025-02-21 09:18:05 +0900

    feat:하이라이트 버튼 아이콘에 스켈레톤 적용

--- frontend/src/components/highlight/components/HighlightButton/index.tsx
+++ frontend/src/components/highlight/components/HighlightButton/index.tsx
@@ -1,7 +1,13 @@
 import EraserIcon from '@/assets/eraser.svg';
 import HighlighterIcon from '@/assets/highlighter.svg';
+import { ImgWithSkeleton } from '@/components/skeleton';
 
 import * as S from './style';
+
+const BUTTON_ICON_STYLE = {
+  width: '1.6rem',
+  height: '1.6rem',
+};
 
 interface DragHighlightAddButtonProps {
   addHighlightByDrag: () => void;
@@ -10,7 +16,14 @@ interface DragHighlightAddButtonProps {
 const DragHighlightAddButton = ({ addHighlightByDrag }: DragHighlightAddButtonProps) => {
   return (
     <S.Button onClick={addHighlightByDrag} aria-label="하이하이트 추가 버튼">
-      <S.ButtonIcon src={HighlighterIcon} alt="" />
+      <ImgWithSkeleton imgWidth={BUTTON_ICON_STYLE.width} imgHeight={BUTTON_ICON_STYLE.height}>
+        <S.ButtonIcon
+          src={HighlighterIcon}
+          alt=""
+          $height={BUTTON_ICON_STYLE.height}
+          $width={BUTTON_ICON_STYLE.width}
+        />
+      </ImgWithSkeleton>
     </S.Button>
   );
 };
@@ -22,15 +35,9 @@ interface DragHighlightRemoveButtonProps {
 const DragHighlightRemoveButton = ({ removeHighlightByDrag }: DragHighlightRemoveButtonProps) => {
   return (
     <S.Button onClick={removeHighlightByDrag} aria-label="하이라이트 삭제 버튼">
-      <S.ButtonIcon src={EraserIcon} alt="" />
-    </S.Button>
-  );
-};
-
-interface LongPressHighlightRemoveButtonProps {
-  removeHighlightByLongPress: () => void;
-}
-
+      <ImgWithSkeleton imgWidth={BUTTON_ICON_STYLE.width} imgHeight={BUTTON_ICON_STYLE.height}>
+        <S.ButtonIcon src={EraserIcon} alt="" $height={BUTTON_ICON_STYLE.height} $width={BUTTON_ICON_STYLE.width} />
+      </ImgWithSkeleton>
     </S.Button>
   );
 };
